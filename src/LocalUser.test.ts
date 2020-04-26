110e394c2d11e7ec329d34723273587bcada4ac4
Herb Caudill <herb@devresults.com>
2020-04-26 12:30:57 +0200

    add LocalUser

---
+++ src/LocalUser.test.ts
@@ -0,0 +1,68 @@
+import { LocalUser } from './LocalUser'
+import { signatures, asymmetric, symmetric } from './crypto'
+
+describe('LocalUser', () => {
+  const message = 'the crocodiles lunges at dawn'
+
+  beforeEach(() => {
+    localStorage.clear()
+  })
+
+  describe('constructor', () => {
+    it('should instantiate a user', () => {
+      const herb = new LocalUser({ username: 'herb' })
+      expect(herb).toHaveProperty('keyset')
+    })
+
+    it('should retrieve the keyset for a known user', () => {
+      // herb uses app for the first time
+      const herb1 = new LocalUser({ username: 'herb' })
+      const { keyset } = herb1
+      expect(keyset).toHaveProperty('signature')
+      expect(keyset).toHaveProperty('asymmetric')
+      expect(keyset).toHaveProperty('symmetric')
+
+      // herb uses app for the second time
+      const herb2 = new LocalUser({ username: 'herb' })
+
+      // keyset is the same
+      expect(herb2.keyset).toEqual(keyset)
+    })
+
+    it('should keep keysets separate for different users', () => {
+      // herb uses app
+      const herb = new LocalUser({ username: 'herb' })
+
+      // alice uses app on the same device (?)
+      const alice = new LocalUser({ username: 'alice' })
+
+      // keyset is different
+      expect(alice.keyset).not.toEqual(herb.keyset)
+    })
+
+    it('should provide a working keypair for signatures', () => {
+      const keypair = new LocalUser({ username: 'bob' }).keyset.signature
+      const { secretKey, publicKey } = keypair
+      const signature = signatures.sign(message, secretKey)
+      expect(
+        signatures.verify({ content: message, signature, publicKey })
+      ).toBe(true)
+    })
+
+    it('should provide a working keyset for asymmetric encryption', () => {
+      const a = new LocalUser({ username: 'charlie' }).keyset.asymmetric
+      const b = asymmetric.keyPair()
+      const cipher = asymmetric.encrypt(message, b.publicKey, a.secretKey)
+      const decrypted = asymmetric.decrypt(cipher, a.publicKey, b.secretKey)
+      expect(decrypted).toEqual(message)
+    })
+
+    it('should provide a working key for symmetric encryption', () => {
+      const { keyset } = new LocalUser({ username: 'eve' })
+      const key = keyset.symmetric
+      const cipher = symmetric.encrypt(message, key)
+      const decrypted = symmetric.decrypt(cipher, key)
+      expect(decrypted).toEqual(message)
+    })
+  })
+})
---
+++ src/LocalUser.ts
@@ -0,0 +1,46 @@
+﻿import { Base64Keypair, Base64 } from './types'
+import { asymmetric, symmetric, signatures } from './crypto'
+
+export interface Keyset {
+  signature: Base64Keypair
+  asymmetric: Base64Keypair
+  symmetric: Base64
+}
+
+export interface LocalUserOptions {
+  username: string
+}
+
+const STORAGE_KEY = 'TACO_KEY_STORAGE'
+
+export class LocalUser {
+  constructor(options: LocalUserOptions) {
+    const { username } = options
+    this.username = username
+
+    this.keyset = this.loadKeyset() || this.generateNewKeyset()
+    this.storeKeyset()
+  }
+
+  public username: string
+  public keyset: Keyset
+
+  private loadKeyset = (): Keyset | undefined => {
+    const allKeysets = JSON.parse(localStorage.getItem(STORAGE_KEY) || '{}')
+    return allKeysets[this.username]
+  }
+
+  private storeKeyset = () => {
+    const allKeysets = JSON.parse(localStorage.getItem(STORAGE_KEY) || '{}')
+    allKeysets[this.username] = this.keyset
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(allKeysets))
+  }
+
+  private generateNewKeyset = (): Keyset => {
+    return {
+      signature: signatures.keyPair(),
+      asymmetric: asymmetric.keyPair(),
+      symmetric: symmetric.key(),
+    }
+  }
+}
