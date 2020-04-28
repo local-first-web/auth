1985e7bdd1412289c51209aeae2854e7407a540b
Herb Caudill <herb@devresults.com>
2020-04-28 13:50:09 +0200

    types.ts:

--- src/types.ts
+++ src/types.ts
@@ -1,70 +1,70 @@
-﻿export interface TacoOptions {
-  localUserName: string
-}
+﻿export type SignatureChain = SignedLink[]
 
-export interface TeamOptions {
-  localUserName: string
-  source?: string | object // JSON or instantiated object
-  secureStorage?: any // TODO
-}
-
-export type SignatureChain = SignatureBlock[]
-
-export interface SignatureBlock {
-  body: {
-    type: SignatureBlockType
-
-    // context
-    device: Device
-    user: string
-    client?: Client
-
-    // public keys
-    encryption_key?: Base64
-    signing_key: Base64
-
-    // the generation increments every time keys are rotated
-    generation: number
-
-    // hash of previous block
-    // TODO hmac? key?
-    prev: Base64
-
-    // Unix timestamp on device that created this block
-    timestamp: UnixTimestamp
-
-    // Unix time when this block should be automatically revoked
-    expires?: UnixTimestamp
-
-    // index of this block within signature chain
-    index: number
+export interface SignedLink {
+  body: LinkBody
+  signed: {
+    signature: Base64
+    name: string
+    key: Base64
   }
-
-  // body signed with `signing_key`
-  signature: Base64
 }
 
-export enum SignatureBlockType {
-  root,
-  invite,
-  add_member,
-  add_device,
-  add_role,
-  change_membership,
-  revoke,
-  rotate,
+export interface LinkBody {
+  type: SignatureBlockType
+
+  // context
+  device?: Device
+  user: string
+  client?: Client
+
+  // public keys
+  encryption_key?: Base64
+  signing_key: Base64
+
+  // the generation increments every time keys are rotated
+  generation: number
+
+  // hash of previous block
+  // TODO hmac? key?
+  prev: Base64 | null
+
+  // Unix timestamp on device that created this block
+  timestamp: UnixTimestamp
+
+  // Unix time when this block should be automatically revoked
+  expires?: UnixTimestamp
+
+  // index of this block within signature chain
+  index: number
 }
 
-export enum DeviceType {
-  desktop,
-  laptop,
-  tablet,
-  mobile,
-  other,
+export const signatureBlockType = {
+  root: 1,
+  invite: 2,
+  add_member: 3,
+  add_device: 4,
+  add_role: 5,
+  change_membership: 6,
+  revoke: 7,
+  rotate: 8,
 }
+
+export type SignatureBlockType = ValueOf<typeof signatureBlockType>
+
+export const deviceType = {
+  desktop: 1,
+  laptop: 2,
+  tablet: 3,
+  mobile: 4,
+  bot: 5,
+  server: 6,
+  other: 99,
+}
+export type DeviceType = ValueOf<typeof signatureBlockType>
 
 export interface Device {
   id: Base64
+  name: string
   type: DeviceType
 }
 
@@ -78,9 +78,11 @@ export type Utf8 = string
 export type Base64 = string
 export type SemVer = string
 export type Key = Utf8 | Uint8Array
-export type Message = Base64 | Uint8Array
+export type Message = Base64 | Uint8Array | object
 
 export type Base64Keypair = {
   publicKey: Base64
   secretKey: Base64
 }
+
+export type ValueOf<Obj> = Obj[keyof Obj]
