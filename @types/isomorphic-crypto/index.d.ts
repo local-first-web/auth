7e1421e88ae721eead92a01d7ccfdf1f8f76997e
Herb Caudill <herb@devresults.com>
2020-05-02 19:16:20 +0200

    @types/isomorphic-crypto/index.d.ts:

--- @types/isomorphic-crypto/index.d.ts
+++ @types/isomorphic-crypto/index.d.ts
@@ -1,7 +1,5 @@
 ﻿/// <reference types="node" />
 
 declare module 'isomorphic-crypto' {
-  import crypto from 'crypto'
-  declare const createHmac = crypto.createHmac
-  declare const createHash = crypto.createHash
+  export { createHmac, createHash } from 'crypto'
 }
