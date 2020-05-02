19786e8e5321f79583b44a72cc95d41b53d08b9b
Herb Caudill <herb@devresults.com>
2020-05-02 13:08:11 +0200

    constants.ts:

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
-﻿export const HASH_PURPOSE = {
-  SIGNATURE: 'derived-signature',
-  ENCRYPTION_ASYMMETRIC: 'derived-encryption-asymmetric',
-  ENCRYPTION_SYMMETRIC: 'derived-encryption-symmetric',
-  LINK_TO_PREVIOUS: 'link-to-previous',
+﻿export enum HashPurpose {
+  Signature = 'SIGNATURE',
+  EncryptionAsymmetric = 'ENCRYPTION_ASYMMETRIC',
+  EncryptionSymmetric = 'ENCRYPTION_SYMMETRIC',
+  LinkToPrevious = 'LINK_TO_PREVIOUS',
 }
