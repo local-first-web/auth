64a6027ca9d71cf40fa1e774c085ef33f3f9be3b
Herb Caudill <herb@devresults.com>
2020-05-04 18:20:56 +0200

    team/reducer.ts: create

---
+++ src/team/reducer.ts
@@ -0,0 +1,23 @@
+﻿import { TeamState, RootLinkPayload } from './types'
+import { SignedLink } from 'chain'
+
+export const reducer = (prevState: TeamState, link: SignedLink) => {
+  const { type, payload } = link.body
+  switch (type) {
+    case 'ROOT': {
+      const { name } = payload as RootLinkPayload
+      const nextState = {
+        ...prevState,
+        name,
+      }
+      return nextState
+    }
+
+    case 'ADD_MEMBER': {
+      const nextState = prevState
+      // ..
+      return nextState
+    }
+  }
+  return prevState
+}
