3f561a01b5bd7ba73707ca95bafee7c6fdd475d4
Herb Caudill <herb@devresults.com>
2020-05-04 18:37:57 +0200

    team:

--- src/team/reducer.ts
+++ src/team/reducer.ts
@@ -1,23 +1,25 @@
-﻿import { TeamState, RootLinkPayload } from './types'
+﻿import { TeamState, RootLinkPayload, LinkType } from './types'
 import { SignedLink } from 'chain'
 
 export const reducer = (prevState: TeamState, link: SignedLink) => {
   const { type, payload } = link.body
   switch (type) {
-    case 'ROOT': {
-      const { name } = payload as RootLinkPayload
-      const nextState = {
+    case LinkType.ROOT: {
+      const { name, rootContext } = payload as RootLinkPayload
+      return {
         ...prevState,
         name,
+        rootContext,
       }
-      return nextState
     }
 
-    case 'ADD_MEMBER': {
+    case LinkType.ADD_MEMBER: {
       const nextState = prevState
       // ..
       return nextState
     }
   }
+
+  // fallthrough
   return prevState
 }
