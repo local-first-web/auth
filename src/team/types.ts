4e317b6e232f336354ea70b12ef545d158158682
Herb Caudill <herb@devresults.com>
2020-05-04 18:37:44 +0200

    team/types.ts:

--- src/team/types.ts
+++ src/team/types.ts
@@ -1,3 +1,6 @@
+import { Context, ContextWithSecrets } from '../context'
+import { SignatureChain } from '../chain'
+
 export enum LinkType {
   ROOT,
   ADD_MEMBER,
@@ -7,4 +10,34 @@ export enum LinkType {
   CHANGE_MEMBERSHIP,
   REVOKE,
   ROTATE,
+}
+
+export interface TeamState {
+  name: string
+  rootContext?: Context
+  members: string[]
+  roles: string[]
+}
+
+export interface NewTeamOptions {
+  name: string
+  context: ContextWithSecrets
+}
+
+export interface ExistingTeamOptions {
+  source: SignatureChain
+  context: ContextWithSecrets
+}
+
+export type TeamOptions = NewTeamOptions | ExistingTeamOptions // type guard
+
+export function isExistingTeam(
+  options: TeamOptions
+): options is ExistingTeamOptions {
+  return (options as ExistingTeamOptions).source !== undefined
+}
+
+export interface RootLinkPayload {
+  name: string
+  rootContext: Context
 }
