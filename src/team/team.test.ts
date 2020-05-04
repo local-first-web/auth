497ecb929af71e50000cdc9f165f1209012ecca2
Herb Caudill <herb@devresults.com>
2020-05-04 18:38:27 +0200

    team/team.test.ts:

--- src/team/team.test.ts
+++ src/team/team.test.ts
@@ -25,8 +25,6 @@ describe('Team', () => {
     localStorage.clear()
   })
 
-  test('should ', () => {})
-
   const setup = () => {
     const team = new Team({ name: 'Spies Я Us', context })
     return { team }
@@ -36,23 +34,4 @@ describe('Team', () => {
     const { team } = setup()
     expect(team.name).toBe('Spies Я Us')
   })
-
-  // it('adds a root block to the signature chain', () => {
-  //   const { team } = setup()
-  //   expect(team.signatureChain).toHaveLength(1)
-  //   const rootBlock = team.signatureChain[0].body
-  //   expect(rootBlock.prev).toBeNull()
-  //   expect(rootBlock.type).toEqual(LinkType.root)
-  // })
-
-  // it('root block has a valid signature', () => {
-  //   const { team } = setup()
-  //   const { body, signed } = team.signatureChain[0]
-  //   const signedMessage = {
-  //     content: body,
-  //     signature: signed.signature,
-  //     publicKey: signed.key,
-  //   }
-  //   const isValid = signatures.verify(signedMessage)
-  //   expect(isValid).toBe(true)
 })
