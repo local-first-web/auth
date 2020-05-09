7b580901686c5a9b697d4d6f36982000d734a01e
Herb Caudill <herb@devresults.com>
2020-05-09 08:14:35 +0200

    rename hmac to hash

---
+++ src/lib/hash.ts
@@ -0,0 +1,18 @@
+﻿import nacl from 'tweetnacl'
+import { Key, Payload } from './types'
+import { keyToBytes } from './keyToBytes'
+import { payloadToBytes } from './payloadToBytes'
+
+export const hash = (seed: Key, payload: Payload) => {
+  const seedBytes = keyToBytes(seed, 'utf8')
+  const payloadBytes = payloadToBytes(payload)
+  const keyAndPayload = concatenate(seedBytes, payloadBytes)
+  return nacl.hash(keyAndPayload)
+}
+
+const concatenate = (a: Uint8Array, b: Uint8Array): Uint8Array => {
+  const c = new Uint8Array(a.length + b.length)
+  c.set(a)
+  c.set(b, a.length)
+  return c
+}
