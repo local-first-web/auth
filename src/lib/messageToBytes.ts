4eb6afb46ddfbeb50fc9e21c8ff52b272b04f8f4
Herb Caudill <herb@devresults.com>
2020-05-09 08:15:30 +0200

    lib/messageToBytes.ts:

--- src/lib/messageToBytes.ts
+++ src/lib/messageToBytes.ts
@@ -1,6 +1,7 @@
-﻿import { Message } from 'lib/types'
+﻿import { Message } from './types'
+import { utf8 } from './utf8'
 
 export const messageToBytes = (x: Message) =>
   typeof x === 'string'
-    ? Buffer.from(x) // string
+    ? utf8.encode(x) // string
     : x // Uint8Array
