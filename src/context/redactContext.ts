6b0436310307f748571db3fcdd50b06feee54c43
Herb Caudill <herb@devresults.com>
2020-05-09 08:16:14 +0200

    context/redactContext.ts: create

---
+++ src/context/redactContext.ts
@@ -0,0 +1,7 @@
+﻿import { Context, ContextWithSecrets } from './types'
+import { redactUser } from '../user'
+
+export const redactContext = (context: ContextWithSecrets): Context => ({
+  ...context,
+  user: redactUser(context.user),
+})
