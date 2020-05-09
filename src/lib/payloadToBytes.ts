1899d1a32a1137189186893e6c01f87e73e6f037
Herb Caudill <herb@devresults.com>
2020-05-09 08:15:36 +0200

    lib/payloadToBytes.ts:

--- src/lib/payloadToBytes.ts
+++ src/lib/payloadToBytes.ts
@@ -1,13 +1,14 @@
 ﻿import stringify from 'json-stable-stringify'
 import { Payload } from 'lib/types'
+import { utf8 } from './utf8'
 
 /** Takes either a string, a byte array, or a javascript object, and returns it as a byte array */
 export const payloadToBytes = (x: Payload): Uint8Array =>
   typeof x === 'string'
     ? // string
-      Buffer.from(x)
+      utf8.encode(x)
     : ArrayBuffer.isView(x)
     ? // byte array
       x
     : // object
-      Buffer.from(stringify(x))
+      utf8.encode(stringify(x))
