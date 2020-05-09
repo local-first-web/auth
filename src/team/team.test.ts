b8722bcdac00448c38eeee4ef0052454892baa76
Herb Caudill <herb@devresults.com>
2020-05-09 08:22:44 +0200

    team/team.test.ts:

--- src/team/team.test.ts
+++ src/team/team.test.ts
@@ -4,34 +4,34 @@ import { UserWithSecrets } from '../user'
 import { Team } from './team'
 
 describe('Team', () => {
-  const alice: UserWithSecrets = {
-    name: 'alice',
-    keys: deriveKeys(randomKey()),
-  }
-  const eve: UserWithSecrets = {
-    name: 'eve',
-    keys: deriveKeys(randomKey()),
-  }
-
-  const context: ContextWithSecrets = {
-    user: alice,
-    device: {
-      name: 'windows laptop',
-      type: DeviceType.laptop,
-    },
-    client: {
-      name: 'test',
-      version: '0',
-    },
-  }
-
   beforeEach(() => {
     localStorage.clear()
   })
 
   const setup = () => {
+    const alice: UserWithSecrets = {
+      name: 'alice',
+      keys: deriveKeys(randomKey()),
+    }
+    const eve: UserWithSecrets = {
+      name: 'eve',
+      keys: deriveKeys(randomKey()),
+    }
+
+    const context: ContextWithSecrets = {
+      user: alice,
+      device: {
+        name: 'windows laptop',
+        type: DeviceType.laptop,
+      },
+      client: {
+        name: 'test',
+        version: '0',
+      },
+    }
+
     const team = new Team({ teamName: 'Spies Я Us', context })
-    return { team }
+    return { team, context }
   }
 
   describe('create', () => {
@@ -41,7 +41,7 @@ describe('Team', () => {
     })
 
     it('saves & loads', () => {
-      const { team } = setup()
+      const { team, context } = setup()
       const savedTeam = team.save()
       const restoredTeam = new Team({ source: JSON.parse(savedTeam), context })
       expect(restoredTeam.name).toBe('Spies Я Us')
@@ -49,6 +49,13 @@ describe('Team', () => {
   })
 
   describe('members', () => {
+    it('has a root member', () => {
+      const { team } = setup()
+      expect(team.members().length).toBe(1)
+      const alice = team.members('alice')
+      expect(alice.name).toBe('alice')
+    })
+
     it('has a root member', () => {
       const { team } = setup()
       expect(team.members().length).toBe(1)
