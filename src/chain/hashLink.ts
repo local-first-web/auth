0c8b73fbc5702c6cc42092126db45e04f6accf8f
Herb Caudill <herb@devresults.com>
2020-05-09 08:16:40 +0200

    chain/hashLink.ts:

--- src/chain/hashLink.ts
+++ src/chain/hashLink.ts
@@ -1,6 +1,6 @@
 ﻿import { SignedLink } from './types'
-import { hmac, base64 } from '../lib'
+import { hash, base64 } from '../lib'
 import { HashPurpose } from '../lib/constants'
 
 export const hashLink = (link: SignedLink) =>
-  base64.encode(hmac(HashPurpose.LinkToPrevious, link))
+  base64.encode(hash(HashPurpose.LinkToPrevious, link))
