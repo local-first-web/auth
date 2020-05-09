54935f6237648c55e28971741517a10307071ad3
Herb Caudill <herb@devresults.com>
2020-05-09 08:19:41 +0200

    team/team.ts:

--- src/team/team.ts
+++ src/team/team.ts
@@ -1,16 +1,25 @@
 ﻿import { EventEmitter } from 'events'
-import * as chain from '../chain'
-import { SignatureChain, validate } from '../chain'
+import { chain, PartialLinkBody, SignatureChain, validate } from '../chain'
 import { ContextWithSecrets } from '../context'
-import { redactSecrets } from '../keys'
+import {
+  deriveKeys,
+  KeysetWithSecrets,
+  randomKey,
+  redactKeys,
+  PublicKeyset,
+} from '../keys'
+import { redactUser, User } from '../user'
 import { reducer } from './reducer'
 import {
+  AddMemberPayload,
   ExistingTeamOptions,
-  isExistingTeam,
+  exists,
+  linkType,
   NewTeamOptions,
-  RootLinkPayload,
+  RootPayload,
   TeamOptions,
   TeamState,
+  Member,
 } from './types'
 
 export class Team extends EventEmitter {
@@ -19,87 +28,117 @@ export class Team extends EventEmitter {
 
     this.context = options.context
 
-    if (isExistingTeam(options)) this.load(options)
+    if (exists(options)) this.loadChain(options)
     else this.create(options)
-
-    this.state = this.extractState()
   }
 
-  // public properties
-
-  public chain: SignatureChain
-
-  // public functions
+  // public API
 
   public get name() {
     return this.state.name
   }
 
-  public save = () => {}
-
-  public add = (_name: string) => {
-    // NEXT: ADD A MEMBER
+  public save = () => {
+    return JSON.stringify(this.chain)
   }
 
-  public invite = (_name: string) => {}
+  public add = (name: string, keys: PublicKeyset) => {
+    if (this.members(name)) throw new Error(`Member ${name} already exists`)
+    this.addMember({ name, keys })
+  }
 
-  public remove = (_name: string) => {}
+  public invite = (name: string) => {}
 
-  public members = (_name?: string) => {
-    if (_name === undefined) {
-      // return all members
+  public remove = (name: string) => {}
+
+  public members(): Member[]
+  public members(name: string): Member
+  public members(name?: string): Member | Member[] {
+    if (name === undefined) {
+      return this.state.members
     } else {
-      // return one member
+      const result = this.state.members.find(m => m.name === name)
+      if (!result) throw new Error(`Member ${name} was not found`)
+      return result
     }
   }
 
   public roles = {
-    add: (_name: string) => {},
-    remove: (_name: string) => {},
+    add: (name: string) => {},
+    remove: (name: string) => {},
     list: () => {},
   }
 
   // private properties
 
-  public state: TeamState
+  private chain: SignatureChain
   private context: ContextWithSecrets
+  private state: TeamState
 
   // private functions
 
-  private extractState = (): TeamState => {
-    this.validate()
+  private validateChain() {
+    const validation = validate(this.chain)
+    if (!validation.isValid) throw validation.error
+  }
+
+  private updateState = () => {
+    this.validateChain()
     const initialState = {
       name: '',
       members: [],
-      roles: [],
+      roles: ['admin'],
     }
-    return this.chain.reduce<TeamState>(reducer, initialState)
+    this.state = this.chain.reduce<TeamState>(reducer, initialState)
   }
 
   private create(options: NewTeamOptions) {
-    // set root context (excluding private keys, since this will be written into the public chain)
-    const rootContext = {
-      ...this.context,
-      user: {
-        ...this.context.user,
-        keys: redactSecrets(this.context.user.keys),
-        lockboxes: [
-          // lockbox.create()
-        ],
-      },
-    }
-    const payload = { name: options.name, rootContext } as RootLinkPayload
-    this.chain = chain.create(payload, this.context)
-    this.state = this.extractState()
+    // redact user's secret keys, since this will be written into the public chain
+    const user = redactUser(options.context.user)
+
+    // the team secret will never be stored in plaintext, only encrypted into individual lockboxes
+    const teamSecret = randomKey()
+    const teamKeys = deriveKeys(teamSecret)
+
+    // create root link
+    this.initializeChain(options.teamName, teamKeys, user)
+
+    // add root member
+    this.addMember(user, ['admin'])
+
+    // this.addLockbox()
   }
 
-  private load(options: ExistingTeamOptions) {
+  private loadChain(options: ExistingTeamOptions) {
     this.chain = options.source
-    this.state = this.extractState()
+    this.updateState()
   }
 
-  private validate() {
-    const validation = validate(this.chain)
-    if (!validation.isValid) throw validation.error
+  private initializeChain(
+    teamName: string,
+    teamKeys: KeysetWithSecrets,
+    foundingMember: User
+  ) {
+    const publicKeys = redactKeys(teamKeys)
+    const payload: RootPayload = {
+      teamName,
+      publicKeys,
+      foundingMember,
+    }
+    this.chain = []
+    this.dispatch({ type: linkType.ROOT, payload })
+  }
+
+  private addMember(user: User, roles: string[] = []) {
+    const payload: AddMemberPayload = { user, roles }
+    this.dispatch({ type: linkType.ADD_MEMBER, payload })
+  }
+
+  private addMemberRole() {}
+
+  private dispatch(link: PartialLinkBody) {
+    this.chain = chain.append(this.chain, link, this.context)
+    // update state
+    this.updateState()
   }
 }
