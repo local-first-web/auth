f47a639adcbab1e8bd87d9ade19a67c0427825f5
Herb Caudill <herb@devresults.com>
2020-05-09 08:19:51 +0200

    lockbox/create.ts:

--- src/lockbox/create.ts
+++ src/lockbox/create.ts
@@ -1,25 +1,30 @@
-﻿import { UserWithSecrets, User } from 'user'
-import { Payload } from 'lib/types'
-import { asymmetric } from 'lib'
+﻿import { hasSecrets, redactKeys } from 'keys'
+import { asymmetric, Payload } from '../lib'
+import { User, UserWithSecrets } from '../user'
 import { Lockbox } from './types'
 
 /** Creates a new lockbox that can be opened using sender's public key and recipient's private key. */
 export const create = (args: {
   scope?: string
   sender: UserWithSecrets
-  recipient: User
+  recipient: User | UserWithSecrets
   secret: Payload
 }): Lockbox => {
   const { sender, recipient, secret, scope } = args
+
+  const recipientPublicKeys = hasSecrets(recipient.keys)
+    ? redactKeys(recipient.keys)
+    : recipient.keys
+
   return {
     scope,
     sender: sender.name,
     senderPublicKey: sender.keys.asymmetric.publicKey,
     recipient: recipient.name,
-    recipientPublicKey: recipient.keys.encryption,
+    recipientPublicKey: recipientPublicKeys.encryption,
     encryptedSecret: asymmetric.encrypt(
       secret,
-      recipient.keys.encryption,
+      recipientPublicKeys.encryption,
       sender.keys.asymmetric.secretKey
     ),
   }
