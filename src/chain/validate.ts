6e11be55d00210a3e6163e266f315729a8d8c514
Herb Caudill <herb@devresults.com>
2020-05-13 14:55:12 +0200

    chain/validate.ts: refactor to map of validators

--- src/chain/validate.ts
+++ src/chain/validate.ts
@@ -9,17 +9,14 @@ import { baseLinkType } from './baseLinkType'
  */
 export const validate = (chain: SignatureChain): ValidationResult => {
   const initialValue = { isValid: true } as ValidResult
-  return chain.reduce(
-    compose([validateRoot, validateSequence, validateHash, validateSignatures]),
-    initialValue
-  )
+  return chain.reduce(compose(validators), initialValue)
 }
 
 /**
- * Returns a reducer function that runs all validators on each link in a signature chain.
- * @param validators An array of validator functions
+ * Returns a single reducer function that runs all validators.
+ * @param validators A map of validators
  */
-const compose = (validators: Validator[]) => (
+const compose = (validators: ValidatorSet) => (
   result: ValidationResult,
   currentLink: SignedLink,
   i: number,
@@ -29,7 +26,8 @@ const compose = (validators: Validator[]) => (
   if (result.isValid === false) return result as InvalidResult
 
   const prevLink = i === 0 ? undefined : chain[i - 1]
-  for (const validator of validators)
+  for (const key in validators) {
+    const validator = validators[key]
     try {
       const result = validator(currentLink, prevLink)
       if (result.isValid === false) return result
@@ -40,6 +38,7 @@ const compose = (validators: Validator[]) => (
         error: { message: e.message, index: i, details: e },
       } as InvalidResult
     }
+  }
 
   // no validators failed
   return { isValid: true } as ValidResult
@@ -52,81 +51,89 @@ type Validator = (
   prevLink?: SignedLink
 ) => ValidationResult
 
-/** Does this link contain a hash of the previous link?  */
-const validateHash: Validator = (currentLink, prevLink) => {
-  const isFirstLink = prevLink === undefined
-  if (isFirstLink) return { isValid: true } // nothing to validate on first link
-  const expected = hashLink(prevLink!)
-  const actual = currentLink.body.prev
-  return actual === expected
-    ? { isValid: true }
-    : {
+const validators: ValidatorSet = {
+  /** Does this link contain a hash of the previous link?  */
+  validateHash: (currentLink, prevLink) => {
+    const isFirstLink = prevLink === undefined
+    if (isFirstLink) return { isValid: true } // nothing to validate on first link
+    const expected = hashLink(prevLink!)
+    const actual = currentLink.body.prev
+    return actual === expected
+      ? { isValid: true }
+      : {
+          isValid: false,
+          error: new ValidationError(
+            'Hash does not match previous link',
+            currentLink.body.index,
+            { actual, expected }
+          ),
+        }
+  },
+
+  /** If this is a root link, is it the first link in the chain? */
+  validateRoot: (currentLink, prevLink) => {
+    const { type } = currentLink.body
+    const isRoot = type === baseLinkType.ROOT
+    const isFirstLink = prevLink === undefined
+
+    // both should be true, or both should be false
+    if (isRoot === isFirstLink) return { isValid: true }
+    else {
+      const message = isRoot
+        ? // has type ROOT but isn't first
+          'The root link must be the first link in the signature chain.'
+        : // is first but doesn't have type ROOT
+          'The first link in the signature chain must be the root link. '
+      return {
         isValid: false,
-        error: new ValidationError(
-          'Hash does not match previous link',
-          currentLink.body.index,
-          { actual, expected }
-        ),
+        error: new ValidationError(message, currentLink.body.index),
       }
-}
-
-/** If this is a root link, is it the first link in the chain? */
-const validateRoot: Validator = (currentLink, prevLink) => {
-  const { type } = currentLink.body
-  const isRoot = type === baseLinkType.ROOT
-  const isFirstLink = prevLink === undefined
-
-  // both should be true, or both should be false
-  if (isRoot === isFirstLink) return { isValid: true }
-  else {
-    const message = isRoot
-      ? // has type ROOT but isn't first
-        'The root link must be the first link in the signature chain.'
-      : // is first but doesn't have type ROOT
-        'The first link in the signature chain must be the root link. '
-    return {
-      isValid: false,
-      error: new ValidationError(message, currentLink.body.index),
     }
-  }
+  },
+
+  /** Does this link's signature check out? */
+  validateSignatures: currentLink => {
+    const signedMessage = {
+      payload: currentLink.body,
+      signature: currentLink.signed.signature,
+      publicKey: currentLink.signed.key,
+    }
+    return signatures.verify(signedMessage)
+      ? { isValid: true }
+      : {
+          isValid: false,
+          error: new ValidationError(
+            'Signature is not valid',
+            currentLink.body.index,
+            signedMessage
+          ),
+        }
+  },
+
+  /** Is this link's index consecutive to the previous link's index? */
+  validateSequence: (currentLink, prevLink) => {
+    const isFirstLink = prevLink === undefined
+    const expected = isFirstLink
+      ? 0 // first link should have index 0
+      : (prevLink!.body.index || 0) + 1 // other links should increment previous by one
+    const index = currentLink.body.index
+    return index === expected
+      ? { isValid: true }
+      : {
+          isValid: false,
+          error: new ValidationError(
+            'Index is not consecutive',
+            currentLink.body.index,
+            { actual: index, expected }
+          ),
+        }
+  },
 }
 
-/** Does this link's signature check out? */
-const validateSignatures: Validator = currentLink => {
-  const signedMessage = {
-    payload: currentLink.body,
-    signature: currentLink.signed.signature,
-    publicKey: currentLink.signed.key,
-  }
-  return signatures.verify(signedMessage)
-    ? { isValid: true }
-    : {
-        isValid: false,
-        error: new ValidationError(
-          'Signature is not valid',
-          currentLink.body.index,
-          signedMessage
-        ),
-      }
-}
+// TYPES
 
-/** Is this link's index consecutive to the previous link's index? */
-const validateSequence: Validator = (currentLink, prevLink) => {
-  const isFirstLink = prevLink === undefined
-  const expected = isFirstLink
-    ? 0 // first link should have index 0
-    : (prevLink!.body.index || 0) + 1 // other links should increment previous by one
-  const index = currentLink.body.index
-  return index === expected
-    ? { isValid: true }
-    : {
-        isValid: false,
-        error: new ValidationError(
-          'Index is not consecutive',
-          currentLink.body.index,
-          { actual: index, expected }
-        ),
-      }
+export type ValidatorSet = {
+  [key: string]: Validator
 }
 
 export interface InvalidResult {
