b447c524104c519824b61ce0d7c6b97cc2cc085c
Herb Caudill <herb@devresults.com>
2020-05-13 14:57:13 +0200

    team/team.test.ts:

--- src/team/team.test.ts
+++ src/team/team.test.ts
@@ -1,6 +1,7 @@
-import { ContextWithSecrets, DeviceType } from '../context'
-import { deriveKeys, randomKey, redactKeys } from '../keys'
-import { User, UserWithSecrets } from '../user'
+import { Client, ContextWithSecrets, Device, DeviceType } from '../context'
+import { deriveKeys, randomKey } from '../keys'
+import { ADMIN } from '../role'
+import { redactUser, UserWithSecrets } from '../user'
 import { Team } from './team'
 
 describe('Team', () => {
@@ -8,28 +9,26 @@ describe('Team', () => {
     localStorage.clear()
   })
 
-  const alice: UserWithSecrets = {
-    userName: 'alice',
-    keys: deriveKeys(randomKey()),
-  }
-  const bob: User = {
-    userName: 'bob',
-    keys: redactKeys(deriveKeys(randomKey())),
+  const makeUser = (userName: string): UserWithSecrets => {
+    const keys = deriveKeys(randomKey())
+    return { userName, keys }
   }
 
-  const setup = () => {
-    const context: ContextWithSecrets = {
-      user: alice,
-      device: {
-        name: 'windows laptop',
-        type: DeviceType.laptop,
-      },
-      client: {
-        name: 'test',
-        version: '0',
-      },
-    }
+  const alice = makeUser('alice')
+  const bob = makeUser('bob')
+  const charlie = makeUser('charlie')
 
+  const device: Device = {
+    name: 'windows laptop',
+    type: DeviceType.laptop,
+  }
+  const client: Client = {
+    name: 'test',
+    version: '0',
+  }
+
+  const setup = (user = alice) => {
+    const context: ContextWithSecrets = { user, device, client }
     const team = new Team({ teamName: 'Spies Я Us', context })
     return { team, context }
   }
@@ -53,7 +52,7 @@ describe('Team', () => {
       const tamperedChain = savedChain.replace(/alice/gi, 'eve')
       const restoreTampered = () =>
         new Team({ source: JSON.parse(tamperedChain), context })
-      expect(restoreTampered).toThrow('Signature is not valid')
+      expect(restoreTampered).toThrow(/signature(.*)not valid/i)
     })
   })
 
@@ -68,62 +67,133 @@ describe('Team', () => {
     it('throws when asked for a nonexistent member', () => {
       const { team } = setup()
       const findNonexistent = () => team.members('ned')
-      expect(findNonexistent).toThrow('Member ned was not found')
+      expect(findNonexistent).toThrow(/member(.*)not found/i)
     })
 
     it('adds a member', () => {
       const { team } = setup()
-      team.add(bob)
+      team.add(redactUser(bob))
       expect(team.members().length).toBe(2)
       expect(team.members('bob').userName).toBe('bob')
+    })
+
+    it('does not add a member that is already present', () => {
+      const { team } = setup()
+      team.add(redactUser(bob))
+
+      // try adding bob again
+      const addBobAgain = () => team.add(redactUser(bob))
+      expect(addBobAgain).toThrow(/member(.*)already exists/i)
     })
 
     it('removes a member', () => {
       const { team } = setup()
 
-      team.add(bob)
+      team.add(redactUser(bob))
       expect(team.has('bob')).toBe(true)
 
       team.remove('bob')
       expect(team.has('bob')).toBe(false)
     })
 
+    it.todo('does not remove a member that is not already present')
+
     it.todo('rotates keys after removing a member')
     it.todo('adds a device')
     it.todo('removes a device')
     it.todo('rotates keys after removing a device')
-  })
 
-  describe('invitations', () => {
-    it.todo('generates an invitation')
-    it.todo('accepts valid proof of invitation')
-    it.todo('rejects invalid proof of invitation')
-    it.todo('only allows an invitation to be used once ')
-    it.todo('does not allow another admin to hijack an invitation')
+    it.todo('automatically resolves concurrent edits')
   })
 
   describe('roles', () => {
     it('alice is admin', () => {
       const { team } = setup()
-      const _alice = team.members('alice')
-      expect(_alice.hasRole('admin')).toBe(true)
+      expect(team.memberIsAdmin('alice')).toBe(true)
     })
 
     it('bob is not admin', () => {
       const { team } = setup()
-      team.add(bob)
-      const _bob = team.members('bob')
-      expect(_bob.hasRole('admin')).toBe(false)
+      team.add(redactUser(bob))
+      expect(team.memberIsAdmin('bob')).toBe(false)
     })
 
-    it('does not allow a non-admin to add a member', () => {})
+    it('allows an admin other than alice to add a member', () => {
+      // get alice's team in JSON form
+      const getTeamJson = () => {
+        const { team } = setup()
+        team.add(redactUser(bob), [ADMIN]) // bob is an admin
+        return team.save()
+      }
 
-    it.todo('does not allow a non-admin to remove a member')
+      // rehydrate the team on bob's device
+      const source = JSON.parse(getTeamJson())
+      const context: ContextWithSecrets = { user: bob, device, client }
+      const team = new Team({ source, context })
+
+      // bob tries to add a user
+      const addUser = () => team.add(redactUser(charlie))
+      // bob is allowed because he is an admin
+      expect(addUser).not.toThrow()
+    })
+
+    it('does not allow a non-admin to add a member', () => {
+      // get alice's team in JSON form
+      const getTeamJson = () => {
+        const { team } = setup()
+        team.add(redactUser(bob), []) // bob is not an admin
+        return team.save()
+      }
+
+      // rehydrate the team on bob's device
+      const source = JSON.parse(getTeamJson())
+      const context: ContextWithSecrets = { user: bob, device, client }
+      const team = new Team({ source, context })
+
+      // bob tries to add charlie as a member
+      const addUser = () => team.add(redactUser(charlie))
+      // bob can't because bob is not an admin
+      expect(addUser).toThrow()
+    })
+
+    it('does not allow a non-admin to remove a member', () => {
+      // get alice's team in JSON form
+      const getTeamJson = () => {
+        const { team } = setup()
+        team.add(redactUser(bob), []) // bob is not an admin
+        team.add(redactUser(charlie), [])
+        return team.save()
+      }
+
+      // rehydrate the team on bob's device
+      const source = JSON.parse(getTeamJson())
+      const context: ContextWithSecrets = { user: bob, device, client }
+      const team = new Team({ source, context })
+
+      // bob tries to remove charlie
+      const remove = () => team.add(redactUser(charlie))
+      // bob can't because bob is not an admin
+      expect(remove).toThrow()
+    })
 
     it.todo('adds a role')
     it.todo('adds a member to a role')
     it.todo('removes a member from a role')
     it.todo('rotates keys after removing a member from a role')
+  })
+
+  describe('invitations', () => {
+    // alice issues the invitation (sends code via whatsapp)
+    // bob presents invitation (clicks link)
+    // presents? accepts?
+    // charlie (not an admin) consummates the invitation
+    // consummates? completes? accepts?
+
+    it.todo('generates an invitation')
+    it.todo('accepts valid proof of invitation')
+    it.todo('rejects invalid proof of invitation')
+    it.todo('only allows an invitation to be used once ')
+    it.todo('does not allow another admin to hijack an invitation')
   })
 
   describe('crypto', () => {
