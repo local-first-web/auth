2099b3b68cd7ecbc9d2be79fc1301ea6528fefae
Herb Caudill <herb@devresults.com>
2020-05-13 14:56:46 +0200

    team/teamState.ts: create

---
+++ src/team/teamState.ts
@@ -0,0 +1,10 @@
+﻿import { Context } from '../context'
+import { Member } from '../member'
+import { Role } from '../role'
+
+export interface TeamState {
+  teamName: string
+  rootContext?: Context
+  members: Member[]
+  roles: Role[]
+}
