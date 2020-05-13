6bc7b99a453854e31988ba43545afb6acd4ed9f0
Herb Caudill <herb@devresults.com>
2020-05-13 14:56:56 +0200

    team/team.ts: refactor to use selectors

--- src/team/team.ts
+++ src/team/team.ts
@@ -1,13 +1,7 @@
 ﻿import { EventEmitter } from 'events'
 import { chain, PartialLinkBody, SignatureChain, validate } from '../chain'
 import { ContextWithSecrets } from '../context'
-import {
-  deriveKeys,
-  KeysetWithSecrets,
-  PublicKeyset,
-  randomKey,
-  redactKeys,
-} from '../keys'
+import { deriveKeys, KeysetWithSecrets, randomKey, redactKeys } from '../keys'
 import { Member } from '../member'
 import { redactUser, User } from '../user'
 import { reducer } from './reducer'
@@ -17,14 +11,12 @@ import {
   exists,
   linkType,
   NewTeamOptions,
+  RevokeMemberPayload,
   RootPayload,
   TeamOptions,
-  TeamState,
-  RevokeMemberPayload,
-  RevokeRolePayload,
-  AddRolePayload,
-  AddMemberRolePayload,
 } from './types'
+import { TeamState } from './teamState'
+import * as selectors from './selectors'
 
 export class Team extends EventEmitter {
   constructor(options: TeamOptions) {
@@ -47,65 +39,43 @@ export class Team extends EventEmitter {
   }
 
   public has = (userName: string) => {
-    return this.state.members.find(m => m.userName === userName) !== undefined
+    return selectors.hasMember(this.state, userName)
   }
 
   public add = (user: User, roles: string[] = []) => {
-    const { userName } = user
-    if (this.has(userName)) throw new Error(`Member ${userName} already exists`)
+    // TODO these error checks belong on the chain
+    // const { userName } = user
+    // if (this.has(userName)) throw new Error(`Member ${userName} already exists`)
     const payload: AddMemberPayload = { user, roles }
     this.dispatch({ type: linkType.ADD_MEMBER, payload })
   }
 
-  public invite = (userName: string) => {}
-
   public remove = (userName: string) => {
-    if (!this.has(userName))
-      throw new Error(`Member ${userName} does not exist`)
+    // TODO these error checks belong on the chain
+    // look up the user to ensure it exists
+    // const user = this.members(userName)
     const payload: RevokeMemberPayload = { userName }
     this.dispatch({ type: linkType.REVOKE_MEMBER, payload })
   }
 
-  public members(): WrappedMember[]
-  public members(userName: string): WrappedMember
+  public members(): Member[]
+  public members(userName: string): Member
 
-  public members(name?: string): WrappedMember | WrappedMember[] {
-    const wrap = (member: Member): WrappedMember => {
-      const { userName } = member
-      return {
-        ...member,
+  public members(userName?: string): Member | Member[] {
+    if (userName === undefined) return this.state.members
+    else return selectors.getMember(this.state, userName)
+  }
 
-        addRole: (roleName: string) => {
-          const payload = { userName, roleName } as AddMemberRolePayload
-          this.dispatch({ type: linkType.ADD_MEMBER_ROLE, payload })
-        },
+  public memberHasRole(userName: string, role: string) {
+    return selectors.memberHasRole(this.state, userName, role)
+  }
 
-        removeRole: (roleName: string) => {
-          const payload = { userName, roleName } as RevokeRolePayload
-          this.dispatch({ type: linkType.REVOKE_MEMBER_ROLE, payload })
-        },
+  public memberIsAdmin(userName: string) {
+    return selectors.memberIsAdmin(this.state, userName)
+  }
 
-        hasRole: (role: string) => member.roles.includes(role),
-
-        permissions: () => {
-          // TODO
-          return []
-        },
-
-        hasPermission: (permission: string) => {
-          // TODO
-          return false
-        },
-      }
-    }
-
-    if (name === undefined) {
-      return this.state.members.map(wrap)
-    } else {
-      const result = this.state.members.find(m => m.userName === name)
-      if (!result) throw new Error(`Member ${name} was not found`)
-      return wrap(result)
-    }
+  public invite = (userName: string) => {
+    // TODO
   }
 
   public roles = {
@@ -115,7 +85,7 @@ export class Team extends EventEmitter {
 
     add: (roleName: string) => {
       if (this.roles.has(roleName))
-        throw new Error(`Role ${roleName} already exists`)
+        throw new Error(`A role called '${roleName}' already exists`)
     },
 
     remove: (roleName: string) => {},
@@ -159,9 +129,6 @@ export class Team extends EventEmitter {
     // create root link
     this.initializeChain(options.teamName, teamKeys, user)
 
-    // add root member
-    this.add(user, ['admin'])
-
     // this.addLockbox()
   }
 
@@ -185,17 +152,9 @@ export class Team extends EventEmitter {
     this.dispatch({ type: linkType.ROOT, payload })
   }
 
-  private dispatch(link: PartialLinkBody) {
+  public dispatch(link: PartialLinkBody) {
     this.chain = chain.append(this.chain, link, this.context)
     // update state
     this.updateState()
   }
-}
-
-interface WrappedMember extends Member {
-  addRole(role: string): void
-  removeRole(role: string): void
-  hasRole(role: string): boolean
-  permissions(): string[]
-  hasPermission(permission: string): boolean
 }
