55d18072925b087e393a8fd693c65b3b265e0c5c
Herb Caudill <herb@devresults.com>
2020-05-14 16:59:32 +0200

    team/constants.ts: create

---
+++ src/team/constants.ts
@@ -0,0 +1,10 @@
+﻿import { TeamState } from './teamState'
+import { ADMIN_ROLE } from '../role'
+
+export const initialState: TeamState = {
+  teamName: '',
+  members: [],
+  roles: [ADMIN_ROLE],
+}
+
+export const ALL = 'ALL'
