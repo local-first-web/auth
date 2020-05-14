691cf756fc667551b4c094a14cc7ba077bd0f8e8
Herb Caudill <herb@devresults.com>
2020-05-14 16:06:48 +0200

    team/validate.ts:

--- src/team/validate.ts
+++ src/team/validate.ts
@@ -1,44 +1,73 @@
-﻿import { SignedLink, ValidationError } from '../chain'
+﻿import { SignedLink, ValidationError, ValidationResult } from '../chain'
 import * as selectors from './selectors'
 import { TeamState } from './teamState'
-import { linkType } from './types'
-import { ValidationResult } from '../chain'
+import {
+  linkType,
+  TeamStateValidatorSet,
+  ValidationArgs,
+  TeamStateValidator,
+} from './types'
 
-const VALID = { isValid: true } as ValidationResult
-
-export const validate = (prevState: TeamState, link: SignedLink) => {
+export const validate: TeamStateValidator = (...args: ValidationArgs) => {
   for (const key in validators) {
     const validator = validators[key]
-    const validation = validator(prevState, link)
+    const validation = validator(...args)
     if (!validation.isValid) return validation
   }
   return VALID
 }
 
-type TeamStateValidator = (
-  prevState: TeamState,
-  link: SignedLink
-) => ValidationResult
+const validators: TeamStateValidatorSet = {
+  /** check that the user who made these changes was admin at the time */
+  mustBeAdmin: (...args) => {
+    const [prevState, link] = args
 
-/** check that the user who made these changes was admin at the time */
-
-const validators: { [key: string]: TeamStateValidator } = {
-  validateAdminUser: (prevState, link) => {
-    const { type, context, index } = link.body
-
-    if (type !== linkType.ROOT) {
-      const { userName } = context.user
+    // At root stage, there are no members
+    if (link.body.type !== linkType.ROOT) {
+      const { userName } = link.body.context.user
       const isntAdmin = !selectors.memberIsAdmin(prevState, userName)
       if (isntAdmin)
-        return {
-          isValid: false,
-          error: new ValidationError(
-            `Invalid signature chain: member '${userName}' is not an admin at this time`,
-            index,
-            { prevState, link }
-          ),
-        }
+        return fail(
+          `Member '${userName}' is not an admin at this time`,
+          ...args
+        )
+    }
+    return VALID
+  },
+
+  cantAddExistingMember: (...args) => {
+    const [prevState, link] = args
+    if (link.body.type === linkType.ADD_MEMBER) {
+      const { userName } = link.body.payload.user
+      if (selectors.hasMember(prevState, userName))
+        return fail(
+          `Can't add member; there is already a member called '${userName}'`,
+          ...args
+        )
+    }
+    return VALID
+  },
+
+  cantRemoveNonexistentMember: (...args) => {
+    const [prevState, link] = args
+    if (link.body.type === linkType.REVOKE_MEMBER) {
+      const { userName } = link.body.payload
+      if (!selectors.hasMember(prevState, userName))
+        return fail(
+          `Can't remove member; there is no member called '${userName}'`,
+          ...args
+        )
     }
     return VALID
   },
 }
+
+const fail = (message: string, prevState: TeamState, link: SignedLink) => {
+  const { index } = link.body
+  return {
+    isValid: false,
+    error: new ValidationError(message, index, { prevState, link }),
+  }
+}
+
+const VALID = { isValid: true } as ValidationResult
