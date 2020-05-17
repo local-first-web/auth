c48a352532ed7c24c3009c95ecb3b3e0f3e46c3f
Herb Caudill <herb@devresults.com>
2020-05-17 18:14:38 +0200

    lib/keyToString.ts: create

---
+++ src/lib/keyToString.ts
@@ -0,0 +1,10 @@
+﻿import * as utf8 from '@stablelib/utf8'
+import * as base64 from '@stablelib/base64'
+import { Key } from '/lib'
+
+export const keyToString = (x: Key, encoding: Encoding = 'base64') => {
+  const encode = encoding === 'base64' ? base64.encode : utf8.decode
+  return typeof x === 'string' ? x : encode(x)
+}
+
+type Encoding = 'base64' | 'utf8'
