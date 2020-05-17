b2266ced251d65c0dc1b72436522287181ca6b79
Herb Caudill <herb@devresults.com>
2020-05-17 13:24:31 +0200

    move team/team.ts to team/index.ts

--- src/team/index.ts
+++ src/team/index.ts
@@ -1,2 +1,185 @@
-export * from '/team/team'
 export * from '/team/types'
+
+import { EventEmitter } from 'events'
+import { chain, SignatureChain, validate } from '/chain'
+import { ContextWithSecrets } from '/context'
+import { deriveKeys, KeysetWithSecrets, randomKey, redactKeys } from '/keys'
+import { Member } from '/member'
+import { Role, ADMIN } from '/role'
+import { ALL, initialState } from '/team/constants'
+import { reducer } from '/team/reducer'
+import * as selectors from '/team/selectors'
+import { TeamState } from '/team/teamState'
+import {
+  isNew,
+  NewTeamOptions,
+  OldTeamOptions,
+  TeamAction,
+  TeamLink,
+  TeamOptions,
+} from '/team/types'
+import { redactUser, User } from '/user'
+import { lockbox } from '/lockbox'
+
+export class Team extends EventEmitter {
+  constructor(options: TeamOptions) {
+    super()
+
+    this.context = options.context
+
+    if (isNew(options)) this.create(options)
+    else this.loadChain(options)
+  }
+
+  // PUBLIC API
+
+  public get teamName() {
+    return this.state.teamName
+  }
+
+  public get lockboxes() {
+    return this.state.lockboxes
+  }
+
+  public save = () => JSON.stringify(this.chain)
+
+  // READ METHODS
+  // to read from team state, we rely on selectors
+
+  public has = (userName: string) => selectors.hasMember(this.state, userName)
+
+  public members(): Member[] // overload: all members
+  public members(userName: string): Member // overload: one member
+  public members(userName: string = ALL): Member | Member[] {
+    return userName === ALL
+      ? this.state.members
+      : selectors.getMember(this.state, userName)
+  }
+
+  public memberHasRole(userName: string, role: string) {
+    return selectors.memberHasRole(this.state, userName, role)
+  }
+
+  public memberIsAdmin(userName: string) {
+    return selectors.memberIsAdmin(this.state, userName)
+  }
+
+  public hasRole = (roleName: string) => selectors.hasRole(this.state, roleName)
+
+  public roles(): Role[] // overload: all roles
+  public roles(roleName: string): Role // overload: one role
+  public roles(roleName: string = ALL): Role | Role[] {
+    return roleName === ALL
+      ? this.state.roles
+      : selectors.getRole(this.state, roleName)
+  }
+
+  // WRITE METHODS
+  // to mutate team state, we dispatch changes to the chain
+  // and then run the chain through the reducer to recalculate team state
+
+  public add = (user: User, roles: string[] = []) => {
+    return this.dispatch({
+      type: 'ADD_MEMBER',
+      payload: { user, roles },
+    })
+  }
+
+  public remove = (userName: string) => {
+    return this.dispatch({
+      type: 'REVOKE_MEMBER',
+      payload: { userName },
+    })
+  }
+
+  public addRole = (role: Role) => {
+    return this.dispatch({
+      type: 'ADD_ROLE',
+      payload: role,
+    })
+  }
+
+  public removeRole = (roleName: string) => {
+    return this.dispatch({
+      type: 'REVOKE_ROLE',
+      payload: { roleName },
+    })
+  }
+
+  public addMemberRole = (userName: string, roleName: string) => {
+    return this.dispatch({
+      type: 'ADD_MEMBER_ROLE',
+      payload: { userName, roleName },
+    })
+  }
+
+  public removeMemberRole = (userName: string, roleName: string) => {
+    return this.dispatch({
+      type: 'REVOKE_MEMBER_ROLE',
+      payload: { userName, roleName },
+    })
+  }
+
+  public invite = (userName: string) => {}
+  public accept = (userName: string) => {}
+
+  // private properties
+
+  private chain: SignatureChain<TeamLink>
+  private context: ContextWithSecrets
+  private state: TeamState
+
+  // private functions
+
+  /** Create a new team with the current user as founding member. */
+  private create(options: NewTeamOptions) {
+    const { teamName } = options
+
+    // the team admin secret will never be stored in plaintext, only encrypted into individual lockboxes
+    const adminSecret = randomKey()
+    const adminKeys = deriveKeys(adminSecret)
+    const publicKeys = redactKeys(adminKeys)
+
+    // redact user's secret keys, since this will be written into the public chain
+    const foundingMember = redactUser(options.context.user)
+
+    // create a lockbox containing the admin secret for the founding member
+    const foundingMemberLockbox = lockbox.create({
+      scope: ADMIN,
+      senderKeys: adminKeys,
+      recipient: foundingMember,
+      secret: adminSecret,
+    })
+    const lockboxes = [foundingMemberLockbox]
+
+    // create root link
+    this.chain = []
+    this.dispatch({
+      type: 'ROOT',
+      payload: { teamName, publicKeys, foundingMember, lockboxes },
+    })
+  }
+
+  /** Load a team from a serialized chain. */
+  private loadChain(options: OldTeamOptions) {
+    this.chain = options.source
+    this.updateState()
+  }
+
+  public dispatch(link: TeamAction) {
+    this.chain = chain.append(this.chain, link, this.context)
+    this.updateState()
+  }
+
+  /** Validate the chain's integrity. (This does not enforce team rules - that is done in the
+   * reducer as it progresses through each link. ) */
+  private validateChain() {
+    const validation = validate(this.chain)
+    if (!validation.isValid) throw validation.error
+  }
+
+  private updateState = () => {
+    this.validateChain()
+    this.state = this.chain.reduce(reducer, initialState)
+  }
+}
