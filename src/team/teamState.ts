f4740996f523fd8a5508067a3dcd823b0411ee61
Herb Caudill <herb@devresults.com>
2020-05-17 13:24:03 +0200

    team/teamState.ts: add lockboxes

--- src/team/teamState.ts
+++ src/team/teamState.ts
@@ -1,10 +1,16 @@
 ﻿import { Context } from '/context'
 import { Member } from '/member'
 import { Role } from '/role'
+import { Lockbox } from '/lockbox'
+
+export interface TeamLockboxMap {
+  [userName: string]: Lockbox[]
+}
 
 export interface TeamState {
   teamName: string
   rootContext?: Context
   members: Member[]
   roles: Role[]
+  lockboxes: TeamLockboxMap
 }
