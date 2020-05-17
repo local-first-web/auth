4fa1fd2444fc0de0a30fa47faddb5ebd497c6b9d
Herb Caudill <herb@devresults.com>
2020-05-17 18:15:56 +0200

    team/teamState.ts:

--- src/team/teamState.ts
+++ src/team/teamState.ts
@@ -2,9 +2,18 @@
 import { Member } from '/member'
 import { Role } from '/role'
 import { Lockbox } from '/lockbox'
+import { KeysetWithSecrets } from '/keys'
 
 export interface TeamLockboxMap {
-  [userName: string]: Lockbox[]
+  [userName: string]: UserLockboxMap
+}
+
+export interface UserLockboxMap {
+  [publicKey: string]: Lockbox[]
+}
+
+export interface KeysetMap {
+  [scope: string]: KeysetWithSecrets
 }
 
 export interface TeamState {
