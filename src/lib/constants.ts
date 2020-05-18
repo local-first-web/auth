97f493f0a1923317f5ec5ec568fca1e045561c7e
Herb Caudill <herb@devresults.com>
2020-05-18 08:32:55 +0200

    lib/constants.ts:

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,4 +3,7 @@
   EncryptionAsymmetric = 'ENCRYPTION_ASYMMETRIC',
   EncryptionSymmetric = 'ENCRYPTION_SYMMETRIC',
   LinkToPrevious = 'LINK_TO_PREVIOUS',
+  InvitationIdFromInvitationKey = 'INVITATION_ID_FROM_INVITATION_KEY',
+  SigningPairFromFromInvitationKey = 'SIGNING_PAIR_FROM_FROM_INVITATION_KEY',
+  Invitation = 'INVITATION',
 }
