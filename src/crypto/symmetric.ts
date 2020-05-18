2a26ff09354342813348ba94eddd3d4b654783e3
Herb Caudill <herb@devresults.com>
2020-05-18 09:28:57 +0200

    src:

--- src/crypto/symmetric.ts
+++ src/crypto/symmetric.ts
@@ -3,7 +3,7 @@ import * as utf8 from '@stablelib/utf8'
 import nacl from 'tweetnacl'
 import { newNonce, nonceLength } from '/crypto/nonce'
 import { stretch } from '/crypto/stretch'
-import { Key, keyToBytes, Message, payloadToBytes } from '/lib'
+import { Key, keyToBytes, Payload, payloadToBytes } from '/lib'
 
 // These are straightforward implementations of NaCl crypto functions, accepting and returning
 // base64 strings rather than byte arrays. The symmetric `encrypt` and `decrypt` functions can take
@@ -13,15 +13,15 @@ import { Key, keyToBytes, Message, payloadToBytes } from '/lib'
 export const symmetric = {
   /**
    * Symmetrically encrypts a string of text (or utf8-encoded byte array).
-   * @param message The plaintext (or utf8-encoded byte array) to encrypt
+   * @param payload The plaintext (or utf8-encoded byte array) to encrypt
    * @param password The password to use as a seed for an encryption key
    * @returns The encrypted data. The first 24 characters are the nonce; the rest is the encrypted message.
    * @see symmetric.decrypt
    */
-  encrypt: (message: Message, password: Key) => {
+  encrypt: (payload: Payload, password: Key) => {
     const key = stretch(password)
     const nonce = newNonce()
-    const messageBytes = payloadToBytes(message)
+    const messageBytes = payloadToBytes(payload)
     const box = nacl.secretbox(messageBytes, nonce, key)
     const cipherBytes = new Uint8Array(nonceLength + box.length)
 
