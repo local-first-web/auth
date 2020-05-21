93177c022c4d525977fe0b2bc45e2124c02c1e00
Herb Caudill <herb@devresults.com>
2020-05-21 14:26:45 +0200

    team/transforms/revokeRole.ts: create

---
+++ src/team/transforms/revokeRole.ts
@@ -0,0 +1,6 @@
+﻿import { Transform } from './index'
+
+export const revokeRole = (roleName: string): Transform => (state) => ({
+  ...state,
+  roles: state.roles.filter((role) => role.roleName !== roleName),
+})
