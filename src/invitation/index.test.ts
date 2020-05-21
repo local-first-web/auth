231bb19adb890d41092131bac8f8d9faf7d5e298
Herb Caudill <herb@devresults.com>
2020-05-21 09:23:38 +0200

    invitation/index.test.ts:

--- src/invitation/index.test.ts
+++ src/invitation/index.test.ts
@@ -1,13 +1,34 @@
 import { create } from '/invitation/create'
+import { accept } from '/invitation/accept'
+import { validate } from '/invitation/validate'
 import { deriveKeys, randomKey } from '/keys'
 
 describe('invitations', () => {
   const teamKeys = deriveKeys(randomKey())
 
-  test('newInvitation', () => {
-    const invitation = create(teamKeys, 'bob')
+  test('create', () => {
+    const { secretKey, invitation } = create(teamKeys, 'bob')
+    expect(secretKey).toHaveLength(16)
     expect(invitation).toHaveProperty('id')
     expect(invitation.id).toHaveLength(15)
-    expect(invitation).toHaveProperty('body')
+    expect(invitation).toHaveProperty('encryptedPayload')
+  })
+
+  test('validate', () => {
+    // Alice creates invitation. She sends the secret key to Bob, and records the invitation on the
+    // team's signature chain.
+    const { secretKey, invitation } = create(teamKeys, 'bob')
+
+    // Bob accepts invitation and obtains a credential proving that he was invited.
+    const proofOfInvitation = accept(secretKey, 'bob')
+
+    // Bob shows up to join the team & sees Charlie. Bob shows Charlie his proof of invitation, and
+    // Charlie checks it against the invitation that Alice posted on the signature chain.
+    const validation = () => {
+      const validationResult = validate(proofOfInvitation, invitation, teamKeys)
+      if (!validationResult.isValid) throw validationResult.error
+    }
+
+    expect(validation).not.toThrow()
   })
 })
