7bb4f99f360025e50ea447e3af090b14e2b03db8
Herb Caudill <herb@devresults.com>
2020-05-21 11:14:09 +0200

    invitation/create.ts: refactor out secretKey generation

--- src/invitation/create.ts
+++ src/invitation/create.ts
@@ -1,36 +1,35 @@
-﻿import nacl from 'tweetnacl'
-import { symmetric } from '/crypto'
-import { KeysetWithSecrets, deriveKeys } from '/keys'
-import { base30 } from '/lib'
-import { InvitationAndSecretKey, InvitationPayload, Invitation } from '/invitation/types'
+﻿import { symmetric } from '/crypto'
 import { deriveId } from '/invitation/deriveId'
+import { Invitation, InvitationPayload } from '/invitation/types'
+import { deriveKeys, KeysetWithSecrets } from '/keys'
 
-const IKEY_LENGTH = 16
+export const IKEY_LENGTH = 16
 
-// Implements a modified version of Keybase's Seitan Token v2 exchange specification. Step numbers
+// A loosely adapted implementation of Keybase's Seitan Token v2 exchange protocol. Step numbers
 // refer to this page: http://keybase.io/docs/teams/seitan_v2
 
 /**
- * Returns a secret key to give Bob, as well as an invitation to public post on the team's signature chain.
+ * Returns an an invitation to public post on the team's signature chain.
  * @param teamKeys The global team keys (Alice obtains these from the appropriate lockbox)
  * @param userName Bob's username
+ *
+ * @param secretKey A randomly generated secret (Step 1a) to be passed to Bob via a side channel
+ * @see newSecretKey
  */
-export const create = (teamKeys: KeysetWithSecrets, userName: string): InvitationAndSecretKey => {
+export const create = (
+  teamKeys: KeysetWithSecrets,
+  userName: string,
+  secretKey: string
+): Invitation => {
   const generation = teamKeys.generation || 0
-
-  // ## Step 1a
-  // The invitation key is a randomly generated secret that is sent to Bob via a pre-authenticated
-  // channel (e.g. WhatsApp). If Alice is inviting Bob, only Alice and Bob will ever have this key.
-  // (Keybase docs refer to this as `iKey`)
-  const secretKey = base30.encode(nacl.randomBytes(IKEY_LENGTH))
 
   // ## Step 1b, 1c
   // Stretch the key and hash it to obtain an invitation ID (Keybase docs: `inviteID`)
   const id = deriveId(secretKey)
 
   // ## Step 1d
-  // Generate a signing keypair for Bob to use to verify that he knows the iKey. This keypair is
-  // also derived from the iKey, so Bob can generate it independently.
+  // Generate a signing keypair for Bob to use to verify that he knows the secret key. This keypair
+  // is also derived from the secret key, so Bob can generate it independently.
   const { publicKey } = deriveKeys(secretKey).signature
 
   // ## Step 2a
@@ -42,5 +41,5 @@ export const create = (teamKeys: KeysetWithSecrets, userName: string): Invitatio
   // ## Step 2b
   // We put it all together to create the invitation.
   const invitation: Invitation = { id, encryptedPayload: body, generation }
-  return { secretKey, invitation }
+  return invitation
 }
