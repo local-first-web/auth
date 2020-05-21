4f9be6d3427892360547b4096b7b118643befb44
Herb Caudill <herb@devresults.com>
2020-05-21 09:25:18 +0200

    invitation/admit.ts: create

---
+++ src/invitation/admit.ts
@@ -0,0 +1,13 @@
+﻿import { Invitation, ProofOfInvitation } from './types'
+import { KeysetWithSecrets } from '/keys'
+import { validate } from './validate'
+
+export const admit = (
+  proof: ProofOfInvitation,
+  invitation: Invitation,
+  teamKeys: KeysetWithSecrets
+) => {
+  const validation = validate(proof, invitation, teamKeys)
+  if (!validation.isValid) throw validation.error
+  // TODO: Add this person to the team
+}
