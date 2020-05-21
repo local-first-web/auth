e62057541fa7bbfbab67c57660bf2c24612a4181
Herb Caudill <herb@devresults.com>
2020-05-21 16:28:02 +0200

    lockbox/types.ts:

--- src/lockbox/types.ts
+++ src/lockbox/types.ts
@@ -4,14 +4,14 @@ export interface Lockbox {
   /**  e.g. team, role, user */
   scope: string
 
-  /** the public half of the sender's asymmetric encryption keys */
-  senderPublicKey: Key
-
-  /** the recipient's username */
-  recipient: string
+  /** the public half of the asymmetric keys used to encrypt the lockbox */
+  publicKey: Key
 
   /** the public half of the recipient's asymmetric encryption keys */
   recipientPublicKey: Key
+
+  /** the recipient's username */
+  recipient: string
 
   /** the secret, encrypted using `asymmetric.encrypt` */
   encryptedSecret: Base64
