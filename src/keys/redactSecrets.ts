d3efaeec9e0dc59d8030e4935d94ae65f960ec89
Herb Caudill <herb@devresults.com>
2020-05-27 11:09:09 +0200

    keys/redactSecrets.ts:

--- src/keys/redactSecrets.ts
+++ src/keys/redactSecrets.ts
@@ -1,12 +1,15 @@
-﻿import { KeysetWithSecrets, PublicKeyset } from '/keys/types'
+﻿import { KeysetWithSecrets, PublicKeyset, hasSecrets } from '/keys/types'
 
 /**
- * Takes a keyset that includes secret keys, and returns just the public keys.
+ * Takes a keyset that includes secret keys, and returns just the public keys. (If the keyset provided doesn't include secret keys, it is returned as-is)
  * @param keys a keyset of the form `{signature: {secretKey, publicKey}, encryption: {secretKey, publicKey}}`
  * @returns a set of public keys of the form `{signature (publicKey), encryption (publicKey)}`
  */
-export const redactKeys = (keys: KeysetWithSecrets): PublicKeyset => ({
-  encryption: keys.encryption.publicKey,
-  signature: keys.signature.publicKey,
-  generation: keys.generation,
-})
+export const redactKeys = (keys: KeysetWithSecrets): PublicKeyset =>
+  !hasSecrets(keys)
+    ? keys
+    : {
+        ...keys,
+        encryption: keys.encryption.publicKey,
+        signature: keys.signature.publicKey,
+      }
