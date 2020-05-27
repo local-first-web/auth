472a1e421db5ab893c7f9eab992ce42eb27ca216
Herb Caudill <herb@devresults.com>
2020-05-27 11:08:52 +0200

    lockbox/getId.ts: create

---
+++ src/lockbox/getId.ts
@@ -0,0 +1,9 @@
+﻿import { KeysetScope } from '/keys'
+
+export const getId = (scope: KeysetScope, name?: string) =>
+  name ? `${scope}::${name}` : scope.toString()
+
+export const getScopeAndName = (id: string) => {
+  const [scope, name] = id.split('::')
+  return { scope, name }
+}
