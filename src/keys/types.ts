b49be3c191be11fdd4519f4ca9be5a4837d18610
Herb Caudill <herb@devresults.com>
2020-05-27 11:08:43 +0200

    keys/types.ts:

--- src/keys/types.ts
+++ src/keys/types.ts
@@ -1,16 +1,28 @@
-﻿import { Base64Keypair, Base64 } from '/lib'
+﻿import { Base64, Base64Keypair } from '/lib'
 
-export interface KeysetWithSecrets {
+export enum KeysetScope {
+  TEAM = 'TEAM',
+  ROLE = 'ROLE',
+  MEMBER = 'MEMBER',
+  DEVICE = 'DEVICE',
+  EPHEMERAL = 'EPHEMERAL',
+}
+
+export interface KeyMetadata {
+  scope: KeysetScope /**  e.g. ROLE, MEMBER, DEVICE */
+  name: string /**  e.g. 'admin', 'alice', 'dell-laptop' */
+  generation: number /** generation index (0-based) - incremented each time this key is rotated */
+}
+
+export interface KeysetWithSecrets extends KeyMetadata {
   seed: Base64
   signature: Base64Keypair
   encryption: Base64Keypair
-  generation?: number
 }
 
-export interface PublicKeyset {
+export interface PublicKeyset extends KeyMetadata {
   signature: Base64 // = signature.publicKey
   encryption: Base64 // = encryption.publicKey
-  generation?: number
 }
 
 export const hasSecrets = (keys: PublicKeyset | KeysetWithSecrets): keys is KeysetWithSecrets => {
