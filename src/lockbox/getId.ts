f49e3b46f594e6073cf4cfc45d7cbcba304cdbbd
Herb Caudill <herb@devresults.com>
2020-05-28 15:34:07 +0200

    delete unused

--- src/lockbox/getId.ts
+++
@@ -1,9 +0,0 @@
-﻿import { KeysetScope } from '/keys'
-
-export const getId = (scope: KeysetScope, name?: string) =>
-  name ? `${scope}::${name}` : scope.toString()
-
-export const getScopeAndName = (id: string) => {
-  const [scope, name] = id.split('::')
-  return { scope, name }
-}
