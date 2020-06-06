233e4a825f19ddafee79c26d8eba187d3fa6ba75
Herb Caudill <herb@devresults.com>
2020-06-06 11:45:24 +0200

    lockbox.rotate to own file

---
+++ src/lockbox/rotate.ts
@@ -0,0 +1,19 @@
+﻿import { KeysetWithSecrets } from '/keys'
+import { Lockbox } from '/lockbox/types'
+import { create } from './create'
+export const rotate = (oldLockbox: Lockbox, newContents: KeysetWithSecrets): Lockbox => {
+  // make sure new keys have the same scope and name as the old lockbox
+  if (
+    newContents.scope !== oldLockbox.contents.scope ||
+    newContents.name !== oldLockbox.contents.name
+  )
+    throw new Error('The scope and name of the new contents must match those of the old lockbox')
+  // increment the keys' generation index
+  const prevGeneration = oldLockbox.contents.generation || 0
+  const contents = {
+    ...newContents,
+    generation: prevGeneration + 1,
+  }
+  const newLockbox = create(contents, oldLockbox.recipient)
+  return newLockbox
+}
