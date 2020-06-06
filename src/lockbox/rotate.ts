27c754f4743e1a5f3dce8f010327e972505afb63
Herb Caudill <herb@devresults.com>
2020-06-06 12:01:43 +0200

    lockbox/rotate.ts:

--- src/lockbox/rotate.ts
+++ src/lockbox/rotate.ts
@@ -1,22 +1,19 @@
-﻿import { KeysetWithSecrets } from '/keys'
+﻿import { KeysWithSecrets } from '/keys'
 import { Lockbox } from '/lockbox/types'
 import { create } from './create'
 
-export const rotate = (oldLockbox: Lockbox, newContents: KeysetWithSecrets): Lockbox => {
+export const rotate = (oldLockbox: Lockbox, contents: KeysWithSecrets): Lockbox => {
   // make sure new keys have the same scope and name as the old lockbox
-  if (
-    newContents.scope !== oldLockbox.contents.scope ||
-    newContents.name !== oldLockbox.contents.name
-  )
+  if (contents.scope !== oldLockbox.contents.scope || contents.name !== oldLockbox.contents.name)
     throw new Error('The scope and name of the new contents must match those of the old lockbox')
 
   // increment the keys' generation index
   const prevGeneration = oldLockbox.contents.generation || 0
-  const contents = {
-    ...newContents,
+  const newContents = {
+    ...contents,
     generation: prevGeneration + 1,
   }
 
-  const newLockbox = create(contents, oldLockbox.recipient)
+  const newLockbox = create(newContents, oldLockbox.recipient)
   return newLockbox
 }
