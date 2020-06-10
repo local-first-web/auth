27d91837966700df688e84ed6a69f38203840ebe
Herb Caudill <herb@devresults.com>
2020-06-10 13:22:32 +0200

    message/index.ts:

--- src/message/index.ts
+++ src/message/index.ts
@@ -1,7 +1,12 @@
 ﻿import { KeyScope } from '/keyset'
 import { Base64 } from '/lib'
 
-export type Message = ClaimIdentityMessage | ChallengeIdentityMessage | ProveIdentityMessage
+export type Message =
+  | ClaimIdentityMessage
+  | ChallengeIdentityMessage
+  | ProveIdentityMessage
+  | typeof ACCEPT_IDENTITY
+  | typeof REJECT_IDENTITY
 
 export type ClaimIdentityMessage = {
   type: 'CLAIM_IDENTITY'
@@ -24,5 +29,8 @@ type ChallengePayload = KeyScope & {
   nonce: Base64
   timestamp: UnixTimestamp
 }
+
+export const ACCEPT_IDENTITY = { type: 'ACCEPT_IDENTITY' }
+export const REJECT_IDENTITY = { type: 'REJECT_IDENTITY' }
 
 export type UnixTimestamp = number
