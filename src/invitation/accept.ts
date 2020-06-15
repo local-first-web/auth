04d53d7f4fcd317f319486d868284f060f7d5d09
Herb Caudill <herb@devresults.com>
2020-06-15 10:51:24 +0200

    invitation/accept.ts: normalize

--- src/invitation/accept.ts
+++ src/invitation/accept.ts
@@ -1,20 +1,21 @@
 ﻿import { signatures } from '/crypto'
 import { deriveId } from '/invitation/deriveId'
+import { normalize } from '/invitation/normalize'
 import { ProofOfInvitation } from '/invitation/types'
-import { EPHEMERAL_SCOPE, KeyType, create } from '/keyset'
+import { create, EPHEMERAL_SCOPE } from '/keyset'
 import { Member } from '/member'
 
-export const acceptInvitation = (key: string, member: Member): ProofOfInvitation => {
+export const acceptInvitation = (secretKey: string, member: Member): ProofOfInvitation => {
+  secretKey = normalize(secretKey)
+
   // ## Step 4
 
-  // TODO filter out any non-base30 characters
-
   // Bob independently derives the invitation id
-  const id = deriveId(key)
+  const id = deriveId(secretKey)
 
   // Bob uses the one-time signature keys to sign a message consisting of his username and public keys, and the invitation id
-  const { secretKey } = create(EPHEMERAL_SCOPE, key).signature
-  const signature = signatures.sign({ member, id }, secretKey)
+  const signatureKeys = create(EPHEMERAL_SCOPE, secretKey).signature
+  const signature = signatures.sign({ member, id }, signatureKeys.secretKey)
 
   // The invitation id and the signature will be shown to an existing team member as proof that Bob
   // knows the secret invitation key
