bde7305232720326cd1567a834d6d14364206071
Herb Caudill <herb@devresults.com>
2020-06-23 13:22:30 +0200

    invitation/accept.ts: include device when accepting

--- src/invitation/accept.ts
+++ src/invitation/accept.ts
@@ -2,10 +2,19 @@
 import { deriveId } from '/invitation/deriveId'
 import { normalize } from '/invitation/normalize'
 import { ProofOfInvitation } from '/invitation/types'
-import { create, EPHEMERAL_SCOPE } from '/keyset'
-import { Member } from '/member'
+import { create, EPHEMERAL_SCOPE, redact as redactKeyset } from '/keyset'
+import { redact as redactUser, User } from '/user'
 
-export const acceptInvitation = (secretKey: string, member: Member): ProofOfInvitation => {
+export const acceptInvitation = (secretKey: string, user: User): ProofOfInvitation => {
+  // don't leak secrets to the signature chain
+  const member = redactUser(user)
+  const device = {
+    userName: user.userName,
+    name: user.device.name,
+    type: user.device.type,
+    keys: redactKeyset(user.device.keys),
+  }
+
   secretKey = normalize(secretKey)
 
   // ## Step 4
@@ -15,9 +24,10 @@ export const acceptInvitation = (secretKey: string, member: Member): ProofOfInvi
 
   // Bob uses the one-time signature keys to sign a message consisting of his username and public keys, and the invitation id
   const signatureKeys = create(EPHEMERAL_SCOPE, secretKey).signature
-  const signature = signatures.sign({ member, id }, signatureKeys.secretKey)
+  const payload = { id, member, device }
+  const signature = signatures.sign(payload, signatureKeys.secretKey)
 
   // The invitation id and the signature will be shown to an existing team member as proof that Bob
-  // knows the secret invitation key
-  return { id, member, signature }
+  // knows the secret invitation key. His user public keys and device public keys will be added to the signature chain.
+  return { ...payload, signature }
 }
