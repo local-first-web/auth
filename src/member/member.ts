72fc48f46357c8e614653284099a177a067b4f66
Herb Caudill <herb@devresults.com>
2020-06-23 13:28:48 +0200

    remove comment

--- src/member/member.ts
+++ src/member/member.ts
@@ -4,10 +4,6 @@ export interface Member {
   userName: string
   keys: PublicKeyset
   roles?: string[]
-
-  /** All the member's keysets over their history of key rotation.
-   * The index of the keyset in the array corresponds to the
-   * key generation: previousKeys[0] is generation 0, etc.
-   */
+  devices?: string[]
   keyHistory?: PublicKeyset[]
 }
