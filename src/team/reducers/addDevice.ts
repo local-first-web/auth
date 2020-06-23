9929bf101c09a0f2bf8580d72345e5604ff3b1b1
Herb Caudill <herb@devresults.com>
2020-06-23 13:26:46 +0200

    team/reducers/addDevice.ts: create

---
+++ src/team/reducers/addDevice.ts
@@ -0,0 +1,17 @@
+﻿import { Reducer } from '/team/reducers/index'
+
+export const addDevice = (userName: string): Reducer => state => {
+  return {
+    ...state,
+    members: state.members.map(member => {
+      if (member.userName === userName) {
+        const { devices } = member
+
+        return {
+          ...member,
+          devices,
+        }
+      } else return member
+    }),
+  }
+}
