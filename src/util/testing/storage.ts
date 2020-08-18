f132d7bb6a3e1187ab7decace7a560f4deb71e6b
Herb Caudill <herb@devresults.com>
2020-08-18 14:22:49 +0200

    util/testing/storage.ts: create

---
+++ src/util/testing/storage.ts
@@ -0,0 +1,15 @@
+import { LocalUserContext } from '/context'
+import * as teams from '/team'
+import { Team } from '/team'
+
+/** A simple little storage emulator */
+export const storage = {
+  contents: undefined as string | undefined,
+  save: (team: Team) => {
+    storage.contents = team.save()
+  },
+  load: (context: LocalUserContext) => {
+    if (storage.contents === undefined) throw new Error('need to save before you can load')
+    return teams.load(JSON.parse(storage.contents), context)
+  },
+}
