d579ab4a4013f78d53d82ad68258a2a989f4243f
Herb Caudill <herb@devresults.com>
2020-08-18 14:24:37 +0200

    identity/accept.ts: create

---
+++ src/identity/accept.ts
@@ -0,0 +1,22 @@
+﻿﻿import { randomKey, KeysetWithSecrets, PublicKeyset } from '/keyset'
+import { AcceptIdentityMessage, ClaimIdentityMessage } from '/message'
+import { Base64 } from '/util'
+import { asymmetric } from '/crypto'
+
+export const accept = ({ seed, peerKeys, userKeys }: AcceptArgs): AcceptIdentityMessage => {
+  // encrypt the seed for our peer
+  const peerPublicKey = peerKeys.encryption
+  const mySecretKey = userKeys.encryption.secretKey
+  const encryptedSeed = asymmetric.encrypt(seed, peerPublicKey, mySecretKey)
+
+  return {
+    type: 'ACCEPT_IDENTITY',
+    payload: { encryptedSeed },
+  }
+}
+
+interface AcceptArgs {
+  seed: Base64
+  peerKeys: PublicKeyset
+  userKeys: KeysetWithSecrets
+}
