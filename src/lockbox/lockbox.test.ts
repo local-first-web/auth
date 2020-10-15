a07f64631797fe447f083492df54214e004c4920
HerbCaudill <herb@devresults.com>
2020-10-15 17:39:02 +0200

    lockbox/lockbox.test.ts: add tests

--- src/lockbox/lockbox.test.ts
+++ src/lockbox/lockbox.test.ts
@@ -3,10 +3,11 @@ import * as keyset from '/keyset'
 import { create } from '/lockbox/create'
 import { open } from '/lockbox/open'
 import { ADMIN } from '/role'
-import { bob } from '/util/testing'
+import { bob, eve, MANAGERS } from '/util/testing'
+import { rotate } from './rotate'
 
 describe('lockbox', () => {
-  it('round trip', () => {
+  it('can be opened by the intended recipient', () => {
     const adminKeys = keyset.create({ type: KeyType.ROLE, name: ADMIN })
 
     // Alice creates a lockbox for Bob containing the admin keys
@@ -15,5 +16,26 @@ describe('lockbox', () => {
     // Bob opens the lockbox and gets the admin keys
     const keys = open(lockbox, bob.keys)
     expect(keys).toEqual(adminKeys)
+  })
+  it(`can't be opened by anyone else`, () => {
+    const adminKeys = keyset.create({ type: KeyType.ROLE, name: ADMIN })
+
+    // Alice creates a lockbox for Bob containing the admin keys
+    const lockbox = create(adminKeys, bob.keys)
+
+    // Eve tries to open the lockbox but can't
+    const eveTriesToOpen = () => open(lockbox, eve.keys)
+    expect(eveTriesToOpen).toThrow()
+  })
+
+  it(`can only be rotated with a keyset of the same type`, () => {
+    const adminKeys = keyset.create({ type: KeyType.ROLE, name: ADMIN })
+
+    // Alice creates a lockbox for Bob containing the admin keys
+    const lockbox = create(adminKeys, bob.keys)
+
+    const newKeys = keyset.create({ type: KeyType.ROLE, name: MANAGERS })
+    const tryToRotate = () => rotate(lockbox, newKeys)
+    expect(tryToRotate).toThrow()
   })
 })
