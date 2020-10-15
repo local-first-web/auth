b184c43b0ddcb98a57fd70af6872c8c0c4ad4530
HerbCaudill <herb@devresults.com>
2020-10-15 17:19:44 +0200

    wallaby.js: figured out 'autoDetect' to extend auto config

---
+++ wallaby.js
@@ -0,0 +1,9 @@
+﻿module.exports = wallaby => ({
+  autoDetect: true,
+  runMode: 'onsave',
+  slowTestThreshold: 1000,
+  lowCoverageThreshold: 99, // 99%
+  hints: {
+    ignoreCoverageForFile: /ignore file coverage/,
+  },
+})
