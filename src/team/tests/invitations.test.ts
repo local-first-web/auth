aa7bc930c322f4acb442ea8becb5b2322805cf46
HerbCaudill <herb@devresults.com>
2020-10-15 15:12:21 +0200

    team/tests/invitations.test.ts: add device test

--- src/team/tests/invitations.test.ts
+++ src/team/tests/invitations.test.ts
@@ -1,5 +1,8 @@
-import { acceptMemberInvitation, ProofOfInvitation } from '/invitation'
-import { redact } from '/user'
+import { DeviceType, DeviceWithSecrets, getDeviceId, redact as redactDevice } from '/device'
+import { acceptDeviceInvitation, acceptMemberInvitation, ProofOfInvitation } from '/invitation'
+import * as keyset from '/keyset'
+import { KeyType } from '/keyset'
+import { redact as redactUser } from '/user'
 import {
   alicesContext,
   bob,
@@ -8,8 +11,10 @@ import {
   defaultContext,
   eve,
   newTeam,
+  storage,
 } from '/util/testing'
-import { storage } from '/util/testing'
+
+const { DEVICE } = KeyType
 
 describe('Team', () => {
   beforeEach(() => {
@@ -38,7 +43,7 @@ describe('Team', () => {
       const { secretKey } = alicesTeam.invite('bob')
 
       // 👨‍🦲 Bob accepts the invitation
-      const proofOfInvitation = acceptMemberInvitation(secretKey, bob)
+      const proofOfInvitation = acceptMemberInvitation(secretKey, redactUser(bob))
 
       // 👨‍🦲 Bob shows 👩🏾 Alice his proof of invitation, and she lets him in
       alicesTeam.admit(proofOfInvitation)
@@ -54,13 +59,13 @@ describe('Team', () => {
       const { secretKey } = alicesTeam.invite('bob')
 
       // 👨‍🦲 Bob accepts the invitation
-      const proofOfInvitation = acceptMemberInvitation(secretKey, bob)
+      const proofOfInvitation = acceptMemberInvitation(secretKey, redactUser(bob))
 
       // 🦹‍♀️ Eve intercepts the invitation and tries to use it by swapping out Bob's info for hers
       const forgedProofOfInvitation: ProofOfInvitation = {
         ...proofOfInvitation,
         type: 'MEMBER',
-        payload: redact(eve),
+        payload: redactUser(eve),
       }
 
       // 🦹‍♀️ Eve shows 👩🏾 Alice her fake proof of invitation
@@ -79,7 +84,7 @@ describe('Team', () => {
       storage.save(alicesTeam)
 
       // 👳‍♂️ Charlie accepts the invitation
-      const proofOfInvitation = acceptMemberInvitation(secretKey, charlie)
+      const proofOfInvitation = acceptMemberInvitation(secretKey, redactUser(charlie))
 
       // Alice is no longer around, but 👨‍🦲 Bob is online
       const bobsTeam = storage.load(bobsContext)
@@ -97,6 +102,30 @@ describe('Team', () => {
       storage.save(bobsTeam)
       alicesTeam = storage.load(alicesContext)
       expect(alicesTeam.has('charlie')).toBe(true)
+    })
+
+    it('creates and accepts an invitation for a device', () => {
+      const { team, context } = setup()
+
+      // Alice is on her laptop
+      expect(context.user.device.name).toBe(`alice's device`)
+
+      // Alice generates an invitation, which is stored on the team's signature chain
+      const device = { userName: 'alice', name: `alice's phone`, type: DeviceType.mobile }
+      const { secretKey } = team.inviteDevice(device)
+
+      // Alice gets the secret invitation key to her phone, perhaps by typing it in or by scanning a
+      // QR code. Alice's phone uses the secret key to generate proof of invitation
+      const deviceId = getDeviceId(device)
+      const deviceKeys = keyset.create({ type: DEVICE, name: deviceId })
+      const deviceWithSecrets: DeviceWithSecrets = { ...device, keys: deviceKeys }
+      const proofOfInvitation = acceptDeviceInvitation(secretKey, redactDevice(deviceWithSecrets))
+
+      // Alice's phone connects with her laptop and presents the proof
+      team.admitDevice(proofOfInvitation)
+
+      // Alice's phone is now listed on the signature chain
+      expect(team.members('alice').devices!.map(d => d.deviceId)).toContain(deviceId)
     })
   })
 })
