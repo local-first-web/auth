c60efca89d27318f4a11080057079b0c7c00dc18
HerbCaudill <herb@devresults.com>
2020-10-15 15:10:55 +0200

    team/selectors/membersInRole.ts: coverage

--- src/team/selectors/membersInRole.ts
+++ src/team/selectors/membersInRole.ts
@@ -2,6 +2,6 @@
 import { TeamState } from '/team/types'
 
 export const membersInRole = (state: TeamState, roleName: string) =>
-  state.members.filter(member => member.roles?.includes(roleName))
+  state.members.filter(member => member.roles && member.roles.includes(roleName))
 
 export const admins = (state: TeamState) => membersInRole(state, ADMIN)
