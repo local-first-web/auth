00a8934fad484a82fe095f820c26e9bab1dd830a
HerbCaudill <herb@devresults.com>
2020-10-15 15:12:11 +0200

    team/tests/devices.test.ts: move invitation test

--- src/team/tests/devices.test.ts
+++ src/team/tests/devices.test.ts
@@ -1,6 +1,11 @@
-﻿import { DeviceType } from '/device'
+﻿import { redact as redactDevice, DeviceType, DeviceWithSecrets, getDeviceId } from '/device'
+import { KeyType } from '/keyset'
 import { defaultContext, newTeam } from '/util/testing'
 import { storage } from '/util/testing'
+import * as keyset from '/keyset'
+import { acceptDeviceInvitation, newInvitationKey } from '/invitation'
+
+const { DEVICE } = KeyType
 
 describe('Team', () => {
   beforeEach(() => {
@@ -14,27 +19,6 @@ describe('Team', () => {
   })
 
   describe('devices', () => {
-    it('adds a device', () => {
-      const { team, context } = setup()
-
-      // Alice is on her laptop
-      expect(context.user.device.name).toBe(`alice's device`)
-
-      // Alice authorizes her phone from her laptop
-      const secretKey = team.inviteDevice({
-        userName: 'alice',
-        name: 'iPhone 11',
-        type: DeviceType.mobile,
-      })
-
-      // Alice sends the secret invitation key to the phone via a secure channel (e.g. by showing the phone a QR code)
-      // Alice's phone uses the secret key to generate proof of invitation
-
-      // Alice's phone connects with her laptop and presents the proof
-
-      // Alice's laptop adds the phone
-    })
-
     it('removes a device', () => {})
 
     it('rotates keys after removing a device', () => {})
