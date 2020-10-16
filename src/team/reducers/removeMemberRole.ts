05a322a76219f11a3142246df794ad67cbb2b658
HerbCaudill <herb@devresults.com>
2020-10-16 15:45:50 +0200

    team/reducers/removeMemberRole.ts:

--- src/team/reducers/removeMemberRole.ts
+++ src/team/reducers/removeMemberRole.ts
@@ -5,13 +5,15 @@ export const removeMemberRole = (userName: string, roleName: string): Reducer =>
   ...state,
 
   // remove this role from this member's list of roles
-  members: state.members.map(member => ({
-    ...member,
-    roles:
-      member.userName !== userName //
-        ? member.roles
-        : (member.roles || []).filter(r => r !== roleName),
-  })),
+  members: state.members.map(member => {
+    return {
+      ...member,
+      roles:
+        member.userName !== userName //
+          ? member.roles // leave other members' roles alone
+          : member.roles.filter(r => r !== roleName),
+    }
+  }),
 
   // remove any lockboxes this member has for this role
   lockboxes: state.lockboxes.filter(
