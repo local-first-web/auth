63e5ed5b09b8fe55d752005303a17043fa3e953c
HerbCaudill <herb@devresults.com>
2020-11-02 16:48:52 +0100

    graph/merge.ts:

--- src/graph/merge.ts
+++ src/graph/merge.ts
@@ -1,12 +1,13 @@
-﻿import { hashNode } from './hashNode'
+﻿import { clone } from './clone'
+import { hashNode } from './hashNode'
 import { MergeNode, SignatureGraph } from './types'
 
 export const merge = (a: SignatureGraph, b: SignatureGraph): SignatureGraph => {
   if (a.root !== b.root) throw new Error('Cannot merge two graphs with different roots')
 
-  if (a.head === b.head) return a // they're the same
-  if (a.nodes.has(b.head)) return a // a is ahead of b; fast forward
-  if (b.nodes.has(a.head)) return b // b is ahead of a; fast forward
+  if (a.head === b.head) return clone(a) // they're the same
+  if (a.nodes.has(b.head)) return clone(a) // a is ahead of b; fast forward
+  if (b.nodes.has(a.head)) return clone(b) // b is ahead of a; fast forward
 
   // create a new map containing all nodes from both graphs
   const nodes = mergeMaps(a.nodes, b.nodes)
@@ -14,12 +15,13 @@ export const merge = (a: SignatureGraph, b: SignatureGraph): SignatureGraph => {
   const root = a.root
 
   // create a merge node, which is just a pointer to each of the two heads
-  const mergeNode = { type: 'MERGE', payload: [a.head, b.head].sort() } as MergeNode
+  const body = [a.head, b.head].sort() // ensure deterministic order
+  const hash = hashNode(body)
+  const mergeNode = { type: 'MERGE', hash, body } as MergeNode
 
   // add this node as the new head
-  const head = hashNode(mergeNode)
-  nodes.set(head, mergeNode)
-
+  const head = hash
+  nodes.set(hash, mergeNode)
   return { root, head, nodes }
 }
 
