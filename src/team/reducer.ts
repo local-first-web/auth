a990118d11b50f1d166536b2230cf6f4f9109b65
HerbCaudill <herb@devresults.com>
2020-11-02 19:23:50 +0100

    team/reducer.ts:

--- src/team/reducer.ts
+++ src/team/reducer.ts
@@ -1,4 +1,5 @@
-﻿import { ADMIN } from '/role'
+﻿import { RootNode } from '/graph'
+import { ADMIN } from '/role'
 import {
   addDevice,
   addMember,
@@ -15,29 +16,29 @@ import {
   revokeInvitation,
   setTeamName,
 } from '/team/reducers'
-import { TeamAction, TeamLink, TeamState } from '/team/types'
+import { TeamAction, TeamNode, TeamState } from '/team/types'
 import { validate } from '/team/validate'
 
 /**
- * Each link has a `type` and a `payload`, just like a Redux action. So we can derive a `teamState`
- * from `teamChain`, by applying a Redux-style reducer to the array of links. The reducer runs on
- * each link in sequence, accumulating a team state.
+ * Each node has a `type` and a `payload`, just like a Redux action. So we can derive a `teamState`
+ * from `teamChain`, by applying a Redux-style reducer to the array of nodes. The reducer runs on
+ * each node in sequence, accumulating a team state.
  *
  * > *Note:* Keep in mind that this reducer is a pure function that acts on the publicly available
- * links in the signature chain, and must independently return the same result for every member. It
+ * nodes in the signature chain, and must independently return the same result for every member. It
  * knows nothing about the current user's context, and it does not have access to any secrets. Any
  * crypto operations using secret keys that *the current user has* must happen elsewhere.
  *
- * @param state The team state as of the previous link in the signature chain.
- * @param link The current link being processed.
+ * @param state The team state as of the previous node in the signature chain.
+ * @param node The current node being processed.
  */
-export const reducer = (state: TeamState, link: TeamLink) => {
-  // make sure this link can be applied to the previous state & doesn't put us in an invalid state
-  const validation = validate(state, link)
+export const reducer = (state: TeamState, node: TeamNode | RootNode) => {
+  // make sure this node can be applied to the previous state & doesn't put us in an invalid state
+  const validation = validate(state, node)
   if (!validation.isValid) throw validation.error
 
   // recast as TeamAction so we get type enforcement on payloads
-  const action: TeamAction = link.body
+  const action = node.body as TeamAction
 
   // get all transforms and compose them into a single function
   const applyTransforms = compose([
@@ -151,6 +152,6 @@ const getTransforms = (action: TeamAction): Reducer[] => {
 
     default:
       // @ts-ignore (should never get here)
-      throw new Error(`Unrecognized link type: ${action.type}`)
+      throw new Error(`Unrecognized node type: ${action.type}`)
   }
 }
