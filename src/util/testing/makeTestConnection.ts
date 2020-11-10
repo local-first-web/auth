5c063989fcced6c23099e022ade0da8ccb599a1c
HerbCaudill <herb@devresults.com>
2020-11-10 13:35:01 +0100

    util/testing/makeTestConnection.ts:

--- src/util/testing/makeTestConnection.ts
+++ src/util/testing/makeTestConnection.ts
@@ -1,15 +1,16 @@
 import { TestChannel } from './TestChannel'
-import { ConnectionContext, ConnectionEvent, ConnectionService } from '/connection'
+import { ConnectionContext, ConnectionService } from '/connection'
+import { ConnectionMessage } from '/message'
 
 export const joinTestChannel = (channel: TestChannel) => (
   id: string,
   context: ConnectionContext
 ) => {
   // hook up send
-  const sendMessage = (msg: ConnectionEvent) => channel.write(id, msg)
+  const sendMessage = (msg: ConnectionMessage) => channel.write(id, msg)
 
   // Instantiate the connection service
-  const connection = new ConnectionService({ ...context, sendMessage }).start()
+  const connection = new ConnectionService({ sendMessage, context }).start()
 
   // hook up receive
   channel.addListener('data', (senderId, msg) => {
