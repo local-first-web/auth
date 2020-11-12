7d8ecd739669c211cfc130f254fe23efb3eee9af
HerbCaudill <herb@devresults.com>
2020-11-12 17:18:13 +0100

    identity/claim.ts: remove

--- src/identity/claim.ts
+++
@@ -1,7 +0,0 @@
-﻿import { KeyScope } from '/keyset'
-import { ClaimIdentityMessage } from '/message'
-
-export const claim = (scope: KeyScope): ClaimIdentityMessage => ({
-  type: 'CLAIM_IDENTITY',
-  payload: scope,
-})
