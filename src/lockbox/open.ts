025b8075d1fe86c5d6877b30650f70d78a32528d
HerbCaudill <herb@devresults.com>
2020-11-16 11:45:28 +0100

    lockbox/open.ts:

--- src/lockbox/open.ts
+++ src/lockbox/open.ts
@@ -1,18 +1,20 @@
 ﻿import { asymmetric } from '@herbcaudill/crypto'
+import memoize from 'fast-memoize'
 import { KeysetWithSecrets } from '/keyset'
 import { Lockbox } from '/lockbox/types'
 
-//TODO: Memoize this
-export const open = (lockbox: Lockbox, decryptionKeys: KeysetWithSecrets): KeysetWithSecrets => {
-  const { encryptionKey, encryptedPayload } = lockbox
+export const open = memoize(
+  (lockbox: Lockbox, decryptionKeys: KeysetWithSecrets): KeysetWithSecrets => {
+    const { encryptionKey, encryptedPayload } = lockbox
 
-  const keys = JSON.parse(
-    asymmetric.decrypt({
-      cipher: encryptedPayload,
-      senderPublicKey: encryptionKey.publicKey,
-      recipientSecretKey: decryptionKeys.encryption.secretKey,
-    })
-  )
+    const keys = JSON.parse(
+      asymmetric.decrypt({
+        cipher: encryptedPayload,
+        senderPublicKey: encryptionKey.publicKey,
+        recipientSecretKey: decryptionKeys.encryption.secretKey,
+      })
+    )
 
-  return keys
-}
+    return keys
+  }
+)
