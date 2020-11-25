40a19c99afc011338c5373656687f2b802659db6
HerbCaudill <herb@devresults.com>
2020-11-25 13:51:48 +0100

    team/strongRemoveResolver.ts: create

---
+++ src/team/strongRemoveResolver.ts
@@ -0,0 +1,8 @@
+﻿import { arbitraryDeterministicSort } from '/chain/arbitraryDeterministicSort'
+import { Resolver } from '/chain/types'
+import { TeamAction } from '/team/types'
+
+export const strongRemoveResolver: Resolver<TeamAction> = (a = [], b = []) => {
+  const [_a, _b] = [a, b].sort(arbitraryDeterministicSort()) // ensure predictable order
+  return _a.concat(_b)
+}
