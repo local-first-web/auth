a6c693d22bab22dfe8bad9498a39e6fb3611ed15
HerbCaudill <herb@devresults.com>
2020-11-25 13:51:22 +0100

    chain/getSequence.test.ts: fix brittle setup

--- src/chain/getSequence.test.ts
+++ src/chain/getSequence.test.ts
@@ -1,12 +1,23 @@
 import { buildChain, findByPayload, getPayloads } from '/chain/testUtils'
 import { append, create, getSequence, Resolver, SignedLink } from '/chain'
 import { defaultContext } from '/util/testing'
+import { arbitraryDeterministicSort } from './arbitraryDeterministicSort'
+import { randomKey } from '@herbcaudill/crypto'
+
+const chaosMonkeyResolver: Resolver<any> = (a = [], b = []) => {
+  // change the hash key on each run, to ensure our tests aren't bound to one arbitrary sort
+  const hashKey = randomKey()
+  const [_a, _b] = [a, b].sort(arbitraryDeterministicSort(hashKey))
+  return _a.concat(_b)
+}
+
+const resolver = chaosMonkeyResolver
 
 describe('chains', () => {
   describe('getSequence', () => {
     test('upon creation', () => {
       var chain = create('a', defaultContext)
-      const sequence = getSequence({ chain })
+      const sequence = getSequence({ chain, resolver })
       expect(getPayloads(sequence)).toEqual(['a'])
     })
 
@@ -14,44 +25,77 @@ describe('chains', () => {
       var chain = create('a', defaultContext)
       chain = append(chain, { type: 'FOO', payload: 'b' }, defaultContext)
       chain = append(chain, { type: 'FOO', payload: 'c' }, defaultContext)
-      const sequence = getSequence({ chain })
+      const sequence = getSequence({ chain, resolver })
 
       const expected = 'a b c'
 
       expect(getPayloads(sequence)).toEqual(split(expected))
     })
 
-    /*
-                        ┌─→ e ─→ g ─┐
-      a ─→ b ─┬─→ c ─→ d ┴─→ f ───── * ── * ─→ o ── * ─→ n
-              ├─→ h ─→ i ─────────────────┘         │
-              └─→ j ─→ k ─→ l ──────────────────────┘
-
-    */
+    /*                      ┌─→ e ─→ g ─┐
+         a ─→ b ─┬─→ c ─→ d ┴─→ f ───── * ── * ─→ o ── * ─→ n
+                 ├─→ h ─→ i ─────────────────┘         │
+                 └─→ j ─→ k ─→ l ──────────────────────┘           */
 
     describe('complex chain', () => {
       const chain = buildChain()
 
       test('full sequence', () => {
-        const sequence = getSequence({ chain })
+        const sequence = getSequence({ chain, resolver })
 
-        const expected = 'a b   j k l   c d f e g   h i   o n'
+        const expected = [
+          'a b   j k l   c d f e g   h i   o n',
+          'a b   j k l   c d e g f   h i   o n',
 
-        expect(getPayloads(sequence)).toEqual(split(expected))
+          'a b   j k l   h i   c d f e g   o n',
+          'a b   j k l   h i   c d e g f   o n',
+
+          'a b   h i   c d f e g   j k l   o n',
+          'a b   h i   c d e g f   j k l   o n',
+
+          'a b   h i   c d f e g  o  j k l   n',
+          'a b   h i   c d e g f  o  j k l   n',
+
+          'a b   c d f e g   h i  o  j k l   n',
+          'a b   c d e g f   h i  o  j k l   n',
+
+          'a b   c d f e g   h i   j k l   o n',
+          'a b   c d e g f   h i   j k l   o n',
+        ].map(split)
+
+        expect(expected).toContainEqual(getPayloads(sequence))
       })
 
       test('root', () => {
         const b = findByPayload(chain, 'b')
-        const sequence = getSequence({ chain, root: b })
+        const sequence = getSequence({ chain, root: b, resolver })
 
-        const expected = 'b   j k l   c d f e g   h i   o n'
+        const expected = [
+          'b   j k l   c d f e g   h i   o n',
+          'b   j k l   c d e g f   h i   o n',
 
-        expect(getPayloads(sequence)).toEqual(split(expected))
+          'b   j k l   h i   c d f e g   o n',
+          'b   j k l   h i   c d e g f   o n',
+
+          'b   h i   c d f e g   j k l   o n',
+          'b   h i   c d e g f   j k l   o n',
+
+          'b   h i   c d f e g  o  j k l   n',
+          'b   h i   c d e g f  o  j k l   n',
+
+          'b   c d f e g   h i  o  j k l   n',
+          'b   c d e g f   h i  o  j k l   n',
+
+          'b   c d f e g   h i   j k l   o n',
+          'b   c d e g f   h i   j k l   o n',
+        ].map(split)
+
+        expect(expected).toContainEqual(getPayloads(sequence))
       })
 
       test('head', () => {
         const d = findByPayload(chain, 'd')
-        const sequence = getSequence({ chain, head: d })
+        const sequence = getSequence({ chain, head: d, resolver })
 
         const expected = 'a b c d'
 
@@ -61,7 +105,7 @@ describe('chains', () => {
       test('root & head', () => {
         const j = findByPayload(chain, 'j')
         const l = findByPayload(chain, 'l')
-        const sequence = getSequence({ chain, root: j, head: l })
+        const sequence = getSequence({ chain, root: j, head: l, resolver })
 
         const expected = 'j k l'
 
@@ -70,11 +114,14 @@ describe('chains', () => {
 
       test('root within a branch', () => {
         const c = findByPayload(chain, 'c')
-        const sequence = getSequence({ chain, root: c })
+        const sequence = getSequence({ chain, root: c, resolver })
 
-        const expected = 'c    d f e g   o n'
+        const expected = [
+          'c d   e g   f   o n', //
+          'c d   f   e g   o n',
+        ].map(split)
 
-        expect(getPayloads(sequence)).toEqual(split(expected))
+        expect(expected).toContainEqual(getPayloads(sequence))
       })
 
       test('custom resolver', () => {
