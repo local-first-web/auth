c149962eb88e009443f0803615216e8dd9b82614
HerbCaudill <herb@devresults.com>
2020-11-25 13:51:41 +0100

    chain/arbitraryDeterministicSort.ts: allow passing in hash key

--- src/chain/arbitraryDeterministicSort.ts
+++ src/chain/arbitraryDeterministicSort.ts
@@ -1,7 +1,7 @@
 import { hash } from '@herbcaudill/crypto'
 import { Link } from '/chain/types'
 
-export const arbitraryDeterministicSort = (a: Link<any>[], b: Link<any>[]) => {
-  const hashKey = 'DETERMINISTIC_SORT'
-  return hash(hashKey, a[0].body.payload) > hash(hashKey, b[0].body.payload) ? 1 : -1
-}
+export const arbitraryDeterministicSort = (hashKey = 'DETERMINISTIC_SORT') => (
+  a: Link<any>[],
+  b: Link<any>[]
+) => (hash(hashKey, a) > hash(hashKey, b) ? 1 : -1)
