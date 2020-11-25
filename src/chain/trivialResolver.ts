f8b974734aa51c78492b9de39baa157a0bf8fc99
HerbCaudill <herb@devresults.com>
2020-11-25 13:51:58 +0100

    chain/trivialResolver.ts:

--- src/chain/trivialResolver.ts
+++ src/chain/trivialResolver.ts
@@ -1,8 +1,8 @@
-import { Resolver } from '/chain/types'
+import { Action, Resolver } from '/chain/types'
 import { arbitraryDeterministicSort } from '/chain/arbitraryDeterministicSort'
 
 /// If no resolver is provided, we just concatenate the two sequences in an arbitrary but deterministic manner
-export const trivialResolver: Resolver = (a = [], b = []) => {
-  const [_a, _b] = [a, b].sort(arbitraryDeterministicSort) // ensure predictable order
+export const trivialResolver: Resolver<any> = (a = [], b = []) => {
+  const [_a, _b] = [a, b].sort(arbitraryDeterministicSort()) // ensure predictable order
   return _a.concat(_b)
 }
