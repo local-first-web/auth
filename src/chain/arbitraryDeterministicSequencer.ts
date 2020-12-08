f3ec6fa34a2ec5e92fcccf357f35d7e15c6b03c9
HerbCaudill <herb@devresults.com>
2020-12-08 14:20:01 +0100

    chain/arbitraryDeterministicSequencer.ts: create

---
+++ src/chain/arbitraryDeterministicSequencer.ts
@@ -0,0 +1,7 @@
+import { arbitraryDeterministicSort } from '/chain/arbitraryDeterministicSort'
+import { Sequencer } from '/chain/types'
+
+export const arbitraryDeterministicSequencer: Sequencer = (a, b) => {
+  const [_a, _b] = [a, b].sort(arbitraryDeterministicSort()) // ensure predictable order
+  return _a.concat(_b)
+}
