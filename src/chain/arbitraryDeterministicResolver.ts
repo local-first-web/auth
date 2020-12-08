b75c665de3fbea01da9f2896c0c8ca567bc5f342
HerbCaudill <herb@devresults.com>
2020-12-08 14:19:57 +0100

    chain/arbitraryDeterministicResolver.ts: remove

--- src/chain/arbitraryDeterministicResolver.ts
+++
@@ -1,8 +0,0 @@
-import { getCommonPredecessor } from './predecessors'
-import { arbitraryDeterministicSort } from '/chain/arbitraryDeterministicSort'
-import { Resolver } from '/chain/types'
-
-export const arbitraryDeterministicResolver: Resolver<any> = (a, b) => {
-  const [_a, _b] = [a, b].sort(arbitraryDeterministicSort()) // ensure predictable order
-  return _a.concat(_b)
-}
