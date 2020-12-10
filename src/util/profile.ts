33e027cb0bcc8184259e3bac62e4dd016db8d42a
HerbCaudill <herb@devresults.com>
2020-12-10 09:38:00 +0100

    util/profile.ts:

--- src/util/profile.ts
+++ src/util/profile.ts
@@ -3,9 +3,9 @@ import path from 'path'
 
 const profiler = require('v8-profiler-node8')
 
-export const profile = (fn: Function) => {
+export const profile = async (fn: Function) => {
   profiler.startProfiling()
-  fn()
+  await fn()
   const profile = profiler.stopProfiling()
   profile.export((err: any, result: any) => {
     const name = fn.name || 'anonymous'
