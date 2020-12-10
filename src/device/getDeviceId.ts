7029e4fa6938fc86645aa35d7926859f05fab1d0
HerbCaudill <herb@devresults.com>
2020-12-10 09:37:42 +0100

    device/getDeviceId.ts:

--- src/device/getDeviceId.ts
+++ src/device/getDeviceId.ts
@@ -5,5 +5,10 @@ import { HashPurpose } from '/util'
 /** The deviceId is a hash of the name and username; we use it to identify the device uniquely in
  *  public situations where we want to avoid leaking more info than necessary
  */
-export const getDeviceId = ({ name, userName }: DeviceInfo): Base64 => `${userName}:${name}`
-// base64.encode(hash(HashPurpose.DEVICE_ID, { name, userName }))
+export const getDeviceId = ({ deviceName, userName }: Omit<DeviceInfo, 'type'>): Base64 => {
+  const isTestEnvironment = process.env.NODE_ENV === 'test'
+
+  return isTestEnvironment
+    ? `${userName}:${deviceName}` // keep it human-readable when testing
+    : base64.encode(hash(HashPurpose.DEVICE_ID, { deviceName, userName }))
+}
