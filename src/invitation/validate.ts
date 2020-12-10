272df0ebc0221691befd629b5e4001c21e730163
HerbCaudill <herb@devresults.com>
2020-12-10 09:38:53 +0100

    invitation/validate.ts:

--- src/invitation/validate.ts
+++ src/invitation/validate.ts
@@ -2,32 +2,35 @@
 import { Invitation, InvitationBody, ProofOfInvitation } from '/invitation/types'
 import { KeysetWithSecrets } from '/keyset'
 import { VALID, ValidationResult } from '/util'
+import memoize from 'fast-memoize'
 
-export const validate = (
-  proof: ProofOfInvitation,
-  invitation: Invitation,
-  teamKeys: KeysetWithSecrets
-): ValidationResult => {
-  // Decrypt and parse invitation
-  const decryptedBody = symmetric.decrypt(invitation.encryptedBody, teamKeys.secretKey)
-  const invitationBody = JSON.parse(decryptedBody) as InvitationBody
+export const validate = memoize(
+  (
+    proof: ProofOfInvitation,
+    invitation: Invitation,
+    teamKeys: KeysetWithSecrets
+  ): ValidationResult => {
+    // Decrypt and parse invitation
+    const decryptedBody = symmetric.decrypt(invitation.encryptedBody, teamKeys.secretKey)
+    const invitationBody = JSON.parse(decryptedBody) as InvitationBody
 
-  const details = { invitationBody, proof }
+    const details = { invitationBody, proof }
 
-  // Check that IDs and user names from proof match invitation
-  const { id, userName } = proof
-  if (id !== invitation.id) return fail(`IDs don't match`, details)
-  if (userName !== invitationBody.userName) return fail(`User names don't match`, details)
+    // Check that IDs and user names from proof match invitation
+    const { id, userName } = proof
+    if (id !== invitation.id) return fail(`IDs don't match`, details)
+    if (userName !== invitationBody.userName) return fail(`User names don't match`, details)
 
-  // Check signature on proof
-  const { signature } = proof
-  const payload = { id, userName }
-  const { publicKey } = invitationBody
-  if (!signatures.verify({ payload, signature, publicKey }))
-    return fail(`Signature provided is not valid`, details)
+    // Check signature on proof
+    const { signature } = proof
+    const payload = { id, userName }
+    const { publicKey } = invitationBody
+    if (!signatures.verify({ payload, signature, publicKey }))
+      return fail(`Signature provided is not valid`, details)
 
-  return VALID
-}
+    return VALID
+  }
+)
 
 const fail = (message: string, details: any) =>
   ({
