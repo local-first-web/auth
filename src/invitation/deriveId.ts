700c0f1c12d4a8e21815ff3f05373114e8c7bdf1
HerbCaudill <herb@devresults.com>
2021-01-28 12:56:37 +0100

    invitation/deriveId.ts:

--- src/invitation/deriveId.ts
+++ src/invitation/deriveId.ts
@@ -1,8 +1,6 @@
 ﻿import { hash, stretch, base64 } from '@herbcaudill/crypto'
 import { HashPurpose } from '/util'
 
-const { INVITATION } = HashPurpose
-
 export function deriveId(secretKey: string, userName: string) {
   // ## Step 1b
   // The iKey is stretched using `scrypt` to discourage brute-force attacks (docs refer to this as
@@ -13,5 +11,5 @@ export function deriveId(secretKey: string, userName: string) {
   // The invitation id is derived from the stretched iKey, so Bob can generate it independently.
   // This will be visible in the signature chain and serves to uniquely identify the invitation.
   // (Keybase docs: `inviteID`)
-  return base64.encode(hash(INVITATION, stretchedKey)).slice(0, 15)
+  return base64.encode(hash(HashPurpose.INVITATION, stretchedKey)).slice(0, 15)
 }
