615774ec314f3620dfeac7bc8bc013a7173b510b
HerbCaudill <herb@devresults.com>
2021-01-29 13:32:56 +0100

    lockbox/rotate.ts:

--- src/lockbox/rotate.ts
+++ src/lockbox/rotate.ts
@@ -1,29 +1,29 @@
 ﻿import { KeysetWithSecrets } from '/keyset'
 import { Lockbox } from '/lockbox/types'
 import { create } from '/lockbox/create'
-
+import assert from 'assert'
+import { getScope } from '/keyset/getScope'
+import * as R from 'ramda'
 /**
  * "Rotating" a lockbox means replacing the keys it contains with new ones.
  *
  * For example, if the admin keys are compromised, we'll need to come up with a new set of keys;
- * then we'll need to find every lockbox that contained the old keys, and replace them with the new
- * ones.
+ * then we'll need to find every lockbox that contained the old keys, and make a replacement lockbox
+ * for each one, containing the new keys.
  *
  * ```js
  * const newAdminKeys = keyset.create({ type: ROLE, name: ADMIN })
  * const newAdminLockboxForAlice = lockbox.rotate(adminLockboxForAlice, newAdminKeys)
  * ```
  */
-export const rotate = (oldLockbox: Lockbox, contents: KeysetWithSecrets): Lockbox => {
-  // make sure new keys have the same type and name as the old lockbox
-  if (contents.type !== oldLockbox.contents.type || contents.name !== oldLockbox.contents.name)
-    throw new Error('The type and name of the new contents must match those of the old lockbox')
+export const rotate = (oldLockbox: Lockbox, newContents: KeysetWithSecrets): Lockbox => {
+  assert(
+    R.equals(getScope(newContents), getScope(oldLockbox.contents)),
+    'The scope (type and name) of the new keys must match those of the old lockbox'
+  )
 
   // increment the keys' generation index
-  const newContents = {
-    ...contents,
-    generation: oldLockbox.contents.generation + 1,
-  }
+  newContents.generation = oldLockbox.contents.generation + 1
 
   // make a new lockbox and return that
   const newLockbox = create(newContents, oldLockbox.recipient)
