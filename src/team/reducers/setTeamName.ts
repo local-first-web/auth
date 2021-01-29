3f00be83a0e9c0a36becfe226ec6aa1265c2251a
HerbCaudill <herb@devresults.com>
2021-01-29 12:46:05 +0100

    team/reducers/setTeamName.ts:

--- src/team/reducers/setTeamName.ts
+++ src/team/reducers/setTeamName.ts
@@ -1,6 +1,9 @@
 ﻿import { Reducer } from '/team/reducers/index'
 
-export const setTeamName = (teamName: string): Reducer => state => ({
+// TODO we haven't exposed this yet because we're using the team name as a unique identifier.
+// We need to put in place a team ID that's a stable CUID.
+
+export const setTeamName = (teamName: string): Reducer => (state) => ({
   ...state,
   teamName,
 })
