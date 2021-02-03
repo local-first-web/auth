fd591f6bad4fe335900ff1179c8f202009816e99
HerbCaudill <herb@devresults.com>
2021-02-03 20:11:55 +0100

    chain/successors.test ✅

--- src/team/reducers/removeDevice.ts
+++ src/team/reducers/removeDevice.ts
@@ -1,23 +1,23 @@
 ﻿import { Reducer } from '/team/reducers/index'
 import { KeyType } from '/keyset'
 
-export const removeDevice = (userName: string, deviceId: string): Reducer => state => ({
+export const removeDevice = (userName: string, deviceId: string): Reducer => (state) => ({
   ...state,
 
   // remove this device from this member's list of devices
-  members: state.members.map(member => {
+  members: state.members.map((member) => {
     return {
       ...member,
       devices:
         member.userName !== userName //
           ? member.devices // leave other members' devices alone
-          : member.devices?.filter(d => d.deviceId !== deviceId),
+          : member.devices?.filter((d) => d.deviceId !== deviceId),
     }
   }),
 
   // remove any lockboxes this device has
   lockboxes: state.lockboxes.filter(
-    lockbox =>
+    (lockbox) =>
       !(
         lockbox.recipient.name === userName &&
         lockbox.contents.type === KeyType.DEVICE &&
