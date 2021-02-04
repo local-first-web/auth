d7b12b607bf91453f64e413a4ebe73be40b677b1
HerbCaudill <herb@devresults.com>
2021-02-04 12:50:06 +0100

    team/devices.test.ts:

--- src/team/devices.test.ts
+++ src/team/devices.test.ts
@@ -1,30 +1,15 @@
-﻿import { redactDevice, DeviceType, DeviceWithSecrets, getDeviceId } from '/device'
-import { KeyType } from '/keyset'
-import { defaultContext, newTeam } from '/util/testing'
-import { storage } from '/util/testing'
-import * as keyset from '/keyset'
-import { randomSeed } from '/invitation'
+﻿import { KeyType } from '/keyset'
 
 const { DEVICE } = KeyType
 
 describe('Team', () => {
-  beforeEach(() => {
-    localStorage.clear()
-    storage.contents = undefined
-  })
-
-  const setup = () => ({
-    team: newTeam(),
-    context: defaultContext,
-  })
-
   // TODO
   describe('devices', () => {
     it('removes a device', () => {
       // const { team, context } = setup()
       // // From her laptop, Alice adds her phone
       // expect(context.user.device.name).toBe(`alice's device`)
-      // const device = { userName: 'alice', name: `alice's phone`, type: DeviceType.mobile }
+      // const device = { userName: 'alice', name: `alice's phone` }
       // const { secretKey } = team.inviteDevice(device)
       // const deviceId = getDeviceId(device)
       // const deviceKeys = keyset.create({ type: DEVICE, name: deviceId })
