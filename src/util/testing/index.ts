c7df1aebca8ce7ab7c1cee9d8b41c75d5249266b
HerbCaudill <herb@devresults.com>
2021-02-04 12:52:47 +0100

    no more makeAssets

--- src/util/testing/index.ts
+++ src/util/testing/index.ts
@@ -1,4 +1,3 @@
-﻿export * from '/util/testing/makeAssets'
-export * from '/util/testing/setup'
+﻿export * from '/util/testing/setup'
 export * from '/util/testing/storage'
 export * from '/util/testing/TestChannel'
