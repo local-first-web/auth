c3f5d295c35f12895ea1df013fd871751fa727ea
HerbCaudill <herb@devresults.com>
2021-02-05 11:30:43 +0100

    team/keys.test.ts: ✅

--- src/team/keys.test.ts
+++ src/team/keys.test.ts
@@ -1,14 +1,11 @@
 import { ADMIN } from '/role'
-import { setup, storage } from '/util/testing'
+import { setup } from '/util/testing'
 import '/util/testing/expect/toLookLikeKeyset'
 import * as keysets from '/keyset'
 
-describe('Team', () => {
-  beforeEach(() => {
-    localStorage.clear()
-    storage.contents = undefined
-  })
+const { MEMBER } = keysets.KeyType
 
+describe('Team', () => {
   describe('keys', () => {
     it('Alice has admin keys and team keys', () => {
       const { alice } = setup(['alice'])
@@ -20,71 +17,43 @@ describe('Team', () => {
     })
 
     it('Bob has team keys', () => {
-      // Alice creates a team, adds Bob, and persists it
-      const { alice, bob } = setup(['alice', { user: 'bob', member: false }])
-      alice.team.add(bob.user)
-      storage.save(alice.team)
-
-      // Bob loads the team
-      const bobsTeam = storage.load(bob.localContext)
+      const { bob } = setup(['alice', 'bob'])
 
       // Bob has team keys
-      const teamKeys = bobsTeam.teamKeys()
+      const teamKeys = bob.team.teamKeys()
       expect(teamKeys).toLookLikeKeyset()
-
-      // Bob is not an admin so he doesn't have admin keys
-      const bobLooksForAdminKeys = () => bobsTeam.roleKeys(ADMIN)
-      expect(bobLooksForAdminKeys).toThrow(/not found/)
     })
 
     it(`if Bob isn't admin he doesn't have admin keys`, () => {
-      // Alice creates a team, adds Bob, and persists it
-      const { alice, bob } = setup(['alice', { user: 'bob', member: false }])
-      alice.team.add(bob.user)
-      storage.save(alice.team)
-
-      // Bob loads the team
-      const bobsTeam = storage.load(bob.localContext)
+      const { bob } = setup(['alice', { user: 'bob', admin: false }])
 
       // Bob is not an admin so he doesn't have admin keys
-      const bobLooksForAdminKeys = () => bobsTeam.roleKeys(ADMIN)
-      expect(bobLooksForAdminKeys).toThrow(/not found/)
+      const bobLooksForAdminKeys = () => bob.team.roleKeys(ADMIN)
+      expect(bobLooksForAdminKeys).toThrow()
     })
 
     it('if Bob is an admin he has admin keys', () => {
-      // Alice creates a team, adds Bob as an admin, and persists it
-      const { alice, bob } = setup(['alice', { user: 'bob', member: false }])
-      alice.team.add(bob.user, [ADMIN])
-      storage.save(alice.team)
+      const { bob } = setup(['alice', { user: 'bob', admin: true }])
 
-      // Bob loads the team
-      const bobsTeam = storage.load(bob.localContext)
-
-      // Bob has admin keys
-      const adminKeys = bobsTeam.roleKeys(ADMIN)
+      // Bob is an admin so he does have admin keys
+      const adminKeys = bob.team.roleKeys(ADMIN)
       expect(adminKeys).toLookLikeKeyset()
     })
 
     it('after changing his keys, Bob still has team keys', () => {
-      // Alice creates a team, adds Bob, and persists it
-      const { alice, bob } = setup(['alice', { user: 'bob', member: false }])
-      alice.team.add(bob.user)
-      storage.save(alice.team)
-
-      // Bob loads the team
-      const bobsTeam = storage.load(bob.localContext)
+      const { bob } = setup(['alice', 'bob'])
 
       // Bob has team keys
-      const teamKeys = bobsTeam.teamKeys()
+      const teamKeys = bob.team.teamKeys()
       expect(teamKeys).toLookLikeKeyset()
       expect(teamKeys.generation).toBe(0)
 
       // Bob changes his user keys
-      const newKeys = keysets.create({ type: keysets.KeyType.MEMBER, name: 'bob' })
-      bobsTeam.changeKeys(newKeys)
+      const newKeys = keysets.create({ type: MEMBER, name: 'bob' })
+      bob.team.changeKeys(newKeys)
 
       // Bob still has access to team keys
-      const teamKeys2 = bobsTeam.teamKeys()
+      const teamKeys2 = bob.team.teamKeys()
       expect(teamKeys2).toLookLikeKeyset()
       expect(teamKeys2.generation).toBe(1) // the team keys were rotated, so these are new
     })
