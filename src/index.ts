7a7ad1d6bc17715da85d3e80de65e4c28cf3476e
HerbCaudill <herb@devresults.com>
2021-02-05 13:57:51 +0100

    index.ts:

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-﻿export { create as createUser, load as loadUser, User } from '/user'
+﻿export { create as createUser, User } from '/user'
 export { create as createTeam, load as loadTeam, Team } from '/team'
 export { LocalUserContext } from '/context'
 export { generateProof } from '/invitation'
