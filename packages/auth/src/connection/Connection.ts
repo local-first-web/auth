0ee9456e94ad6e242cf4bc44f5b80a10d0b896e2
HerbCaudill <herb@devresults.com>
2021-04-19 12:24:50 +0200

    connection: include message type in log

--- packages/auth/src/connection/Connection.ts
+++ packages/auth/src/connection/Connection.ts
@@ -575,7 +575,7 @@ export class Connection extends EventEmitter {
 
   private logMessage = (direction: 'in' | 'out', message: ConnectionMessage, index: number) => {
     const arrow = direction === 'in' ? '<-' : '->'
-    this.log(`${arrow} ${this.peerName} #${index} ${message.type} ${messageSummary(message)}`)
+    this.log(`${arrow} ${this.peerName} #${index} ${messageSummary(message)}`)
   }
 
   private rehydrateTeam = (context: ConnectionContext, event: ConnectionMessage) => {
