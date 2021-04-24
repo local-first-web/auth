bc771f966fc6e85bf617cf38bbad66af65e605d6
HerbCaudill <herb@devresults.com>
2021-04-24 14:43:10 +0200

    demo/hooks/useTeam.tsx:

--- demo/src/hooks/useTeam.tsx
+++ demo/src/hooks/useTeam.tsx
@@ -100,7 +100,6 @@ export const useTeam = () => {
       })
       .on('connected', (connection: auth.Connection) => {
         // get the latest team info from the connection
-
         setTeam(connection.team!)
       })
       .on('disconnected', (_id, event) => {
