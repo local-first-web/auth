d5a65525326a0cd69e2ec7b09ddfcd7122a38358
HerbCaudill <herb@devresults.com>
2021-06-06 11:59:36 +0200

    auth/invitation/types.ts: remove InvitationBody; add maxUses, expiration, etc.

--- packages/auth/src/invitation/types.ts
+++ packages/auth/src/invitation/types.ts
@@ -1,45 +1,48 @@
-import { KeyScope, KeyType } from '@/keyset'
-import { Base64, Encrypted } from '@/util'
+import { Base64, UnixTimestamp } from '@/util'
 
-// INVITATION
-
-export interface InvitationBody {
-  /** The user or device to invite (e.g. `{type: MEMBER, name: userName}` or `{type: DEVICE, name: deviceId}` */
-  invitee: Invitee
-  publicKey: Base64 // public half of the ephemeral signature key
-}
-
+/**
+ * The public record of the invitation that Alice adds to the signature chain after inviting Bob.
+ * */
 export interface Invitation {
   /** Public, unique identifier for the invitation */
   id: Base64
 
-  encryptedBody: Encrypted<InvitationBody>
+  /** The public signing key derived from the secret invitation key */
+  publicKey: Base64
 
-  /** Generation # of the team keyset */
-  generation: number
+  /** Number of times the invitation can be used. If 0, the invitation can be used any number of times. */
+  maxUses: number
 
-  /** If true, this invitation has already been used to admin a member or device */
-  used?: Boolean
-
-  /** If true, this invitation was revoked at some point after it was created (but before it was used) */
-  revoked?: Boolean
+  /** Time when the invitation expires. If 0, the invitation does not expire. */
+  expiration: UnixTimestamp
 }
 
-// PROOF OF INVITATION
+/**
+ * The current state of the invitation; appears in the Team state. These properties are populated
+ * by the reducer.
+ * */
+export interface InvitationState extends Invitation {
+  /**  */
+  expired: Boolean
 
-/** This is what Bob takes to the team so they'll let him in */
+  /** Number of times the invitation has been used */
+  useCount: number
+
+  /** If true, this invitation was revoked at some point after it was created (but before it was used) */
+  revoked: Boolean
+}
+
+/**
+ * The document Bob presents the first time he connects to an admin (could be Alice, but could also
+ * be someone else) to prove that he's been invited, so they'll let him in.
+ * */
 export interface ProofOfInvitation {
   /** Public, unique identifier for the invitation */
   id: Base64
 
-  /** The user or device that was invited */
-  invitee: Invitee
+  /** The public signing key derived from the secret invitation key */
+  publicKey: Base64
 
-  /** Signature of userName and id, using the signing keys derived from the secret invitation key */
+  /** Signature of userName and id, using the private signing key derived from the secret invitation key */
   signature: Base64
-}
-
-export interface Invitee extends KeyScope {
-  type: typeof KeyType.MEMBER | typeof KeyType.DEVICE
-  name: string
 }
