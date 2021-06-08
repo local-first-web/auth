e7dffc550938b942182288bbb244a78c96233d32
HerbCaudill <herb@devresults.com>
2021-06-08 11:52:43 +0200

    auth/chain/isAdminOnlyAction.ts: separate ADMIT_MEMBER & ADMIT_DEVICE

--- packages/auth/src/chain/isAdminOnlyAction.ts
+++ packages/auth/src/chain/isAdminOnlyAction.ts
@@ -2,7 +2,13 @@ import { TeamLinkBody } from '@/chain'
 
 export const isAdminOnlyAction = (action: TeamLinkBody) => {
   // any team member can do these things
-  const nonAdminActions = ['ADD_DEVICE', 'CHANGE_MEMBER_KEYS', 'CHANGE_DEVICE_KEYS', 'ADMIT']
+  const nonAdminActions = [
+    'ADD_DEVICE',
+    'CHANGE_MEMBER_KEYS',
+    'CHANGE_DEVICE_KEYS',
+    'ADMIT_MEMBER',
+    'ADMIT_DEVICE',
+  ]
 
   return !nonAdminActions.includes(action.type)
 }
