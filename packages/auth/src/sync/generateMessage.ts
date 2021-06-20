23277dc9e49473064f9514e34f870701178ad9d4
HerbCaudill <herb@devresults.com>
2021-06-20 17:21:57 +0200

    auth/sync/generateMessage.ts:

--- packages/auth/src/sync/generateMessage.ts
+++ packages/auth/src/sync/generateMessage.ts
@@ -6,69 +6,72 @@ import { SyncPayload, SyncState } from './types'
 const log = debug('lf:auth:sync')
 
 export const generateMessage = <A extends Action>(
-  chain: SignatureChain<A>,
-  syncState: SyncState
+  prevChain: SignatureChain<A>,
+  prevSyncState: SyncState
 ): [SyncState, SyncPayload<A> | undefined] => {
-  const { theirHead, commonHead, ourNeed, theirNeed } = syncState
-  const { root, head } = chain
+  const { theirHead, commonHead, ourNeed, theirNeed } = prevSyncState
+  const { root, head } = prevChain
   const ourHead = head
 
   if (commonHead === ourHead) {
     // already synced up
     log('generateMessage: already synced up %o', { ourHead, theirHead })
-    return [syncState, undefined]
+    return [prevSyncState, undefined]
   }
 
-  const newSyncState = { ...syncState, ourHead }
+  const syncState = { ...prevSyncState, ourHead }
   const syncMessage: SyncPayload<A> = { root, head }
 
   const weHaveConverged = theirHead === ourHead
-  const theyAreBehind = theirHead && theirHead in chain.links
+  const theyAreBehind = theirHead && theirHead in prevChain.links
 
   if (weHaveConverged) {
     // we converged with the last message we received
     // (we still want to send them a final message so they know we're done)
-    newSyncState.commonHead = ourHead
+    syncState.commonHead = ourHead
   } else if (theyAreBehind) {
-    // their head precedes ours, so we know exactly what they need
+    // we are ahead of them , so we know exactly what they need
 
     // they already have everything preceding their head
     assert(theirHead)
-    const predecessorHashes = getPredecessors(chain, chain.links[theirHead]).map(link => link.hash)
+    const predecessorHashes = getPredecessors(prevChain, prevChain.links[theirHead]).map(
+      link => link.hash
+    )
     const theyAlreadyHave = [
       ...predecessorHashes,
       theirHead, // also the head itself
     ]
 
     // send them everything we have that they don't already have
-    syncMessage.links = Object.keys(chain.links)
+    syncMessage.links = Object.keys(prevChain.links)
       .filter(hash => !theyAlreadyHave.includes(hash))
-      .map(hash => chain.links[hash])
+      .map(hash => prevChain.links[hash])
       .reduce(arrayToMap('hash'), {})
   } else {
     // we have divergent chains
 
     // build a probabilistic filter representing the hashes we think they may need
-    const syncedLinks = commonHead ? getPredecessors(chain, chain.links[commonHead]) : []
+    const syncedLinks = commonHead
+      ? getPredecessors(prevChain, prevChain.links[commonHead]) // we'll omit everything they already have
+      : []
     const syncedHashes = syncedLinks.map(link => link.hash)
-    const haveHashes = Object.keys(chain.links).filter(
-      hash =>
-        hash !== commonHead && // omit last common head
-        !syncedHashes.includes(hash) // and its predecessors
-    )
+    const hashesTheyMightNeed = Object.keys(prevChain.links)
+      .filter(hash => hash !== commonHead) // omit last common head
+      .filter(hash => !syncedHashes.includes(hash)) // and its predecessors
+
     // TODO: we already have hashes - make TruncatedHashFilter compatible with base64
-    const filter = new TruncatedHashFilter().add(haveHashes)
+    const filter = new TruncatedHashFilter().add(hashesTheyMightNeed)
     syncMessage.encodedFilter = filter.save() // send compact representation of the filter
 
     // send them any links we know they need
     syncMessage.links = theirNeed
-      .map(hash => chain.links[hash]) // look up each link
+      .map(hash => prevChain.links[hash]) // look up each link
       .reduce(arrayToMap('hash'), {}) // put links in a map
 
     // our missing dependencies
     syncMessage.need = ourNeed
   }
 
-  log('generateMessage %o', { syncState, syncMessage })
-  return [newSyncState, syncMessage]
+  log('generateMessage %o', { syncState: prevSyncState, syncMessage })
+  return [syncState, syncMessage]
 }
