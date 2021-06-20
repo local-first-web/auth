59adbac0ecdd2bc72e78092423bb21a70f497dc8
HerbCaudill <herb@devresults.com>
2021-06-20 18:08:37 +0200

    auth/sync/sync.test.ts: many concurrent changes

--- packages/auth/src/sync/sync.test.ts
+++ packages/auth/src/sync/sync.test.ts
@@ -92,6 +92,38 @@ describe('sync', () => {
     // Now they are synced up again
     expectToBeSynced(alice, bob)
   })
+
+  it('many concurrent changes', () => {
+    const [{ alice, bob }, network] = setup('alice', 'bob')
+    network.connect(alice.peer, bob.peer)
+
+    const N = 10
+    for (let i = 0; i < N; i++) {
+      alice.team.addRole(`role-${i}`)
+    }
+    alice.peer.sync()
+    network.deliverAll()
+
+    // no changes yet; Alice and Bob are synced up
+    expectToBeSynced(alice, bob)
+
+    // Alice and Bob both make changes; now they are out of sync
+    for (let i = 0; i < N; i++) {
+      alice.team.addRole(`alice-${i}`)
+      bob.team.addRole(`bob-${i}`)
+    }
+    expectNotToBeSynced(alice, bob)
+
+    // Alice exchanges sync messages with Bob
+    bob.peer.sync()
+    const msgs = network.deliverAll()
+
+    // Links sent should be N per peer, plus 1 merge link
+    expect(countLinks(msgs)).toEqual(N + N + 1)
+
+    // Now they are synced up again
+    expectToBeSynced(alice, bob)
+  })
 })
 
 const expectToBeSynced = (a: UserStuff, b: UserStuff) => {
@@ -103,8 +135,8 @@ const expectNotToBeSynced = (a: UserStuff, b: UserStuff) => {
 }
 
 const countLinks = (messages: NetworkMessage[]) => {
-  const linkCount = (message: NetworkMessage) =>
+  const linksInMessage = (message: NetworkMessage) =>
     message.body.links ? Object.keys(message.body.links).length : 0
 
-  return messages.reduce((result, message) => result + linkCount(message), 0)
+  return messages.reduce((result, message) => result + linksInMessage(message), 0)
 }
