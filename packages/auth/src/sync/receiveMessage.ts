7c2c9349189badcdf2af187a211b93d9dc0c9408
HerbCaudill <herb@devresults.com>
2021-06-20 18:01:39 +0200

    auth/sync/receiveMessage.ts:

--- packages/auth/src/sync/receiveMessage.ts
+++ packages/auth/src/sync/receiveMessage.ts
@@ -1,5 +1,6 @@
-import { Action, getParentHashes, LinkMap, merge, SignatureChain } from '@/chain'
+import { Action, Link, LinkMap, merge, SignatureChain } from '@/chain'
 import { assert, debug, Hash } from '@/util'
+import { getMissingLinks } from '../chain/getMissingLinks'
 import { TruncatedHashFilter } from './TruncatedHashFilter'
 import { SyncPayload, SyncState } from './types'
 const log = debug('lf:auth:sync')
@@ -12,60 +13,60 @@ export const receiveMessage = <A extends Action>(
   const {
     root: theirRoot, //
     head: theirHead,
-    links,
+    links: newLinks = [],
     encodedFilter,
   } = syncMessage
 
-  const {
-    root: ourRoot, //
-    head: ourHead,
-  } = prevChain
+  assert(prevChain.root === theirRoot, `Can't sync chains with different roots`)
 
-  assert(ourRoot === theirRoot, `Can't sync chains with different roots`)
+  let chain = { ...prevChain }
+  // record any links ; if we're missing any dependencies, note them to ask for them next time
+  chain.links = { ...prevChain.links, ...newLinks } as LinkMap<A>
 
-  let chain = prevChain
+  let ourNeed: Hash[] = []
+  if (theirHead) {
+    const theirChain = { ...chain, head: theirHead }
+    ourNeed = getMissingLinks(theirChain)
+
+    // if we're not missing anything, merge with their chain
+    if (ourNeed.length === 0) {
+      // console.log({ prevChain, theirChain })
+      chain = merge(prevChain, theirChain)
+    }
+  }
+  const ourHead = chain.head
+
+  // if they've sent us a filter, run all our links through it to know what they have and what they need
+  const [theirNeed, theirHave] = checkLinksAgainstFilter(prevChain, prevSyncState, encodedFilter)
+
   const syncState: SyncState = {
     ...prevSyncState,
     ourHead,
     theirHead,
-  }
-
-  // if they've sent us links, record them; if we're missing any dependencies, note them to ask for them next time
-  if (links) {
-    chain.links = { ...prevChain.links, ...links } as LinkMap<A>
-
-    const theirChain = { root: theirRoot, head: theirHead, links: chain.links }
-
-    const parentHashes = Object.values(links) //
-      .flatMap(link => getParentHashes(theirChain, link))
-      .concat(theirHead)
-    const ourNeed = parentHashes.filter(hash => !(hash in chain.links))
-
-    // if we're not missing anything, merge with their chain
-    if (ourNeed.length === 0) {
-      chain = merge(prevChain, theirChain)
-      syncState.ourHead = chain.head
-    }
-
-    syncState.ourNeed = ourNeed
-  }
-
-  // if they've sent us a filter, run all our links through it to know what they have and what they need
-  if (encodedFilter) {
-    const theirNeed = new Set<Hash>()
-    const theirHave = new Set<Hash>(prevSyncState.theirHave)
-    const filter = new TruncatedHashFilter().load(encodedFilter)
-    for (const hash in prevChain.links) {
-      if (filter.has(hash) || theirRoot === hash || theirHead === hash) {
-        theirHave.add(hash)
-      } else {
-        theirNeed.add(hash)
-      }
-    }
-    syncState.theirNeed = Array.from(theirNeed)
-    syncState.theirHave = Array.from(theirHave)
+    ourNeed,
+    theirNeed,
+    theirHave,
   }
 
   log('receiveMessage %o', { syncState, chain })
   return [chain, syncState]
+}
+
+const checkLinksAgainstFilter = <A extends Action>(
+  chain: SignatureChain<A>,
+  syncState: SyncState,
+  encodedFilter: Uint8Array | undefined
+) => {
+  if (encodedFilter === undefined || encodedFilter.length === 0) return [[], syncState.theirHave]
+  const theirNeed = new Set<Hash>()
+  const theirHave = new Set<Hash>(syncState.theirHave)
+  const filter = new TruncatedHashFilter().load(encodedFilter)
+  for (const hash in chain.links) {
+    if (filter.has(hash) || chain.root === hash || syncState.theirHead === hash) {
+      theirHave.add(hash)
+    } else {
+      theirNeed.add(hash)
+    }
+  }
+  return [Array.from(theirNeed), Array.from(theirHave)]
 }
