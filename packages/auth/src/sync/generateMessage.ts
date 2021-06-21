97aeea62135b0ed1f56738b304dafdd988bb03bf
HerbCaudill <herb@devresults.com>
2021-06-21 19:37:38 +0200

    auth/sync/generateMessage.ts:

--- packages/auth/src/sync/generateMessage.ts
+++ packages/auth/src/sync/generateMessage.ts
@@ -1,32 +1,33 @@
 import { Action, getHead, getPredecessorHashes, isPredecessor, SignatureChain } from '@/chain'
 import { arrayToMap, assert, debug } from '@/util'
 import { messageSummary } from '@/util/testing'
-import * as R from 'ramda'
 import { TruncatedHashFilter } from './TruncatedHashFilter'
 import { SyncPayload, SyncState } from './types'
+import { unique } from './unique'
 
 const log = debug('lf:auth:sync')
 
 export const generateMessage = <A extends Action>(
   prevChain: SignatureChain<A>,
-  prevSyncState: SyncState
+  prevState: SyncState
 ): [SyncState, SyncPayload<A> | undefined] => {
-  const { theirHead, commonHead, ourNeed, theirNeed } = prevSyncState
+  const { theirHead, commonHead, ourNeed, theirNeed } = prevState
   const { root, head } = prevChain
   const ourHead = head
 
   if (commonHead === ourHead) {
     // already synced up
     log('generateMessage: already synced up %o', { ourHead, theirHead })
-    return [prevSyncState, undefined]
+    return [prevState, undefined]
   }
 
-  const syncState = { ...prevSyncState, ourHead }
-  const syncMessage: SyncPayload<A> = { root, head }
+  const state = { ...prevState, ourHead }
+  const message: SyncPayload<A> = { root, head }
 
   const weHaveConverged = theirHead === ourHead
+
   const theyAreBehind =
-    theirNeed.length === 0 &&
+    !theirNeed.length &&
     theirHead &&
     theirHead in prevChain.links &&
     isPredecessor(prevChain, prevChain.links[theirHead], getHead(prevChain))
@@ -34,7 +35,7 @@ export const generateMessage = <A extends Action>(
   if (weHaveConverged) {
     // we converged with the last message we received
     // (we still want to send them a final message so theJy know we're done)
-    syncState.commonHead = ourHead
+    state.commonHead = ourHead
   } else if (theyAreBehind) {
     // we are ahead of them , so we know exactly what they need
 
@@ -46,9 +47,9 @@ export const generateMessage = <A extends Action>(
     ]
 
     // send them everything we have that they don't already have
-    syncMessage.links = Object.keys(prevChain.links)
+    message.links = Object.keys(prevChain.links)
       .filter(hash => !theyAlreadyHave.includes(hash)) // exclude what they have
-      .filter(hash => !syncState.weHaveSent.includes(hash)) // exclude what we've already sent
+      .filter(hash => !state.weHaveSent.includes(hash)) // exclude what we've already sent
       .map(hash => prevChain.links[hash]) // look up the link
       .reduce(arrayToMap('hash'), {}) // turn into map
   } else {
@@ -64,22 +65,22 @@ export const generateMessage = <A extends Action>(
 
     // TODO: we already have hashes - make TruncatedHashFilter compatible with base64
     const filter = new TruncatedHashFilter().add(hashesTheyMightNeed)
-    syncMessage.encodedFilter = filter.save() // send compact representation of the filter
+    message.encodedFilter = filter.save() // send compact representation of the filter
 
     // send them any links we know they need
-    syncMessage.links = theirNeed
+    message.links = theirNeed
       .map(hash => prevChain.links[hash]) // look up each link
       .reduce(arrayToMap('hash'), {}) // put links in a map
 
-    syncState.theirNeed = []
+    state.theirNeed = []
 
     // our missing dependencies
-    syncMessage.need = ourNeed
+    message.need = ourNeed
   }
 
-  syncState.weHaveSent = R.uniq([...syncState.weHaveSent, ...Object.keys(syncMessage.links ?? {})])
+  state.weHaveSent = unique([...state.weHaveSent, ...Object.keys(message.links ?? {})])
 
-  log('generateMessage %o', messageSummary(syncMessage))
+  log('generateMessage %o', messageSummary(message))
 
-  return [syncState, syncMessage]
+  return [state, message]
 }
