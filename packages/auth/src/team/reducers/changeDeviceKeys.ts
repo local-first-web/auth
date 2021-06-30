d3c07187e239801ae87ac19f6792a7a7da5e3d5d
HerbCaudill <herb@devresults.com>
2021-06-30 14:34:15 +0200

    auth/team/reducers/changeDeviceKeys.ts:

--- packages/auth/src/team/reducers/changeDeviceKeys.ts
+++ packages/auth/src/team/reducers/changeDeviceKeys.ts
@@ -1,28 +1,27 @@
 ﻿import { parseDeviceId } from '@/device'
 import { PublicKeyset } from '@/keyset'
 import { Reducer } from '@/team/reducers/index'
-import { debug } from '@/util'
 
-const log = debug('lf:auth:reducer')
-
-export const changeDeviceKeys = (keys: PublicKeyset): Reducer => state => {
-  const { userName, deviceName } = parseDeviceId(keys.name)
-  return {
-    ...state,
-    members: state.members.map(member => {
-      if (member.userName === userName) {
-        return {
-          ...member,
-          devices: member.devices?.map(device => {
-            if (device.deviceName === deviceName)
-              return {
-                ...device,
-                keys, // 🡐 replace device keys
-              }
-            else return device
-          }),
-        }
-      } else return member
-    }),
+export const changeDeviceKeys =
+  (keys: PublicKeyset): Reducer =>
+  state => {
+    const { userName, deviceName } = parseDeviceId(keys.name)
+    return {
+      ...state,
+      members: state.members.map(member => {
+        if (member.userName === userName) {
+          return {
+            ...member,
+            devices: member.devices?.map(device => {
+              if (device.deviceName === deviceName)
+                return {
+                  ...device,
+                  keys, // 🡐 replace device keys
+                }
+              else return device
+            }),
+          }
+        } else return member
+      }),
+    }
   }
-}
