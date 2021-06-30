e47ee9ff91411c69628577f6cbc182e21adee617
HerbCaudill <herb@devresults.com>
2021-06-30 14:35:06 +0200

    auth/util/truncateHashes.ts:

--- packages/auth/src/util/truncateHashes.ts
+++ packages/auth/src/util/truncateHashes.ts
@@ -1,4 +1,16 @@
-﻿export const truncateHashes = (s: string) => {
-  const hashRx = /(?:[A-Za-z0-9+/=]{32,100})?/g
-  return s.replace(hashRx, s => s.slice(0, 5))
+﻿export function truncateHashes(arg: any) {
+  if (typeof arg === 'string') {
+    const str = arg as string
+    const hashRx = /(?:[A-Za-z0-9+/=]{32,100})?/g
+    return str.replace(hashRx, s => s.slice(0, 5))
+  } else if (typeof arg === 'object') {
+    const obj = {} as any
+    for (const prop in arg) {
+      const value = arg[prop]
+      obj[prop] = truncateHashes(value)
+    }
+    return obj
+  } else {
+    return arg
+  }
 }
