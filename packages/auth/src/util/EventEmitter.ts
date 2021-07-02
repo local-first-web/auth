abf4f2388925e8e951da916f0c2ac7e9b84ed09d
HerbCaudill <herb@devresults.com>
2021-07-02 11:40:01 +0200

    auth/util/EventEmitter.ts: create

---
+++ packages/auth/src/util/EventEmitter.ts
@@ -0,0 +1,17 @@
+﻿import { EventEmitter as _EventEmitter } from 'events'
+import debug from 'debug'
+
+/** EventEmitter with built-in logging */
+export class EventEmitter extends _EventEmitter {
+  /** The `log` method is meant to be overridden, e.g.
+   * ```ts
+   *  this.log = debug(`lf:auth:demo:conn:${context.user.userName}`)
+   * ```
+   */
+  log: debug.Debugger = debug(`EventEmitter`)
+
+  public emit(event: string, ...args: any[]) {
+    this.log(`emit ${event}`, ...args)
+    return super.emit(event, ...args)
+  }
+}
