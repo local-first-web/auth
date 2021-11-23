158b3705f7f4d4bcf0a6a185b63d0191894b6b34
HerbCaudill <herb@devresults.com>
2021-11-23 13:01:10 +0100

    demo/vite.config.ts:

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,11 +1,11 @@
-import inject from '@rollup/plugin-inject'
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 import postcss from './postcss.config'
 
 export default defineConfig({
-  plugins: [reactRefresh()],
+  plugins: [reactRefresh(), tsconfigPaths()],
   build: {
     rollupOptions: {
       external: ['Buffer', 'buffer'],
