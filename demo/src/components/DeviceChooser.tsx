5e28c43f1fb0810963128c56b73bab73c3cd85fd
HerbCaudill <herb@devresults.com>
2021-11-26 16:08:32 +0100

    demo/components/DeviceChooser.tsx: create

---
+++ demo/src/components/DeviceChooser.tsx
@@ -0,0 +1,38 @@
+import React, { useRef } from 'react'
+import { PeerMap } from '../peers'
+
+export const DeviceChooser = ({ onAdd, peers }: ChooserProps) => {
+  const peerSelect = useRef() as React.MutableRefObject<HTMLSelectElement>
+
+  return (
+    <div className="Chooser group flex-grow">
+      <select
+        ref={peerSelect}
+        className={`
+          bg-color-none opacity-25
+          group-hover:opacity-100 group-hover:bg-color-white
+          focus:opacity-100 focus:bg-color-white
+          border-none rounded-lg 
+          p-2 pr-4 h-10 font-normal text-lg`}
+        onChange={() => {
+          peerSelect.current.blur()
+          onAdd(peerSelect.current.value)
+        }}
+      >
+        <option>Show device...</option>
+        {Object.values(peers)
+          .filter(p => !p.show)
+          .map(p => (
+            <option key={p.id} value={p.id}>
+              {p.user.emoji} {p.device.emoji}
+            </option>
+          ))}
+      </select>
+    </div>
+  )
+}
+
+interface ChooserProps {
+  onAdd: (id: string) => void
+  peers: PeerMap
+}
