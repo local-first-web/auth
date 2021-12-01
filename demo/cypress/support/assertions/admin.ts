96ddc1ba1b4fde1f36bc487347cbf837aad8b6a5
HerbCaudill <herb@devresults.com>
2021-12-01 16:37:43 +0100

    demo/cypress/support/assertions/admin.ts: fix `to.be.admin` assertion

--- demo/cypress/support/assertions/admin.ts
+++ demo/cypress/support/assertions/admin.ts
@@ -1,9 +1,18 @@
 ﻿import '@testing-library/cypress/add-commands'
 
-chai.Assertion.addMethod('admin', function () {
+chai.Assertion.addMethod('admin', function() {
   const $element = this._obj
   new chai.Assertion($element).to.be
+
+  // admins see a button
   const $adminButton = $element.find('button:contains("👑")')
-  const isAdmin = $adminButton.css('opacity') === '1'
+  // non-admins just see text
+  const $adminSpan = $element.find('span:contains("👑")')
+
+  const isAdmin = $adminButton.length
+    ? // if the button is not faded out, this member is an admin
+      $adminButton.css('opacity') === '1'
+    : // if the span exists, this member is an admin
+      $adminSpan.length
   this.assert(isAdmin, 'expected to be admin', 'expected not to be admin', true, false)
 })
