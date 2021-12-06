7809c273478d0920f60e4a0a72f5ad6ca483eea9
HerbCaudill <herb@devresults.com>
2021-12-06 14:10:31 +0100

    demo/cypress/support/commands/invite.ts: set maxUses, expiration

--- demo/cypress/support/commands/invite.ts
+++ demo/cypress/support/commands/invite.ts
@@ -1,11 +1,33 @@
 import { CommandFn } from '../'
 
-export const invite: CommandFn = subject => {
+export const SECOND = 1
+export const MINUTE = 60 * SECOND
+export const HOUR = 60 * MINUTE
+export const DAY = 24 * HOUR
+export const WEEK = 7 * DAY
+
+export type InviteOptions = {
+  maxUses?: 1 | 5 | 10 | 0
+  expiration?: typeof SECOND | typeof MINUTE | typeof HOUR | typeof DAY | typeof WEEK
+}
+
+export const invite: CommandFn = (subject, options: InviteOptions = {}) => {
+  const { maxUses = 1, expiration = MINUTE } = options
   const s = () => cy.wrap(subject)
   // click invite button
   s()
     .findByText('Invite members')
     .click()
+
+  // set max uses
+  s()
+    .findByLabelText('How many people can use this invitation code?')
+    .select(maxUses.toString())
+
+  // set expiration
+  s()
+    .findByLabelText('When does this invitation code expire?')
+    .select(expiration.toString())
 
   // press invite button
   s()
