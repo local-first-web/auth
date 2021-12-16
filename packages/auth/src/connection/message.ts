2cd0ec194fee6a0f7bce1d387af9695121b496e7
HerbCaudill <herb@devresults.com>
2021-12-16 13:12:00 +0100

    auth/connection/message.ts: ERROR & LOCAL_ERROR

--- packages/auth/src/connection/message.ts
+++ packages/auth/src/connection/message.ts
@@ -4,16 +4,15 @@ import { SyncMessage as SyncPayload } from 'crdx'
 import { TeamAction, TeamContext } from '@/team'
 import { Base58, Hash } from '@/util'
 import { KeyScope, Keyset } from 'crdx'
+import { ConnectionErrorType, ErrorMessage, LocalErrorMessage } from './errors'
 
 export type ReadyMessage = {
   type: 'REQUEST_IDENTITY'
 }
 
 /**
- * - If we're a member, we just authorize as a device. So all we provide is an identity claim for a
- *   device.
- * - If we're a new member with an invitation, we want to give them our user's public keys and our
- *   device's public keys.
+ * - If we're a member, we just authorize as a device. So all we provide is an identity claim for a device.
+ * - If we're a new member with an invitation, we want to give them our user's public keys and our device's public keys.
  * - If we're a new device with an invitation, we just want to give them our device public keys.
  */
 export type ClaimIdentityMessage = {
@@ -29,14 +28,6 @@ export type ClaimIdentityMessage = {
         userKeys?: Keyset // only for new member (not for new device)
         deviceKeys: Keyset
       }
-}
-
-export type ErrorMessage = {
-  type: 'ERROR'
-  payload: {
-    message: string
-    details?: any
-  }
 }
 
 export type DisconnectMessage = {
@@ -118,42 +109,48 @@ export type EncryptedMessage = {
 }
 
 export type ConnectionMessage =
-  | ReadyMessage
-  | ClaimIdentityMessage
+  | AcceptIdentityMessage
   | AcceptInvitationMessage
   | ChallengeIdentityMessage
-  | ProveIdentityMessage
-  | AcceptIdentityMessage
-  | ErrorMessage
+  | ClaimIdentityMessage
   | DisconnectMessage
-  | ReconnectMessage
-  | SyncMessage
-  | LocalUpdateMessage
-  | SeedMessage
   | EncryptedMessage
+  | ErrorMessage
+  | LocalErrorMessage
+  | LocalUpdateMessage
+  | ProveIdentityMessage
+  | ReadyMessage
+  | ReconnectMessage
+  | SeedMessage
+  | SyncMessage
 
 export type NumberedConnectionMessage = ConnectionMessage & {
   index: number
 }
 
+const msgTypes = [
+  'ACCEPT_IDENTITY',
+  'ACCEPT_INVITATION',
+  'CHALLENGE_IDENTITY',
+  'CLAIM_IDENTITY',
+  'DISCONNECT',
+  'ENCRYPTED_MESSAGE',
+  'ERROR',
+  'LOCAL_ERROR',
+  'LOCAL_UPDATE',
+  'PROVE_IDENTITY',
+  'RECONNECT',
+  'REJECT_IDENTITY',
+  'REQUEST_IDENTITY',
+  'SEED',
+  'SYNC',
+]
+
 export function isNumberedConnectionMessage(msg: any): msg is NumberedConnectionMessage {
-  const msgTypes = [
-    'ACCEPT_IDENTITY',
-    'ACCEPT_INVITATION',
-    'CHALLENGE_IDENTITY',
-    'CLAIM_IDENTITY',
-    'DISCONNECT',
-    'ENCRYPTED_MESSAGE',
-    'ERROR',
-    'LOCAL_UPDATE',
-    'PROVE_IDENTITY',
-    'RECONNECT',
-    'REJECT_IDENTITY',
-    'REQUEST_IDENTITY',
-    'SEED',
-    'SYNC',
-  ]
   return (
-    'index' in msg && typeof msg.index === 'number' && 'type' in msg && msgTypes.includes(msg.type)
+    'index' in msg &&
+    typeof msg.index === 'number' && //
+    'type' in msg &&
+    msgTypes.includes(msg.type)
   )
 }
