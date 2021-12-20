0c14b8bb5ea95053393d796f298612c634272939
HerbCaudill <herb@devresults.com>
2021-12-20 13:34:03 +0100

    auth/team/selectors/scopesToRotate.ts: moved getVisibleScopes to own file

--- packages/auth/src/team/selectors/scopesToRotate.ts
+++ packages/auth/src/team/selectors/scopesToRotate.ts
@@ -1,15 +1,7 @@
 ﻿import { KeyScope } from 'crdx'
 import { TeamState } from '@/team/types'
+import { getVisibleScopes } from './getVisibleScopes'
 
 export const scopesToRotate = (state: TeamState, compromisedScope: KeyScope) => {
   return [compromisedScope, ...getVisibleScopes(state, compromisedScope)]
-}
-
-export const getVisibleScopes = (state: TeamState, { type, name }: KeyScope): KeyScope[] => {
-  const scopes = state.lockboxes
-    .filter(({ recipient }) => recipient.type === type && recipient.name === name)
-    .map(({ contents: { type, name } }) => ({ type, name } as KeyScope))
-
-  const derivedScopes = scopes.flatMap(scope => getVisibleScopes(state, scope))
-  return [...scopes, ...derivedScopes]
 }
