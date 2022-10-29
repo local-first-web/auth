e7d2aab1a81cb53eedb1dea2bf6023dc019b0d68
HerbCaudill <herb@devresults.com>
2022-10-29 10:30:03 +0200

    demo/cypress/integration/basic.ts: don't test connections in hide/show tests

--- demo/cypress/integration/basic.ts
+++ demo/cypress/integration/basic.ts
@@ -28,9 +28,6 @@ it(`We show Bob's device`, () => {
 
 it(`We hide Bob's device`, () => {
   show('Bob:laptop')
-  alice().addToTeam('Bob')
-
-  // we hide Bob's device
   bob().hide()
 
   // we don't see Bob any more
@@ -38,30 +35,15 @@ it(`We hide Bob's device`, () => {
   cy.get('.Peer')
     .userName()
     .should('equal', 'Alice')
-
-  // Alice sees that Bob is disconnected
-  alice()
-    .peerConnectionStatus('Bob')
-    .should('equal', 'disconnected')
 })
 
 it(`We show Bob's device again`, () => {
   show('Bob:laptop')
-  alice().addToTeam('Bob')
-
-  // we hide Bob's device
   bob().hide()
 
   // we show Bob's device again
   show('Bob:laptop')
   cy.get('.Peer').should('have.length', 2)
-  bob().toggleOnline()
-  // Bob rejoins the team
-
-  // Alice sees that Bob is reconnected
-  alice()
-    .peerConnectionStatus('Bob')
-    .should('equal', 'connected')
 })
 
 it('Bob creates another team', () => {
