8744943cf9483eb61884c1c4754cfa7775979638
HerbCaudill <herb@devresults.com>
2022-11-26 16:47:26 +0100

    auth/team/getMissingLinks.ts: HashGraph->Graph

--- packages/auth/src/team/getMissingLinks.ts
+++ packages/auth/src/team/getMissingLinks.ts
@@ -1,6 +1,6 @@
-import { Action, HashGraph } from 'crdx'
+import { Action, Graph } from 'crdx'
 
-export function getMissingLinks<A extends Action, C>(chain: HashGraph<A, C>) {
+export function getMissingLinks<A extends Action, C>(chain: Graph<A, C>) {
   const parentHashes = Object.values(chain.links) //
     .flatMap(link => link.body.prev) as string[]
   return parentHashes //
