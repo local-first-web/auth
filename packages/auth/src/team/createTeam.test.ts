6b8b2344bb0fd47298c0f9215ea14e96d893ff08
HerbCaudill <herb@devresults.com>
2022-11-26 16:48:24 +0100

    auth/team/createTeam.test.ts: create

---
+++ packages/auth/src/team/createTeam.test.ts
@@ -0,0 +1,14 @@
+import { createTeam } from './createTeam'
+import { createDevice } from '@/device'
+import { createUser } from 'crdx'
+
+describe('Team', () => {
+  describe('create', () => {
+    it('returns a new team', () => {
+      const user = createUser('alice', 'Alice McUser')
+      const device = createDevice('alice', 'laptop')
+      const team = createTeam('Spies Я Us', { user, device })
+      expect(team.teamName).toBe('Spies Я Us')
+    })
+  })
+})
