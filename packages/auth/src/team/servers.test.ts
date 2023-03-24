feca3aae2b80b069f76395a426e90a59d66789e4
HerbCaudill <herb@devresults.com>
2023-03-24 14:56:57 +0100

    servers.test.ts: refactor; use new SERVER keytype

--- packages/auth/src/team/servers.test.ts
+++ packages/auth/src/team/servers.test.ts
@@ -1,26 +1,27 @@
-import { createKeyset, createUser, KeyType, redactKeys } from 'crdx'
+import { createKeyset, createUser, redactKeys } from 'crdx'
 import { createDevice, createTeam } from '..'
 
 describe('Team', () => {
-  describe('servers', () => {
-    it('can add and remove servers', () => {
+  describe('a server', () => {
+    it('can be added and removed by an admin', () => {
       const alice = createUser('alice')
       const aliceDevice = createDevice(alice.userId, 'my-computer')
-      const team = createTeam('team team', { user: alice, device: aliceDevice })
+      const aliceTeam = createTeam('team team', { user: alice, device: aliceDevice })
+      const host = 'devresults.com'
+      const serverKeys = createKeyset({ type: 'SERVER', name: host })
 
-      const server = {
-        host: 'www.devresults.com',
-        keys: redactKeys(createKeyset({ type: KeyType.USER })), // maybe need a better way to create arbitrary public keys
-      }
-      team.addServer(server)
+      // add server
+      aliceTeam.addServer({ host, keys: redactKeys(serverKeys) })
+      expect(aliceTeam.servers().length).toBe(1)
 
-      expect(team.servers().length).toBe(1)
-      expect(team.servers(server.host).host).toBe(server.host)
+      // look up server
+      const server = aliceTeam.servers(host)
+      expect(server.host).toBe(host)
 
-      team.removeServer(server.host)
-
-      expect(team.servers().length).toBe(0)
-      expect(team.serverWasRemoved(server.host)).toBe(true)
+      // remove server
+      aliceTeam.removeServer(host)
+      expect(aliceTeam.servers().length).toBe(0)
+      expect(aliceTeam.serverWasRemoved(host)).toBe(true)
     })
   })
 })
