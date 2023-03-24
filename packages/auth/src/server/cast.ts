c137fea88bfc75006055e826fe2e03234257fbe2
HerbCaudill <herb@devresults.com>
2023-03-24 19:32:47 +0100

    server/cast.ts: create
    helpers for casting a server to a member, user, or device

---
+++ packages/auth/src/server/cast.ts
@@ -0,0 +1,27 @@
+import { Device, DeviceWithSecrets } from '@/device'
+import { Member } from '@/team'
+import { User, UserWithSecrets } from 'crdx'
+import { Server, ServerWithSecrets } from './types'
+
+const toMember = (server: Server): Member => ({
+  userId: server.host,
+  userName: server.host,
+  keys: server.keys,
+  roles: [],
+})
+
+const toUser = <T extends Server | ServerWithSecrets>(server: T) =>
+  ({
+    userId: server.host,
+    userName: server.host,
+    keys: server.keys,
+  } as T extends Server ? User : UserWithSecrets)
+
+const toDevice = <T extends Server | ServerWithSecrets>(server: T) =>
+  ({
+    userId: server.host,
+    deviceName: server.host,
+    keys: server.keys,
+  } as T extends Server ? Device : DeviceWithSecrets)
+
+export const cast = { toMember, toUser, toDevice }
