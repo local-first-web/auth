30f5c283dac88759f75305674792f89adf90c6e0
HerbCaudill <herb@devresults.com>
2023-03-24 15:22:39 +0100

    auth/context/types.ts: add ServerContext

--- packages/auth/src/context/types.ts
+++ packages/auth/src/context/types.ts
@@ -1,10 +1,16 @@
-﻿import { DeviceWithSecrets } from '@/device'
-import { SemVer } from '@/util'
-import { UserWithSecrets } from 'crdx'
+﻿import { type DeviceWithSecrets } from '@/device'
+import { type ServerWithSecrets } from '@/server'
+import { type SemVer } from '@/util'
+import { type UserWithSecrets } from 'crdx'
 
 export interface LocalUserContext {
   user: UserWithSecrets
   device: DeviceWithSecrets
+  client?: Client
+}
+
+export interface ServerContext {
+  server: ServerWithSecrets
   client?: Client
 }
 
