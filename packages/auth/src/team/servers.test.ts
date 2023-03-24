11a5b22949f21169bbe813af13eb8835e4bb61d4
HerbCaudill <herb@devresults.com>
2023-03-24 15:00:37 +0100

    servers.test.ts: use setup helper

--- packages/auth/src/team/servers.test.ts
+++ packages/auth/src/team/servers.test.ts
@@ -1,27 +1,27 @@
+import { setup } from '@/util/testing'
 import { createKeyset, createUser, redactKeys } from 'crdx'
 import { createDevice, createTeam } from '..'
 
 describe('Team', () => {
   describe('a server', () => {
     it('can be added and removed by an admin', () => {
-      const alice = createUser('alice')
-      const aliceDevice = createDevice(alice.userId, 'my-computer')
-      const aliceTeam = createTeam('team team', { user: alice, device: aliceDevice })
+      const { alice } = setup('alice')
+
       const host = 'devresults.com'
       const serverKeys = createKeyset({ type: 'SERVER', name: host })
 
       // add server
-      aliceTeam.addServer({ host, keys: redactKeys(serverKeys) })
-      expect(aliceTeam.servers().length).toBe(1)
+      alice.team.addServer({ host, keys: redactKeys(serverKeys) })
+      expect(alice.team.servers().length).toBe(1)
 
       // look up server
-      const server = aliceTeam.servers(host)
+      const server = alice.team.servers(host)
       expect(server.host).toBe(host)
 
       // remove server
-      aliceTeam.removeServer(host)
-      expect(aliceTeam.servers().length).toBe(0)
-      expect(aliceTeam.serverWasRemoved(host)).toBe(true)
+      alice.team.removeServer(host)
+      expect(alice.team.servers().length).toBe(0)
+      expect(alice.team.serverWasRemoved(host)).toBe(true)
     })
 
     it(`can't be added by a non-admin member`, () => {})
