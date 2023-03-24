51cc7c94bec79d235a55a2946d318ad8b5518ea0
Nathan Gerhart <nathan@devresults.com>
2023-03-24 17:11:36 -0600

    let servers change their own keys
    This might make it so other users can change the server keys, which I think we don't want...

--- packages/auth/src/team/isAdminOnlyAction.ts
+++ packages/auth/src/team/isAdminOnlyAction.ts
@@ -1,12 +1,13 @@
-import { TeamLinkBody } from './types'
+import { TeamAction, TeamLinkBody } from './types'
 
 export const isAdminOnlyAction = (action: TeamLinkBody) => {
   // any team member can do these things
-  const nonAdminActions = [
+  const nonAdminActions: TeamAction['type'][] = [
     'INVITE_DEVICE',
     'ADD_DEVICE',
     'CHANGE_MEMBER_KEYS',
     'CHANGE_DEVICE_KEYS',
+    'CHANGE_SERVER_KEYS',
     'ADMIT_MEMBER',
     'ADMIT_DEVICE',
   ]
