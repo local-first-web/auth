d5b04278c460a5d6190a2f466fa511dcbf8c887e
HerbCaudill <herb@devresults.com>
2023-03-24 19:38:12 +0100

    auth/team/selectors/hasServer.ts: create

---
+++ packages/auth/src/team/selectors/hasServer.ts
@@ -0,0 +1,5 @@
+﻿import { Host } from '@/server'
+import { TeamState } from '@/team/types'
+
+export const hasServer = (state: TeamState, host: Host) =>
+  state.servers.find(s => s.host === host) !== undefined
