f7642013738fe6a427f00d431719e8ee56b70948
HerbCaudill <herb@devresults.com>
2023-03-26 15:00:28 +0200

    auth/keysetSummary.ts: fix

--- packages/auth/src/util/keysetSummary.ts
+++ packages/auth/src/util/keysetSummary.ts
@@ -3,6 +3,6 @@ import { Keyset, KeysetWithSecrets } from 'crdx'
 
 export const keysetSummary = (keyset: Keyset | KeysetWithSecrets) => {
   const { name, generation, encryption } = keyset
-  const publicKey = 'publicKey' in encryption ? encryption.publicKey : encryption
+  const publicKey = typeof encryption === 'string' ? encryption : encryption.publicKey
   return `${name}(${truncateHashes(publicKey)})#${generation}`
 }
