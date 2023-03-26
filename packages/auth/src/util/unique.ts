8f9fc37a7f1a99a49d329acbfdef02872f21739e
HerbCaudill <herb@devresults.com>
2023-03-26 14:29:40 +0200

    auth/unique.ts: use lodash.uniqBy

--- packages/auth/src/util/unique.ts
+++ packages/auth/src/util/unique.ts
@@ -1,2 +1,4 @@
-import _uniq from 'lodash/uniq'
-export const unique = _uniq
+import type { ValueIteratee } from 'lodash'
+import _uniqBy from 'lodash/uniqby'
+
+export const unique = <T>(array: T[], fn: ValueIteratee<T> = _ => _) => _uniqBy(array, fn)
