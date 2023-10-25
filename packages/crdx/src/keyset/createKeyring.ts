dc2e975aa5e6a79edfb05573ae7567e496f32771
HerbCaudill <herb@devresults.com>
2023-10-25 17:41:40 +0100

    crdx/keyset/createKeyring.ts:

--- packages/crdx/src/keyset/createKeyring.ts
+++ packages/crdx/src/keyset/createKeyring.ts
@@ -3,5 +3,5 @@ import { isKeyring, isKeyset, type Keyring, type KeysetWithSecrets } from './typ
 export const createKeyring = (keys: Keyring | KeysetWithSecrets | KeysetWithSecrets[]): Keyring => {
   if (isKeyring(keys)) return keys
   if (isKeyset(keys)) keys = [keys]
-  return Object.fromEntries(keys.map<Keyring>(keyset => [keyset.encryption.publicKey, keyset]))
+  return Object.fromEntries(keys.map(keyset => [keyset.encryption.publicKey, keyset]))
 }
