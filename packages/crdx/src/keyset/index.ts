dfc33c332573aeec3f85fa1b1ce79ce57efc7c67
HerbCaudill <herb@devresults.com>
2023-10-27 15:39:04 +0200

    export getLatestGeneration

--- packages/crdx/src/keyset/index.ts
+++ packages/crdx/src/keyset/index.ts
@@ -1,4 +1,5 @@
-export * from './createKeyset.js'
 export * from './createKeyring.js'
+export * from './createKeyset.js'
+export * from './getLatestGeneration.js'
 export * from './redact.js'
 export * from './types.js'
