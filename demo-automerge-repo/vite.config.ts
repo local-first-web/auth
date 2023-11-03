1e8c330817bcece4867c7dc0e42026c071cb4974
Brent Keller <brentkeller@gmail.com>
2023-11-03 12:07:00 -0400

    Add WASM support to demo-automerge-repo

--- demo-automerge-repo/vite.config.ts
+++ demo-automerge-repo/vite.config.ts
@@ -1,10 +1,29 @@
 import viteReact from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import wasm from 'vite-plugin-wasm'
 import postcss from './postcss.config'
 
 export default defineConfig({
-  plugins: [viteReact(), tsconfigPaths()],
+  plugins: [wasm(), viteReact(), tsconfigPaths()],
+
+  worker: {
+    format: 'es',
+    plugins: [wasm()],
+  },
+
+  optimizeDeps: {
+    // This is necessary because otherwise `vite dev` includes two separate
+    // versions of the JS wrapper. This causes problems because the JS
+    // wrapper has a module level variable to track JS side heap
+    // allocations, and initializing this twice causes horrible breakage
+    exclude: [
+      '@automerge/automerge-wasm',
+      '@automerge/automerge-wasm/bundler/bindgen_bg.wasm',
+      '@syntect/wasm',
+    ],
+  },
+
   build: {
     rollupOptions: {
       external: ['Buffer', 'buffer'],
