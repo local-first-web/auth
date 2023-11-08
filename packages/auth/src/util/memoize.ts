c0621a9d821904aae95639d73e2a12b6705d83e9
Brent Keller <brentkeller@gmail.com>
2023-11-08 15:59:30 -0500

    Add memize package and migrate from fast-memoize
    fast-memoize doesn't support ES modules so this switches to a comparable package that does

--- packages/auth/src/util/memoize.ts
+++ packages/auth/src/util/memoize.ts
@@ -1,5 +1,4 @@
-import _memoize from 'fast-memoize'
-import { type MemoizeFunc } from 'fast-memoize/typings/fast-memoize'
+import memize from 'memize'
 
 // ignore file coverage
 
@@ -7,13 +6,4 @@ const BYPASS = false
 
 const passthrough = <T>(f: T) => f
 
-export const memoize = (BYPASS ? passthrough : _memoize) as Memoize
-
-// Types that are not exported from fast-memoize
-
-type Memoize = {
-  strategies: {
-    variadic: MemoizeFunc
-    monadic: MemoizeFunc
-  }
-} & MemoizeFunc
+export const memoize = (BYPASS ? passthrough : memize) as typeof memize
