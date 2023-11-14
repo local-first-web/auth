71dc1ecb6dd552f5bb318bb40c7ad430c10a0778
HerbCaudill <herb@devresults.com>
2023-11-14 17:53:06 +0100

    add messages.test.ts

---
+++ packages/auth/src/team/messages.test.ts
@@ -0,0 +1,45 @@
+import { setup } from 'util/testing/index.js'
+import { describe, expect, it } from 'vitest'
+
+describe('Team', () => {
+  describe('messages', () => {
+    it('an admin member can add and retrieve a message', () => {
+      const { alice } = setup('alice')
+
+      // Alice posts a message to the team graph
+      const message = {
+        type: 'SUPER_IMPORTANT_MESSAGE',
+        payload: {
+          foo: 'pizza',
+        },
+      }
+      alice.team.addMessage(message)
+
+      // Alice retrieves all the messages
+      const messages = alice.team.messages()
+
+      // There's only one
+      expect(messages.length).toBe(1)
+
+      // It's the one Alice posted
+      expect(messages[0]).toEqual(message)
+    })
+
+    // TODO: assuming for now this is the behavior we want
+    it('a non-admin member cannot add messages', () => {
+      const { bob } = setup(['alice', { user: 'bob', admin: false }])
+
+      // Bob tries to post a message to the team graph
+      const message = {
+        type: 'SUPER_IMPORTANT_MESSAGE',
+        payload: {
+          foo: 'pizza',
+        },
+      }
+
+      expect(() => bob.team.addMessage(message)).toThrow()
+
+      expect(bob.team.messages().length).toBe(0)
+    })
+  })
+})
