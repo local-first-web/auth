fd7214a909ccafa7fbe10cc3a938b3f986bd31e0
HerbCaudill <herb@devresults.com>
2023-11-16 17:35:21 +0100

    redactDevice: include new deviceId

--- packages/auth/src/device/redact.ts
+++ packages/auth/src/device/redact.ts
@@ -3,6 +3,7 @@ import { type DeviceWithSecrets, type Device } from 'device/types.js'
 
 export const redactDevice = (device: DeviceWithSecrets): Device => ({
   userId: device.userId,
+  deviceId: device.deviceId,
   deviceName: device.deviceName,
   keys: redactKeys(device.keys),
 })
