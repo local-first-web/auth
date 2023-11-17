5f77506ac5222f93756b78edc361d3f826a64547
HerbCaudill <herb@devresults.com>
2023-11-17 14:21:05 +0100

    fix memoize types

--- packages/crdx/src/util/memoize.ts
+++ packages/crdx/src/util/memoize.ts
@@ -4,9 +4,9 @@ import { memoize as _memoize } from 'lodash-es'
 
 const BYPASS = false
 
-export const nomemoize = <T extends (...args: any) => any>(
+export const nomemoize = <T extends (...args: any[]) => any>(
   f: T,
-  _resolver: (...args: Parameters<T>) => any
+  _resolver: (...args: Parameters<T>) => ReturnType<T>
 ) => f
 
 export const memoize = (BYPASS ? nomemoize : _memoize) as typeof _memoize
