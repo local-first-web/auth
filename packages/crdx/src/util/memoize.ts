ef04462fd2ed62e9fb5967af243c68ffaaafa132
Frederico Rodrigues <fred@devresults.com>
2023-11-16 19:04:23 -0500

    memoize: Updating nomemoize with VS code suggestion

--- packages/crdx/src/util/memoize.ts
+++ packages/crdx/src/util/memoize.ts
@@ -4,6 +4,9 @@ import { memoize as _memoize } from 'lodash-es'
 
 const BYPASS = false
 
-export const nomemoize = <T>(f: T, _resolver: (...args: Parameters<T>) => any) => f
+export const nomemoize = <T extends (...args: any) => any>(
+  f: T,
+  _resolver: (...args: Parameters<T>) => any
+) => f
 
 export const memoize = (BYPASS ? nomemoize : _memoize) as typeof _memoize
