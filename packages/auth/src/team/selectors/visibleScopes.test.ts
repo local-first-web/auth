ec2a24204d69e096914d573825dd88757e95e45c
HerbCaudill <herb@devresults.com>
2023-11-17 19:40:43 +0100

    auth/team/selectors/visibleScopes.test.ts: update tests

--- packages/auth/src/team/selectors/visibleScopes.test.ts
+++ packages/auth/src/team/selectors/visibleScopes.test.ts
@@ -1,6 +1,6 @@
 import { createKeyset } from '@localfirst/crdx'
 import { describe, expect, it } from 'vitest'
-import * as select from '.'
+import * as select from './index.js'
 import { ADMIN } from 'role/index.js'
 import { KeyType } from 'util/index.js'
 import { setup } from 'util/testing/index.js'
@@ -10,12 +10,11 @@ const { USER, DEVICE, TEAM, ROLE } = KeyType
 describe('visibleScopes', () => {
   it("alice's device can see user, admin and team keys", () => {
     const { alice } = setup('alice')
-    const deviceScopes = select.visibleScopes(alice.team.state, {
-      type: DEVICE,
-      name: 'alice::laptop',
-    })
+
+    const { type, name } = alice.device.keys
+    const deviceScopes = select.visibleScopes(alice.team.state, { type, name })
     expect(deviceScopes).toEqual([
-      { type: USER, name: 'alice' },
+      { type: USER, name: alice.userId },
       { type: TEAM, name: TEAM },
       { type: ROLE, name: ADMIN },
     ])
@@ -25,7 +24,7 @@ describe('visibleScopes', () => {
     const { alice } = setup('alice')
     const aliceScopes = select.visibleScopes(alice.team.state, {
       type: USER,
-      name: 'alice',
+      name: alice.userId,
     })
     expect(aliceScopes).toEqual([
       { type: TEAM, name: TEAM },
@@ -37,7 +36,7 @@ describe('visibleScopes', () => {
     const { bob } = setup('alice', { user: 'bob', admin: false })
     const bobScopes = select.visibleScopes(bob.team.state, {
       type: USER,
-      name: 'bob',
+      name: bob.userId,
     })
     expect(bobScopes).toEqual([{ type: TEAM, name: TEAM }])
   })
@@ -54,18 +53,18 @@ describe('visibleScopes', () => {
 
   it('after rotating keys, can still see the same scopes', () => {
     const { alice } = setup('alice')
-    const getDeviceScopes = () =>
-      select.visibleScopes(alice.team.state, {
-        type: DEVICE,
-        name: 'alice::laptop',
-      })
+    const { type, name } = alice.device.keys
+
+    const getDeviceScopes = () => {
+      return select.visibleScopes(alice.team.state, { type, name })
+    }
 
     const changeDeviceKeys = () => {
-      alice.team.changeKeys(createKeyset({ type: DEVICE, name: 'alice::laptop' }))
+      alice.team.changeKeys(createKeyset({ type, name }))
     }
 
     expect(getDeviceScopes()).toEqual([
-      { type: USER, name: 'alice' },
+      { type: USER, name: alice.userId },
       { type: TEAM, name: TEAM },
       { type: ROLE, name: ADMIN },
     ])
