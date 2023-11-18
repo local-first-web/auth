74237d07688d2ed8ea6390ac95cd8166b5f830ec
HerbCaudill <herb@devresults.com>
2023-11-18 18:49:22 +0100

    auth/truncateHashes.ts: we return the same type we're passed

--- packages/auth/src/util/truncateHashes.ts
+++ packages/auth/src/util/truncateHashes.ts
@@ -1,12 +1,12 @@
 // ignore coverage
-export function truncateHashes(arg: any): any {
+export function truncateHashes<T>(arg: T): T {
   if (typeof arg === 'string') {
     const hashRx = /(?:[A-Za-z\d+/=]{32,9999999})?/g
-    return arg.replaceAll(hashRx, s => s.slice(0, 5))
+    return arg.replaceAll(hashRx, s => s.slice(0, 5)) as T
   }
 
   if (Array.isArray(arg)) {
-    return arg.map(truncateHashes)
+    return arg.map(truncateHashes) as T
   }
 
   if (typeof arg === 'object') {
