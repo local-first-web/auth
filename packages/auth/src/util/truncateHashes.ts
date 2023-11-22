7197f7f612c60b0b4fe91cf02ee2dcfd794006cb
HerbCaudill <herb@devresults.com>
2023-11-22 19:28:31 +0100

    auth/truncateHashes.ts: bail from circular objects

--- packages/auth/src/util/truncateHashes.ts
+++ packages/auth/src/util/truncateHashes.ts
@@ -1,22 +1,23 @@
 // ignore coverage
-export function truncateHashes<T>(arg: T): T {
+export function truncateHashes<T>(arg: T, i = 0): T {
+  if (i > 10) return arg
   if (typeof arg === 'string') {
     const hashRx = /(?:[A-Za-z\d+/=]{32,9999999})?/g
     return arg.replaceAll(hashRx, s => s.slice(0, 5)) as T
   }
 
   if (Array.isArray(arg)) {
-    return arg.map(truncateHashes) as T
+    return arg.map(x => truncateHashes(x, i++)) as T
   }
 
   if (typeof arg === 'object') {
-    const object = {} as any
+    const object = {} as Partial<T>
     for (const prop in arg) {
       const value = arg[prop]
-      object[truncateHashes(prop)] = truncateHashes(value)
+      object[truncateHashes(prop)] = truncateHashes(value, i++)
     }
 
-    return object
+    return object as T
   }
 
   return arg
