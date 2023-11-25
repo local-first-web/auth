8d432b3fe77df1bd5725869558c6c701fc785ab1
HerbCaudill <herb@devresults.com>
2023-11-25 12:31:10 +0100

    OrderedMessageService

---
+++ packages/auth/src/connection/OrderedMessageService.ts
@@ -0,0 +1,81 @@
+import { EventEmitter } from 'eventemitter3'
+import { ConnectionMessage, NumberedConnectionMessage } from './message.js'
+
+/**
+ * Receives numbered messages and emits them in order. If a message is missing after a delay, asks
+ * for it to be sent (or resent).
+ */
+export class OrderedMessageService<T> extends EventEmitter<OrderedMessageServiceEvents<T>> {
+  #received: Record<number, NumberedMessage<T>> = {}
+  #nextIndex = 0
+  #started = false
+  #timeout: number
+  #waiting: Record<number, TimeoutId> = {}
+
+  constructor({ timeout = 1000 }: Options = {}) {
+    super()
+    this.#timeout = timeout
+  }
+
+  /**
+   * Messages can be received before the service is started (e.g. while waiting to be ready to send
+   * over the network). They will be emitted in order when start() is called.
+   */
+  public start() {
+    this.#started = true
+    this.#processQueue()
+  }
+
+  /**
+   * Stop emitting messages. Messages will be queued until start() is called again.
+   */
+  public stop() {
+    this.#started = false
+  }
+
+  /**
+   * Queues incoming messages and, if we're started, emits them in order.
+   */
+  public deliver(message: NumberedMessage<T>): void {
+    const { index } = message
+    if (this.#received[index]) return // already received
+    this.#received[index] = message
+    if (this.#started) this.#processQueue()
+  }
+
+  /**
+   * Emits any messages that are next in sequence, and requests any missing messages.
+   */
+  #processQueue = () => {
+    while (this.#received[this.#nextIndex]) {
+      const message = this.#received[this.#nextIndex]
+      this.#nextIndex++
+      this.emit('message', message)
+    }
+    // identify missing messages
+    const highest = Math.max(...Object.keys(this.#received).map(Number))
+    for (let i = this.#nextIndex; i < highest; i++) {
+      if (this.#waiting[i]) continue // already waiting
+      // wait for the message to come
+      this.#waiting[i] = setTimeout(() => {
+        // if it still hasn't come, request it
+        if (!this.#received[i]) this.emit('request', i)
+        delete this.#waiting[i]
+      }, this.#timeout)
+    }
+  }
+}
+
+export type NumberedMessage<T> = T & { index: number }
+
+export type OrderedMessageServiceEvents<T> = {
+  message: (message: NumberedMessage<T>) => void
+  request: (index: number) => void
+}
+
+type Options = {
+  /** Time to wait (in ms) before requesting a missing message */
+  timeout?: number
+}
+
+type TimeoutId = ReturnType<typeof setTimeout>
---
+++ packages/auth/src/connection/test/OrderedMessageService.test.ts
@@ -0,0 +1,157 @@
+import { pause } from 'util/testing/pause.js'
+import { describe, expect, it } from 'vitest'
+import { NumberedMessage, OrderedMessageService } from '../OrderedMessageService.js'
+
+const timeout = 20
+
+describe('OrderedMessageService', () => {
+  const setup = () => {
+    const service = new OrderedMessageService<TestMessage>({ timeout })
+    const received: number[] = []
+    const requested: number[] = []
+    service.on('message', message => received.push(message.index))
+    service.on('request', index => requested.push(index))
+    return { service, received, requested }
+  }
+
+  it('emits messages', () => {
+    const { service, received } = setup()
+    service.start()
+    service.deliver({ index: 0 })
+    service.deliver({ index: 1 })
+    service.deliver({ index: 2 })
+    expect(received).toEqual([0, 1, 2])
+  })
+
+  it('when messages are received out of order, emits them in order', () => {
+    const { service, received } = setup()
+    service.start()
+    service.deliver({ index: 0 })
+    service.deliver({ index: 2 }) // <- out of order
+    service.deliver({ index: 1 })
+    expect(received).toEqual([0, 1, 2])
+  })
+
+  it('ignores duplicate messages', () => {
+    const { service, received } = setup()
+    service.start()
+    service.deliver({ index: 0 })
+    service.deliver({ index: 1 })
+    service.deliver({ index: 1 }) // <- duplicate
+    service.deliver({ index: 2 })
+    service.deliver({ index: 0 }) // <- duplicate
+    service.deliver({ index: 2 }) // <- duplicate
+    expect(received).toEqual([0, 1, 2])
+  })
+
+  it('emits messages received before start', () => {
+    const { service, received } = setup()
+
+    // the service is not started yet
+    service.deliver({ index: 0 })
+    service.deliver({ index: 1 })
+    service.deliver({ index: 2 })
+
+    // Nothing is emitted
+    expect(received).toEqual([])
+
+    // once the service is started, messages are emitted in order
+    service.start()
+    expect(received).toEqual([0, 1, 2])
+  })
+
+  it('requests missing messages', async () => {
+    const { service, received, requested } = setup()
+    service.start()
+    service.deliver({ index: 0 })
+    // service.deliver({ index: 1 }) // <- missing
+    // service.deliver({ index: 2 }) // <- missing
+    service.deliver({ index: 3 })
+    service.deliver({ index: 4 })
+
+    // messages 3 & 4 are not emitted because 1 & 2 are missing
+    expect(received).toEqual([0])
+
+    // after a delay, the service requests a resend for the missing messages
+    await pause(timeout * 2)
+    expect(requested).toEqual([1, 2])
+
+    // we respond with the missing messages
+    service.deliver({ index: 1 })
+    service.deliver({ index: 2 })
+
+    // once the missing messages are received, they are emitted in order
+    expect(received).toEqual([0, 1, 2, 3, 4])
+  })
+
+  it('cancels the request if the missing message is received before the timeout', async () => {
+    const { service, received, requested } = setup()
+    service.start()
+    service.deliver({ index: 0 })
+    // service.deliver({ index: 1 }) <- missing
+    // service.deliver({ index: 2 }) <- missing
+    service.deliver({ index: 3 })
+    service.deliver({ index: 4 })
+
+    // messages 3 & 4 are not emitted because 1 & 2 are missing
+    expect(received).toEqual([0])
+
+    // after a short delay, the missing messages arrive
+    await pause(timeout / 2)
+    service.deliver({ index: 1 })
+    service.deliver({ index: 2 })
+
+    // since the missing messages arrived within the delay, we don't request a resend
+    expect(requested).toEqual([])
+
+    // once the missing messages are received, they are emitted in order
+    expect(received).toEqual([0, 1, 2, 3, 4])
+  })
+
+  it(`re-requests a message if it still doesn't come`, async () => {
+    const { service, received, requested } = setup()
+    service.start()
+    service.deliver({ index: 0 })
+    // service.deliver({ index: 1 }) <- missing
+    service.deliver({ index: 2 })
+
+    // after a delay, the service requests a resend for the missing message
+    await pause(timeout * 2)
+    expect(requested).toEqual([1])
+
+    // a new message comes but we still don't have the missing message
+    service.deliver({ index: 3 })
+
+    // so we request it again
+    await pause(timeout * 2)
+    expect(requested).toEqual([1, 1])
+  })
+
+  it('does not deliver messages while stopped', async () => {
+    const { service, received } = setup()
+    service.start()
+    service.deliver({ index: 0 })
+    expect(received).toEqual([0])
+
+    service.stop()
+    service.deliver({ index: 1 })
+    expect(received).toEqual([0])
+  })
+
+  it('can be restarted after being stopped', async () => {
+    const { service, received } = setup()
+    service.start()
+    service.deliver({ index: 0 })
+    expect(received).toEqual([0])
+
+    service.stop()
+    service.deliver({ index: 1 })
+    expect(received).toEqual([0])
+
+    service.start()
+    service.deliver({ index: 2 })
+    expect(received).toEqual([0, 1, 2])
+  })
+})
+
+type TestMessage = {}
