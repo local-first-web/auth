c10f74300c8314344c8a7be9a00accb72ac1b3d7
HerbCaudill <herb@devresults.com>
2023-11-26 17:27:57 +0100

    bytes: crypto: add hashBytes

--- packages/crypto/src/test/hash.test.ts
+++ packages/crypto/src/test/hash.test.ts
@@ -1,8 +1,30 @@
-import { describe, it, expect } from 'vitest'
-import { hash } from '..'
-import { keyToBytes } from '../util/index.js'
+import { pack } from 'msgpackr'
+import { describe, expect, it } from 'vitest'
+import { hash, hashBytes } from '..'
+import { base58, keyToBytes } from '../util/index.js'
 
 describe('crypto', () => {
+  describe('hashBytes', () => {
+    const payload = pack('one if by day, two if by night')
+
+    it('(bytes) has the correct length', () => {
+      const h = hashBytes('TEST_HASH_PURPOSE', payload)
+      expect(h.length).toBe(32)
+    })
+
+    it('(bytes) results are deterministic', () => {
+      const h = hashBytes('TEST_HASH_PURPOSE', payload)
+      const encoded = base58.encode(h)
+      expect(encoded).toMatchInlineSnapshot('"XHgALgKKPhbnBZ81hJx4aszEU5uoF8hg4m8TidP2F1E"')
+    })
+
+    it('(bytes) gives different results with different seeds', () => {
+      const hash1 = hashBytes('SOMETHING', payload)
+      const hash2 = hashBytes('SOMETHING_ELSE', payload)
+      expect(hash2).not.toEqual(hash1)
+    })
+  })
+
   describe('hash', () => {
     const payload = 'one if by day, two if by night'
 
