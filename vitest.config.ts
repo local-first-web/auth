9749e2dbff99df1c931140938bd5b81237e2d82a
Herb Caudill <herb@devresults.com>
2023-12-22 18:24:04 +0100

    vitest: only include packages/* (not demos/*)

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
   build: {
     target: 'esnext',
+  },
+  test: {
+    include: ['packages/**/*.test.ts'],
   },
 })
