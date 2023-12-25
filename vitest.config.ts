2cd10b557b95f347d06cb80d417181dcf6f470e5
Herb Caudill <herb@devresults.com>
2023-12-25 14:15:58 +0100

    vitest update when other packages build

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,19 @@
 import tsconfigPaths from 'vite-tsconfig-paths'
-import { defineConfig } from 'vitest/config'
+import { configDefaults, defineConfig } from 'vitest/config'
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
   build: {
     target: 'esnext',
+    rollupOptions: {
+      output: {
+        preserveModules: true,
+        inlineDynamicImports: false,
+      },
+    },
   },
   test: {
     include: ['packages/**/*.test.ts'],
+    watchExclude: configDefaults.watchExclude.filter(d => !d.includes('dist')),
   },
 })
