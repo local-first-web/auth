67eabfc66c60b454affb5aba5e7a7097c1329618
Herb Caudill <herb@devresults.com>
2023-12-27 18:23:15 +0100

    remove describe  blocks

--- demos/automerge-repo-todos/test/auth.test.ts
+++ demos/automerge-repo-todos/test/auth.test.ts
@@ -2,72 +2,68 @@ import { test } from '@playwright/test'
 import { newBrowser } from './helpers/basePage'
 import { expect } from './helpers/expect'
 
-test.describe('Create team', () => {
-  test('creates a team', async ({ context }) => {
-    const alice = await newBrowser(context)
-    await alice.createTeam('alice', 'alice & friends')
-    await alice.expect.toBeLoggedIn('alice')
-  })
-
-  test('can use the enter key to submit', async ({ context }) => {
-    const alice = await newBrowser(context)
-    await alice.enterFirstName('alice')
-    await alice.pressEnter()
-
-    // press "create a team"
-
-    await alice.pressButton('Create')
-
-    // provide team name
-    await alice.enterTeamName('alice & friends')
-    await alice.pressEnter()
-
-    await alice.expect.toBeLoggedIn('alice')
-  })
-
-  test('team is persisted', async ({ context }) => {
-    const alice = await newBrowser(context)
-    await alice.createTeam('alice', 'alice & friends')
-    await alice.expect.toBeLoggedIn('alice')
-
-    await alice.reload()
-
-    await alice.expect.toBeLoggedIn('alice')
-  })
-
-  test('logs in with an arbitrary name', async ({ context }) => {
-    const alice = await newBrowser(context)
-    await alice.createTeam('reginald', 'alice & friends')
-    await alice.expect.toBeLoggedIn('reginald')
-  })
+test('creates a team', async ({ context }) => {
+  const alice = await newBrowser(context)
+  await alice.createTeam('alice', 'alice & friends')
+  await alice.expect.toBeLoggedIn('alice')
 })
 
-test.describe('Invitations', () => {
-  test('creates a member invitation', async ({ context }) => {
-    const alice = await newBrowser(context)
-    await alice.createTeam('alice', 'alice & friends')
-    const invitationCode = await alice.createMemberInvitation()
+test('can use the enter key to submit', async ({ context }) => {
+  const alice = await newBrowser(context)
+  await alice.enterFirstName('alice')
+  await alice.pressEnter()
 
-    expect(invitationCode.length).toBeGreaterThan(50)
-    expect(invitationCode).toContain('_')
+  // press "create a team"
 
-    await alice.reload()
-    await alice.expect.toBeLoggedIn('alice')
-  })
+  await alice.pressButton('Create')
 
-  test('uses an invitation', async ({ context }) => {
-    // This ensures that there's more than one team on the sync server
-    const noise = await newBrowser(context)
-    noise.createTeam('noise', `noise team`)
+  // provide team name
+  await alice.enterTeamName('alice & friends')
+  await alice.pressEnter()
 
-    const alice = await newBrowser(context)
-    await alice.createTeam('alice', 'alice & friends')
+  await alice.expect.toBeLoggedIn('alice')
+})
 
-    const invitationCode = await alice.createMemberInvitation()
+test('team is persisted', async ({ context }) => {
+  const alice = await newBrowser(context)
+  await alice.createTeam('alice', 'alice & friends')
+  await alice.expect.toBeLoggedIn('alice')
 
-    const bob = await newBrowser(context)
-    await bob.joinAsMember('bob', invitationCode)
+  await alice.reload()
 
-    await bob.expect.toBeLoggedIn('bob')
-  })
+  await alice.expect.toBeLoggedIn('alice')
+})
+
+test('logs in with an arbitrary name', async ({ context }) => {
+  const alice = await newBrowser(context)
+  await alice.createTeam('reginald', 'alice & friends')
+  await alice.expect.toBeLoggedIn('reginald')
+})
+
+test('creates a member invitation', async ({ context }) => {
+  const alice = await newBrowser(context)
+  await alice.createTeam('alice', 'alice & friends')
+  const invitationCode = await alice.createMemberInvitation()
+
+  expect(invitationCode.length).toBeGreaterThan(50)
+  expect(invitationCode).toContain('_')
+
+  await alice.reload()
+  await alice.expect.toBeLoggedIn('alice')
+})
+
+test('uses an invitation', async ({ context }) => {
+  // This ensures that there's more than one team on the sync server
+  const noise = await newBrowser(context)
+  noise.createTeam('noise', `noise team`)
+
+  const alice = await newBrowser(context)
+  await alice.createTeam('alice', 'alice & friends')
+
+  const invitationCode = await alice.createMemberInvitation()
+
+  const bob = await newBrowser(context)
+  await bob.joinAsMember('bob', invitationCode)
+
+  await bob.expect.toBeLoggedIn('bob')
 })
--- demos/automerge-repo-todos/test/todos.test.ts
+++ demos/automerge-repo-todos/test/todos.test.ts
@@ -4,73 +4,71 @@ import { expect } from './helpers/expect'
 
 const TODOS = ['buy some cheese', 'feed the cat', 'book a doctors appointment']
 
-test.describe('Todos', () => {
-  const setup = async (context: BrowserContext) => {
-    const alice = await newBrowser(context)
-    await alice.createTeam('alice', 'alice & friends')
-    await alice.expect.toBeLoggedIn('alice')
-    // Alice invites Bob
-    const invitationCode = await alice.createMemberInvitation()
+const setup = async (context: BrowserContext) => {
+  const alice = await newBrowser(context)
+  await alice.createTeam('alice', 'alice & friends')
+  await alice.expect.toBeLoggedIn('alice')
+  // Alice invites Bob
+  const invitationCode = await alice.createMemberInvitation()
 
-    // Bob joins
-    const bob = await newBrowser(context)
-    await bob.joinAsMember('bob', invitationCode)
-    await bob.expect.toBeLoggedIn('bob')
+  // Bob joins
+  const bob = await newBrowser(context)
+  await bob.joinAsMember('bob', invitationCode)
+  await bob.expect.toBeLoggedIn('bob')
 
-    // Alice and Bob add one todo each
-    await alice.addTodo(TODOS[0])
-    await bob.addTodo(TODOS[1])
+  // Alice and Bob add one todo each
+  await alice.addTodo(TODOS[0])
+  await bob.addTodo(TODOS[1])
 
-    return { alice, bob }
-  }
+  return { alice, bob }
+}
 
-  test('syncs todos', async ({ context }) => {
-    const { alice, bob } = await setup(context)
-    // Alice sees both todos
-    await expect(alice.todos()).toHaveCount(2)
-    await expect(alice.todos(0)).toHaveValue(TODOS[0])
-    await expect(alice.todos(1)).toHaveValue(TODOS[1])
+test('syncs todos', async ({ context }) => {
+  const { alice, bob } = await setup(context)
+  // Alice sees both todos
+  await expect(alice.todos()).toHaveCount(2)
+  await expect(alice.todos(0)).toHaveValue(TODOS[0])
+  await expect(alice.todos(1)).toHaveValue(TODOS[1])
 
-    // Bob sees both todos
-    await expect(bob.todos()).toHaveCount(2)
-    await expect(bob.todos(0)).toHaveValue(TODOS[0])
-    await expect(bob.todos(1)).toHaveValue(TODOS[1])
-  })
+  // Bob sees both todos
+  await expect(bob.todos()).toHaveCount(2)
+  await expect(bob.todos(0)).toHaveValue(TODOS[0])
+  await expect(bob.todos(1)).toHaveValue(TODOS[1])
+})
 
-  test('syncs checked state', async ({ context }) => {
-    const { alice, bob } = await setup(context)
+test('syncs checked state', async ({ context }) => {
+  const { alice, bob } = await setup(context)
 
-    // Alice checks the first todo
-    await alice.toggleTodo(0)
+  // Alice checks the first todo
+  await alice.toggleTodo(0)
 
-    // Alice sees the first todo checked
-    await expect(alice.page.getByRole('checkbox').nth(0)).toBeChecked()
-    await expect(alice.page.getByRole('checkbox').nth(1)).not.toBeChecked()
+  // Alice sees the first todo checked
+  await expect(alice.page.getByRole('checkbox').nth(0)).toBeChecked()
+  await expect(alice.page.getByRole('checkbox').nth(1)).not.toBeChecked()
 
-    // Bob sees the first todo checked
-    await expect(bob.page.getByRole('checkbox').nth(0)).toBeChecked()
-    await expect(bob.page.getByRole('checkbox').nth(1)).not.toBeChecked()
+  // Bob sees the first todo checked
+  await expect(bob.page.getByRole('checkbox').nth(0)).toBeChecked()
+  await expect(bob.page.getByRole('checkbox').nth(1)).not.toBeChecked()
 
-    // Bob checks the second todo
-    await bob.toggleTodo(1)
+  // Bob checks the second todo
+  await bob.toggleTodo(1)
 
-    // Alice sees the second todo checked
-    await expect(alice.page.getByRole('checkbox').nth(0)).toBeChecked()
-    await expect(alice.page.getByRole('checkbox').nth(1)).toBeChecked()
+  // Alice sees the second todo checked
+  await expect(alice.page.getByRole('checkbox').nth(0)).toBeChecked()
+  await expect(alice.page.getByRole('checkbox').nth(1)).toBeChecked()
 
-    // Bob sees the second todo checked
-    await expect(bob.page.getByRole('checkbox').nth(0)).toBeChecked()
-    await expect(bob.page.getByRole('checkbox').nth(1)).toBeChecked()
+  // Bob sees the second todo checked
+  await expect(bob.page.getByRole('checkbox').nth(0)).toBeChecked()
+  await expect(bob.page.getByRole('checkbox').nth(1)).toBeChecked()
 
-    // Alice unchecks the first todo
-    await alice.toggleTodo(0)
+  // Alice unchecks the first todo
+  await alice.toggleTodo(0)
 
-    // Alice sees the first todo unchecked
-    await expect(alice.page.getByRole('checkbox').nth(0)).not.toBeChecked()
-    await expect(alice.page.getByRole('checkbox').nth(1)).toBeChecked()
+  // Alice sees the first todo unchecked
+  await expect(alice.page.getByRole('checkbox').nth(0)).not.toBeChecked()
+  await expect(alice.page.getByRole('checkbox').nth(1)).toBeChecked()
 
-    // Bob sees the first todo unchecked
-    await expect(bob.page.getByRole('checkbox').nth(0)).not.toBeChecked()
-    await expect(bob.page.getByRole('checkbox').nth(1)).toBeChecked()
-  })
+  // Bob sees the first todo unchecked
+  await expect(bob.page.getByRole('checkbox').nth(0)).not.toBeChecked()
+  await expect(bob.page.getByRole('checkbox').nth(1)).toBeChecked()
 })
