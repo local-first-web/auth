77c726da7af4239b75eaad0af18de7b400a9fd24
Herb Caudill <herb@devresults.com>
2024-01-02 22:51:28 +0100

    remove only

--- demos/automerge-repo-todos/test/auth.test.ts
+++ demos/automerge-repo-todos/test/auth.test.ts
@@ -51,7 +51,7 @@ test('creates a member invitation', async ({ context }) => {
   await alice.expect.toBeLoggedIn('alice')
 })
 
-test.only('uses an invitation', async ({ context }) => {
+test('uses an invitation', async ({ context }) => {
   // This ensures that there's more than one team on the sync server, since that was causing
   // intermittent test failures at one point.
   const noise = await newBrowser(context)
