30922385c2ef424a2cf8aee503e029fa647e3c41
Herb Caudill <herb@devresults.com>
2024-01-13 15:52:05 +0100

    demo sync server: don't clear storage when starting up

--- demos/automerge-repo-todos/syncserver.js
+++ demos/automerge-repo-todos/syncserver.js
@@ -1,15 +1,11 @@
-import fs from "fs"
-import { LocalFirstAuthSyncServer } from "@localfirst/auth-syncserver"
+import fs from 'fs'
+import { LocalFirstAuthSyncServer } from '@localfirst/auth-syncserver'
 
-const storageDir = ".dev-sync-server-data"
-
-// in development, clear stored data on startup
-if (process.env.NODE_ENV === "development")
-  fs.rmSync(storageDir, { force: true, recursive: true })
+const storageDir = '.dev-sync-server-data'
 
 const DEFAULT_PORT = 3030
 const port = Number(process.env.PORT) || DEFAULT_PORT
-const host = process.env.HOST || "localhost"
+const host = process.env.HOST || 'localhost'
 
 const server = new LocalFirstAuthSyncServer(host)
 
