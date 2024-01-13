ed24a0154876afe56790d804c17906852da4610d
Herb Caudill <herb@devresults.com>
2024-01-13 15:52:38 +0100

    throw if context is null or undefined

--- demos/automerge-repo-todos/src/hooks/useAuth.tsx
+++ demos/automerge-repo-todos/src/hooks/useAuth.tsx
@@ -4,6 +4,6 @@ import { LocalFirstAuthContext } from '../components/LocalFirstAuthProvider'
 // Convenience wrapper around our authContext for accessing the auth data and provider
 export const useAuth = () => {
   const context = useContext(LocalFirstAuthContext)
-  if (context === null) throw new Error('useAuth must be used within a LocalFirstAuthProvider')
+  if (!context) throw new Error('useAuth must be used within a LocalFirstAuthProvider')
   return context
 }
