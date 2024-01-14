b6e1637d74bd2916ba6de3e340f0641e1e60b209
Herb Caudill <herb@devresults.com>
2024-01-14 13:52:31 +0100

    revert not clearing storage on sync server

--- demos/automerge-repo-todos/syncserver.js
+++ demos/automerge-repo-todos/syncserver.js
@@ -2,6 +2,8 @@ import fs from 'fs'
 import { LocalFirstAuthSyncServer } from '@localfirst/auth-syncserver'
 
 const storageDir = '.dev-sync-server-data'
+// in development, clear stored data on startup
+if (process.env.NODE_ENV === 'development') fs.rmSync(storageDir, { force: true, recursive: true })
 
 const DEFAULT_PORT = 3030
 const port = Number(process.env.PORT) || DEFAULT_PORT
