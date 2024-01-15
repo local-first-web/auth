c137e00a1affe5c11d057d633e806c558b750a72
Herb Caudill <herb@devresults.com>
2024-01-15 14:55:29 +0100

    createRepoWithAuth: comments

--- demos/automerge-repo-todos/src/util/createRepoWithAuth.ts
+++ demos/automerge-repo-todos/src/util/createRepoWithAuth.ts
@@ -6,14 +6,19 @@ import { AuthProvider } from '@localfirst/auth-provider-automerge-repo'
 import { eventPromise } from '@localfirst/auth-shared'
 import { host, wsUrl } from './syncServerUrl'
 
+/**
+ * Creates an auth provider and a repo with a shared storage adapter and a websocket adapter.
+ * Returns when the repo has a working network connection.
+ */
 export const createRepoWithAuth = async ({ user, device }: Params) => {
+  // We'll use the same storage adapter for the auth provider and the repo
   const storage = new IndexedDBStorageAdapter()
   const auth = new AuthProvider({ user, device, storage, server: host })
 
   const adapter = new BrowserWebSocketClientAdapter(wsUrl)
   const authAdapter = auth.wrap(adapter)
 
-  // Create new automerge-repo with websocket adapter
+  // Create new repo with websocket adapter
   const repo = new Repo({
     peerId: device.deviceId as PeerId,
     network: [authAdapter],
