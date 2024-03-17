01c9d58d80ada3b34c97bac8052bf90053393881
Herb Caudill <herb@devresults.com>
2024-03-17 13:29:12 +0100

    auth-provider-automerge-repo/getShareId.ts:

--- packages/auth-provider-automerge-repo/src/getShareId.ts
+++ packages/auth-provider-automerge-repo/src/getShareId.ts
@@ -1,5 +1,5 @@
 import type { Team } from '@localfirst/auth'
-import { type ShareId } from 'types.js'
+import type { ShareId } from './types.js'
 
 /**
  * We use the first few characters of the team ID as the share ID. The team ID is a hash of the team's
@@ -10,7 +10,7 @@ import { type ShareId } from 'types.js'
  * give us 70 bits of entropy; according to [this calculator](https://kevingal.com/apps/collision.html),
  * you'd need to have 100 trillion shareIds on a single device before you'd have a 1% chance of a collision.
  */
-export const getShareId = (team: Team) => {
+export const getShareId = (team: Team): ShareId => {
   const teamId = team.id
   return teamId.slice(0, 12) as ShareId
 }
