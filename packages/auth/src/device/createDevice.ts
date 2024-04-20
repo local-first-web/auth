c442a95117d68836dbb7c7a8596bcf7bd82e1cb9
Herb Caudill <herb@devresults.com>
2024-04-20 11:29:17 -0400

    createDevice: params type alias

--- packages/auth/src/device/createDevice.ts
+++ packages/auth/src/device/createDevice.ts
@@ -1,19 +1,26 @@
-import { createKeyset } from '@localfirst/crdx'
+import { createKeyset, type UnixTimestamp } from '@localfirst/crdx'
 import { createId } from '@paralleldrive/cuid2'
 import { randomKey } from '@localfirst/crypto'
 import type { DeviceWithSecrets } from './types.js'
 import { KeyType } from 'util/index.js'
 
+console.log('*********** using local createDevice')
 export const createDevice = ({
   userId,
   deviceName,
+  deviceInfo = {},
+  created = Date.now() as UnixTimestamp,
   seed = randomKey(),
-}: {
-  userId: string
-  deviceName: string
-  seed?: string
-}): DeviceWithSecrets => {
+}: Params): DeviceWithSecrets => {
   const deviceId = createId()
   const keys = createKeyset({ type: KeyType.DEVICE, name: deviceId }, seed)
-  return { userId, deviceId, deviceName, keys }
+  return { userId, deviceId, deviceName, keys, created, deviceInfo }
+}
+
+type Params = {
+  userId: string
+  deviceName: string
+  deviceInfo?: any
+  created?: UnixTimestamp
+  seed?: string
 }
