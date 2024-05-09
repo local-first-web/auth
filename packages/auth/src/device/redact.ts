0f656897952ad84fd180e951b1e98f2aa12cf5a4
Herb Caudill <herb@devresults.com>
2024-05-09 12:04:08 +0200

    just replace keys when redacting device

--- packages/auth/src/device/redact.ts
+++ packages/auth/src/device/redact.ts
@@ -2,9 +2,6 @@ import { redactKeys } from '@localfirst/crdx'
 import { type DeviceWithSecrets, type Device } from 'device/types.js'
 
 export const redactDevice = (device: DeviceWithSecrets): Device => ({
-  userId: device.userId,
-  deviceId: device.deviceId,
-  deviceName: device.deviceName,
-  deviceInfo: device.deviceInfo,
+  ...device,
   keys: redactKeys(device.keys),
 })
