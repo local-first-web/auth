3def373b8b19cdb482c736a90d9ad7c469a5b86f
Isla Koenigsknecht <ikoenigbu@gmail.com>
2024-07-16 11:28:24 -0400

    DMService

--- demos/quiet-sandbox/src/auth/services/dm/dm_service.ts
+++ demos/quiet-sandbox/src/auth/services/dm/dm_service.ts
@@ -2,8 +2,26 @@
  * Handles DM/Group DM-related chain operations
  */
 
-class DMService {
-  private constructor() {}
+import { BaseChainService } from "../base_service.js"
+
+class DMService extends BaseChainService {
+  protected static instance: DMService | undefined
+
+  public static init(): DMService {
+    if (DMService.instance == null) {
+      DMService.instance = new DMService() 
+    }
+
+    return DMService.instance
+  }
+
+  public static getInstance(): DMService {
+    if (DMService.instance == null) {
+      throw new Error(`DMService hasn't been initialized yet!  Run init() before accessing`)
+    }
+
+    return DMService.instance
+  }
 
 }
 
