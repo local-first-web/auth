a75289fb4be3adb403d0f43073bf2106a72455a5
Herb Caudill <herb@devresults.com>
2024-09-21 18:24:48 +0200

    teamKeyring: use keyring selector

--- packages/auth/src/team/selectors/teamKeyring.ts
+++ packages/auth/src/team/selectors/teamKeyring.ts
@@ -1,11 +1,9 @@
-import { type KeysetWithSecrets, createKeyring } from '@localfirst/crdx'
+import { type KeysetWithSecrets } from '@localfirst/crdx'
 import { type TeamState } from 'team/types.js'
 import { KeyType } from 'util/types.js'
-import { keyMap } from './keyMap.js'
+import { keyring } from './keyring.js'
 
 const { TEAM } = KeyType
 
-export const teamKeyring = (state: TeamState, keys: KeysetWithSecrets) => {
-  const allTeamKeys = keyMap(state, keys)[TEAM][TEAM]
-  return createKeyring(allTeamKeys)
-}
+export const teamKeyring = (state: TeamState, keys: KeysetWithSecrets) =>
+  keyring(state, { type: TEAM, name: TEAM }, keys)
